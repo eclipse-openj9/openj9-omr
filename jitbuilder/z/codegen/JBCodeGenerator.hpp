c5ff6c95385d27f3836365160ff5276dee591d11
Aman Kumar <amank@ca.ibm.com>
2016-09-28 16:42:01 -0400

    Fix JitBuilder compile errors on s390x
    JitBuilder can now be compiled on s390x, but is untested.
    
    Issue: #283
    Signed-off-by: Aman Kumar <amank@ca.ibm.com>

--- jitbuilder/z/codegen/JBCodeGenerator.hpp
+++ jitbuilder/z/codegen/JBCodeGenerator.hpp
@@ -33,7 +33,7 @@ namespace JitBuilder { typedef Z::CodeGenerator CodeGeneratorConnector; }
 #endif
 
 
-#include "codegen/JBCodeGenerator.hpp"
+#include "jitbuilder/codegen/JBCodeGenerator.hpp"
 #include "codegen/LinkageConventionsEnum.hpp"
 
 

10827fa1dfc32e594d5392908e3ddf0c5ec3bfcf
Filip Jeremic <fjeremic@ca.ibm.com>
2016-10-13 17:19:21 -0400

    Document TR_OutOfLineCodeSection
    Add high-level documentation of what benefits can be achieved by using
    an OOL code section and how such benefits are achieved.
    
    [ci skip] Documentation only.
    
    Signed-off-by: Filip Jeremic <fjeremic@ca.ibm.com>

--- compiler/codegen/OutOfLineCodeSection.hpp
+++ compiler/codegen/OutOfLineCodeSection.hpp
@@ -30,6 +30,77 @@ namespace TR { class LabelSymbol; }
 namespace TR { class Node; }
 namespace TR { class Register; }
 
+/** \brief
+ *
+ *  Represents a sequence of instructions which will be emitted outside of the main-line instruction sequence with
+ *  the register allocator favoring the allocation of registers that appear in the main-line instruction sequence.
+ *
+ *  \details
+ *
+ *  Out of line (OOL) code sections (paths) are typically used to bias internal control flow edges during register
+ *  allocation. The local register allocator (RA) is unaware of control flow regions within a basic block and will
+ *  assign the registers within the respective block in a linear pass.
+ *
+ *  Control flow within a basic block can be introduced by code generator evaluators; so called internal control
+ *  flow (ICF) regions. Typically an ICF will look like a diamond:
+ *
+ *  \code
+ *   ...
+ *    |
+ *  +-+-+     +---+
+ *  | 3 + --> + 1 |
+ *  +-+-+     +-+-+
+ *    |         |     \
+ *   ...       ...    | ICF region
+ *    |         |     /
+ *  +-+-+     +-+-+
+ *  | 4 + <-- + 2 |
+ *  +-+-+     +---+
+ *    |
+ *   ...
+ *  \endcode
+ *
+ *  Block 3 represents a branch instruction which branches to block 1 representing a label. On both sides of the
+ *  diamond ... represent some sequence of non-branch instructions. Block 2 represents a branch which branches to
+ *  block 4 representing the merge label. Note that the sequence of instructions in each block are encoded linearly
+ *  and the diamond diagram is drawn for the sake of example.
+ *
+ *  Because RA is unaware of ICF regions any virtual registers used inside of the ICF region must be attached as
+ *  post register dependencies on the ICF merge label to ensure that no register shuffles or spills can occur
+ *  inside of the ICF region. This must be done to ensure that the register allocation will not shuffle or spill
+ *  any value on one side of the diamond but not the other.
+ *
+ *  If one side of the diamond is not often executed we could be unnecessarily introducing register shuffling or
+ *  register spilling on the merge label to satisfy all post register dependencies, i.e. the union of all registers
+ *  used on either side of the diamond. The OOL code section is used to improve the register allocation of the fast
+ *  path by sacrificing performance of the slow path.
+ *
+ *  We define the fast path of the diamond to be the instructions most often executed and the slow path of the
+ *  diamond to be the instructions least often executed. In our example above instructions between block 3 and block
+ *  4 represent the fast path and instructions between block 1 and block 2 represent the slow path instructions.
+ *
+ *  Instructions in the slow path will be emitted inside of an OOL code section and the register allocation proceeds
+ *  to register allocate the entire region as follows (assuming a backwards register allocation pass):
+ *
+ *  1. Register allocate block 4
+ *  2. Register allocate fast path instructions
+ *  3. Register allocate block 2
+ *  4. Register allocate slow path instructions
+ *  5. Register allocate block 1
+ *  6. Register allocate block 3
+ *
+ *  The register allocator takes a snapshot of the register states, where the register state refers to all register
+ *  associations, assignments, and states between live virtual and real registers at blocks 4 and 3 and restores the
+ *  register state when register allocating blocks 2 and 1 respectively. The act of restoring the register state is
+ *  achieved by creating register dependencies at the respective locations so as to satisfy the exact register state
+ *  in the fast path of the ICF diamond. The register allocator also ensures that any spilled registers in the fast
+ *  path are properly spilled in the slow path.
+ *
+ *  The net effect of using an OOL code section for the slow path of an ICF diamond is that the register allocator
+ *  will register allocate the fast path of the ICF diamond as if no ICF region exists. In addition to the benefits
+ *  achieved through register allocation OOL code sections will typically be binary encoded outside of the main-line
+ *  instruction sequence and be placed towards the end of the method so as to improve i-cache coherence.
+ */
 class TR_OutOfLineCodeSection
    {
 

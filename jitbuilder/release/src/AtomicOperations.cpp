4d6593f62ff48564fed890e85d0971f07ca4e305
fredqiu <fredqiu@ca.ibm.com>
2016-11-23 11:50:29 -0500

    Add new testcase AtomicOperations in jitbuilder for AtomicAdd()
    Signed-off-by: fredqiu <fredqiu@ca.ibm.com>

---
+++ jitbuilder/release/src/AtomicOperations.cpp
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ *
+ * (c) Copyright IBM Corp. 2016, 2016
+ *
+ *  This program and the accompanying materials are made available
+ *  under the terms of the Eclipse Public License v1.0 and
+ *  Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Contributors:
+ *    Multiple authors (IBM Corp.) - initial implementation and documentation
+ ******************************************************************************/
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <dlfcn.h>
+#include <errno.h>
+
+#include "Jit.hpp"
+#include "ilgen/TypeDictionary.hpp"
+#include "ilgen/MethodBuilder.hpp"
+#include "AtomicOperations.hpp"
+
+using std::cout;
+using std::cerr;
+
+
+int
+main(int argc, char *argv[])
+   {
+   cout << "Step 1: initialize JIT\n";
+   bool initialized = initializeJit();
+   if (!initialized)
+      {
+      cerr << "FAIL: could not initialize JIT\n";
+      exit(-1);
+      }
+
+   cout << "Step 2: define type dictionary\n";
+   TR::TypeDictionary types;
+
+   cout << "Step 3: compile 32-bit method builders\n";
+   AtomicInt32Add method(&types);
+   uint8_t *entry32 = 0;
+   int32_t rc = compileMethodBuilder(&method, &entry32);
+   if (rc != 0)
+      {
+      cerr << "First method FAIL: compilation error " << rc << "\n";
+      exit(-2);
+      }
+
+   cout << "Step 4: start testing 32-bit atomic increment\n";
+   typedef int32_t (Atomic32MethodFunction)(int32_t*);
+   Atomic32MethodFunction *increment32 = (Atomic32MethodFunction *) entry32;
+
+   int32_t v;
+   v=0;   cout << "atomic increment32(" << v; cout << ") == " << increment32(&v) << "\n";
+   v=1;   cout << "atomic increment32(" << v; cout << ") == " << increment32(&v) << "\n";
+   v=10;  cout << "atomic increment32(" << v; cout << ") == " << increment32(&v) << "\n";
+   v=-15; cout << "atomic increment32(" << v; cout << ") == " << increment32(&v) << "\n";
+
+   cout << "Step 5: compile 64-bit method builders\n";
+   AtomicInt64Add method2(&types);
+   uint8_t *entry64 = 0;
+   int32_t rsc = compileMethodBuilder(&method2, &entry64);
+   if (rsc != 0)
+      {   
+      cerr << "Second method FAIL: compilation error " << rsc << "\n";
+      exit(-2); 
+      } 
+
+   cout << "Step 6: start testing 64-bit atomic increment\n";
+   typedef int64_t (Atomic64MethodFunction)(int64_t*);
+   Atomic64MethodFunction *increment64 = (Atomic64MethodFunction *) entry64;
+
+   int64_t val;
+   val=0;   cout << "atomic increment64(" << val; cout << ") == " << increment64(&val) << "\n";
+   val=1;   cout << "atomic increment64(" << val; cout << ") == " << increment64(&val) << "\n";
+   val=10;  cout << "atomic increment64(" << val; cout << ") == " << increment64(&val) << "\n";
+   val=-15; cout << "atomic increment64(" << val; cout << ") == " << increment64(&val) << "\n";
+
+   cout << "Step 7: shutdown JIT\n";
+   shutdownJit();
+   }
+
+AtomicInt32Add::AtomicInt32Add(TR::TypeDictionary *d)
+   : MethodBuilder(d)
+   {
+   DefineLine(LINETOSTR(__LINE__));
+   DefineFile(__FILE__);
+
+   DefineName("increment");
+   pInt32=d->PointerTo(Int32);
+   DefineParameter("addressOfValue", pInt32);
+   DefineReturnType(Int32);
+   }
+
+bool
+AtomicInt32Add::buildIL()
+   {
+   cout << "AtomicInt32Add::buildIL() running!\n";
+   AtomicAdd(
+           IndexAt(pInt32, Load("addressOfValue"), ConstInt32(0)),
+           ConstInt32(1));
+   Return(LoadAt(pInt32, Load("addressOfValue")));
+   
+   return true;
+   }
+
+AtomicInt64Add::AtomicInt64Add(TR::TypeDictionary *d) 
+   : MethodBuilder(d)
+   {   
+   DefineLine(LINETOSTR(__LINE__));
+   DefineFile(__FILE__);
+
+   DefineName("increment64");
+   pInt64=d->PointerTo(Int64);
+   DefineParameter("addressOfValue", pInt64);
+   DefineReturnType(Int64);
+   }   
+
+bool
+AtomicInt64Add::buildIL()
+   {   
+   cout << "AtomicInt64Add::buildIL() running!\n";
+   AtomicAdd(
+           IndexAt(pInt64, Load("addressOfValue"), ConstInt64(0)),
+           ConstInt64(1));
+   Return(LoadAt(pInt64, Load("addressOfValue")));
+   
+   return true;
+   }  
---
+++ jitbuilder/release/src/AtomicOperations.hpp
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *
+ * (c) Copyright IBM Corp. 2016, 2016
+ *
+ *  This program and the accompanying materials are made available
+ *  under the terms of the Eclipse Public License v1.0 and
+ *  Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Contributors:
+ *    Multiple authors (IBM Corp.) - initial implementation and documentation
+ ******************************************************************************/
+
+
+#ifndef ATOMICOPS_INCL
+#define ATOMICOPS_IMPL
+
+#include "ilgen/MethodBuilder.hpp"
+
+/**
+ * I created two methodBuilders for testing
+ * Int32 / Int64 atomicadd() respectively
+ */
+
+class AtomicInt32Add : public TR::MethodBuilder
+   {
+   private:
+   TR::IlType *pInt32;
+
+   public:
+   AtomicInt32Add(TR::TypeDictionary *);
+   virtual bool buildIL();
+   };
+
+class AtomicInt64Add : public TR::MethodBuilder
+   {   
+   private:
+   TR::IlType *pInt64;
+
+   public:
+   AtomicInt64Add(TR::TypeDictionary *); 
+   virtual bool buildIL();
+   };
+
+#endif // !defined(ATOMICOPS_INCL)

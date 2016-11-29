c34128c166a8c4cb016d51e6afab1ddc462d4abe
Irwin D'Souza <dsouzai@ca.ibm.com>
2016-11-29 13:50:33 -0500

    Remove unused error codes
    This commit removes unused #define'd error codes.
    
    Issue: #380
    Signed-off-by: Irwin D'Souza <dsouzai@ca.ibm.com>

--- compiler/compile/CompilationException.hpp
+++ compiler/compile/CompilationException.hpp
@@ -30,48 +30,36 @@ struct CompilationException : public virtual std::exception
    virtual const char* what() const throw() { return "Compilation Exception"; }
    };
 
-// For COMPILATION_IL_GEN_FAILURE
 struct ILGenFailure : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "IL Gen Failure"; }
    };
 
-// For:
-//    COMPILATION_LOOPS_OR_BASICBLOCKS_EXCEEDED
-//    COMPILATION_REMOVE_EDGE_NESTING_DEPTH_EXCEEDED
-//    COMPILATION_MAX_DEPTH_EXCEEDED
-//    COMPILATION_MAX_NODE_COUNT_EXCEEDED
-//    COMPILATION_INTERNAL_POINTER_EXCEED_LIMIT
 struct ExcessiveComplexity : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "Excessive Complexity"; }
    };
 
-// For COMPILATION_MAX_CALLER_INDEX_EXCEEDED
 struct MaxCallerIndexExceeded : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "Max Caller Index Exceeded"; }
    };
 
-// For COMPILATION_INTERRUPTED
 struct CompilationInterrupted : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "Compilation Interrupted"; }
    };
 
-// For COMPILATION_UNIMPL_OPCODE
 struct UnimplementedOpCode : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "Unimplemented Op Code"; }
    };
 
-// For COMPILATION_NEEDED_AT_HIGHER_LEVEL
 struct InsufficientlyAggressiveCompilation : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "Insufficiently Aggressive Compilation"; }
    };
 
-// For COMPILATION_GCRPATCH_FAILURE
 struct GCRPatchFailure : public virtual CompilationException
    {
    virtual const char* what() const throw() { return "GCR Patch Failure"; }

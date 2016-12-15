54967edc3d1904d17155fdcab1aa0f00fb269721
Mark Stoodley <mstoodle@ca.ibm.com>
2016-12-15 11:29:32 -0500

    Correct copyright messages in jitbuilder files
    Add missing copyright / license headers to files in the jitbuilder
    directory (particularly underneath its build directory) and update
    several incorrect copyright dates based on the origins of the files
    in the jitbuilder directory.
    
    Issue: #575
    
    Signed-off-by: Mark Stoodley <mstoodle@ca.ibm.com>

--- jitbuilder/env/ObjectModel.hpp
+++ jitbuilder/env/ObjectModel.hpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * (c) Copyright IBM Corp. 2000, 2016
+ * (c) Copyright IBM Corp. 2014, 2016
  *
  *  This program and the accompanying materials are made available
  *  under the terms of the Eclipse Public License v1.0 and

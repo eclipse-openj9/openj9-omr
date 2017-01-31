614e5e1fa4d7c6939846579d4733cf636b7f0d62
Babneet Singh <sbabneet@ca.ibm.com>
2017-01-31 02:48:29 -0500

    Fix potential bugs and refactor spinlock code
    Potential bugs:
    
    1) omrthread_spinlock_acquire gets called from two locations:
    monitor_enter_three_tier and omrthread_monitor_try_enter_using_threadId.
    In monitor_enter_three_tier, there is an attempt to acquire the monitor
    after a call to omrthread_spinlock_acquire. In
    omrthread_monitor_try_enter_using_threadId, there is no attempt to
    acquire the monitor after a call to omrthread_spinlock_acquire. Thus,
    omrthread_spinlock_acquire should attempt to acquire the lock atleast
    once when spinning is disabled. In eclipse#457, changes were pushed that
    violated this requirement. In eclipse#457 changes,
    omrthread_spinlock_acquire would make no attempts to acquire the monitor
    if it was decided not to spin. In the new changes, this bug has been
    fixed.
    
    2) Adaptive spin feature can disable spinning when other threads are
    already in the process of spinning. Threads that are spinning do not use
    any synchronization primitive to read spin counts from
    omrthread_monitor_t. In omrthread_spinlock_acquire, we currently read
    spin counts multiple times from omrthread_monitor_t in the nested for
    loops. If the adaptive spin feature suddenly disables spinning while a
    thread is already in the process of spinning, then the spin counts will
    get corrupted, and incorrect JLM information will be recorded. Also, we
    currently do not stop spinning if the adaptive spin feature disables
    spinning while a thread is already in the process of spinning. The new
    changes fix these bugs. Spin counts are read once before spinning
    instead of reading spin counts multiple times while spinning. In the
    nested for loops, a check has been added to recognize if spinning has
    been disabled by the adaptive spin feature.
    
    Refactor code: 
    
    1) J9_ARE_ALL_BITS_SET is more reliable to make sure if a flag is set.
    In the new changes, J9_ARE_ALL_BITS_SET is used for flag checks.
    
    2) Updating JLM counts at two places makes it difficult to read code.
    In the new changes, JLM counts are updated in a more efficient manner at
    one location. 
    
    3) A new build flag is added: OMR_THR_SPIN_CODE_REFACTOR. The new
    changes are wrapped around this build flag. This build flag helps
    preserve the old implementation. This build flag is currently disabled
    so the old implementation is active.
    
    4) Enabling build flags: OMR_THR_SPIN_CODE_REFACTOR and
    OMR_THR_SPIN_WAKE_CONTROL.
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- thread/common/threadhelpers.cpp
+++ thread/common/threadhelpers.cpp
@@ -46,6 +46,99 @@ omrthread_monitor_unpin(omrthread_monitor_t monitor, omrthread_t self)
  *
  * @return 0 on success, -1 on failure
  */
+#if defined(OMR_THR_SPIN_CODE_REFACTOR)
+intptr_t
+omrthread_spinlock_acquire(omrthread_t self, omrthread_monitor_t monitor)
+{
+	volatile uintptr_t *target = (volatile uintptr_t *)&monitor->spinlockState;
+	intptr_t result = -1;
+	uintptr_t oldState = J9THREAD_MONITOR_SPINLOCK_UNOWNED;
+	uintptr_t newState = J9THREAD_MONITOR_SPINLOCK_OWNED;
+	omrthread_library_t const lib = self->library;
+
+#if defined(OMR_THR_JLM)
+	J9ThreadMonitorTracing *tracing = NULL;
+	if (J9_ARE_ALL_BITS_SET(lib->flags, J9THREAD_LIB_FLAG_JLM_ENABLED)) {
+		tracing = monitor->tracing;
+	}
+#endif /* OMR_THR_JLM */
+
+	uintptr_t spinCount3Init = monitor->spinCount3;
+	uintptr_t spinCount2Init = monitor->spinCount2;
+	uintptr_t spinCount1Init = monitor->spinCount1;
+
+#if defined(OMR_THR_SPIN_WAKE_CONTROL)
+	BOOLEAN spinning = TRUE;
+ 	if (monitor->spinThreads < lib->maxSpinThreads) {
+ 		VM_AtomicSupport::add(&monitor->spinThreads, 1);
+ 	} else {
+ 		spinCount1Init = 1;
+ 		spinCount2Init = 1;
+ 		spinCount3Init = 1;
+ 		spinning = FALSE;
+ 	}
+#endif /* defined(OMR_THR_SPIN_WAKE_CONTROL) */
+
+	uintptr_t spinCount3 = spinCount3Init;
+	uintptr_t spinCount2 = spinCount2Init;
+
+	for (; spinCount3 > 0; spinCount3--) {
+		for (spinCount2 = spinCount2Init; spinCount2 > 0; spinCount2--) {
+			/* Try to put 0 into the target field (-1 indicates free)'. */
+			if (oldState == VM_AtomicSupport::lockCompareExchange(target, oldState, newState, true)) {
+				result = 0;
+				VM_AtomicSupport::readBarrier();
+				goto update_jlm;
+			}
+			/* Stop spinning if adaptive spin heuristic disables spinning */
+			if (J9_ARE_ALL_BITS_SET(monitor->flags, J9THREAD_MONITOR_DISABLE_SPINNING)) {
+				goto update_jlm;
+			}
+			VM_AtomicSupport::yieldCPU();
+			/* begin tight loop */
+			for (uintptr_t spinCount1 = spinCount1Init; spinCount1 > 0; spinCount1--)	{
+				VM_AtomicSupport::nop();
+			} /* end tight loop */
+		}
+#if defined(OMR_THR_YIELD_ALG)
+		omrthread_yield_new(spinCount3);
+#else /* OMR_THR_YIELD_ALG */
+		omrthread_yield();
+#endif /* OMR_THR_YIELD_ALG */
+	}
+
+update_jlm:
+#if defined(OMR_THR_JLM)
+	if (NULL != tracing) {
+		/* Add JLM counts atomically:
+		 * let m=spinCount3Init, n=spinCount2Init
+		 * let i=spinCount2, j=spinCount3
+		 *
+		 * after partial set of spins (0 == result),
+		 * yield_count += m-j, spin2_count += ((m-j)*n)+(n-i+1)
+		 *
+		 * after complete set of spins (-1 == result),
+		 * yield_count += m, spin2_count += m*n
+		 */
+		uintptr_t yield_count = spinCount3Init - spinCount3;
+		uintptr_t spin2_count = yield_count * spinCount2Init;
+		if (0 != yield_count) {
+			spin2_count += (spinCount2Init - spinCount2 + 1);
+		}
+		VM_AtomicSupport::add(&tracing->yield_count, yield_count);
+		VM_AtomicSupport::add(&tracing->spin2_count, spin2_count);
+	}
+#endif /* OMR_THR_JLM */
+
+#if defined(OMR_THR_SPIN_WAKE_CONTROL)
+	if (spinning) {
+		VM_AtomicSupport::subtract(&monitor->spinThreads, 1);
+	}
+#endif /* defined(OMR_THR_SPIN_WAKE_CONTROL) */
+
+	return result;
+}
+#else /* OMR_THR_SPIN_CODE_REFACTOR */
 intptr_t
 omrthread_spinlock_acquire(omrthread_t self, omrthread_monitor_t monitor)
 {
@@ -126,6 +219,7 @@ done:
 #endif /* defined(OMR_THR_SPIN_WAKE_CONTROL) */
 	return result;
 }
+#endif /* OMR_THR_SPIN_CODE_REFACTOR */
 
 /**
   * Try to atomically swap out a value of SPINLOCK_UNOWNED from

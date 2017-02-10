2d8f22a05837afa2c383a4412b6f57fb53f87e62
Yi Zhang <yizhang@ca.ibm.com>
2017-02-10 10:31:50 -0500

    Fix the inliner priv arg remat bool
    The second part of logic to decide suitableForRemat should
    only apply when the first part decide suitableForRemat is true
    
    Signed-off-by: Yi Zhang <yizhang@ca.ibm.com>

--- compiler/optimizer/Inliner.cpp
+++ compiler/optimizer/Inliner.cpp
@@ -1561,23 +1561,25 @@ void TR_InlinerBase::rematerializeCallArguments(TR_TransformInlinedFunction & ti
    static char *disableProfiledGuardRemat = feGetEnv("TR_DisableProfiledGuardRemat");
 
    bool suitableForRemat = !comp()->getOption(TR_DisableGuardedCallArgumentRemat) && !comp()->isProfilingCompilation();
-   // Vijay's Inliner change 02
-   if (guard->_kind == TR_NoGuard)
+   if (suitableForRemat)
       {
-      suitableForRemat = false;
-      }
-   else if (guard->_kind == TR_ProfiledGuard)
-      {
-      if (disableProfiledGuardRemat)
+      if (guard->_kind == TR_NoGuard)
          {
          suitableForRemat = false;
          }
-      else
+      else if (guard->_kind == TR_ProfiledGuard)
          {
-         suitableForRemat = getPolicy()->suitableForRemat(comp(), callNode, guard);
-         if (!suitableForRemat)
+         if (disableProfiledGuardRemat)
             {
-            debugTrace(tracer(),"  skipping remat on profiled guard [%p] due to policy decision",guard);
+            suitableForRemat = false;
+            }
+         else
+            {
+            suitableForRemat = getPolicy()->suitableForRemat(comp(), callNode, guard);
+            if (!suitableForRemat)
+               {
+               debugTrace(tracer(),"  skipping remat on profiled guard [%p] due to policy decision",guard);
+               }
             }
          }
       }

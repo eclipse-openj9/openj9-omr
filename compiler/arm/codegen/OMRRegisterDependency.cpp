b55cd77fe95d73aa15d7d8592b06d492e19208b9
Leonardo Banderali <leob@linux.vnet.ibm.com>
2017-03-10 13:58:17 -0500

    Add ARM definition of unionNoRegPostCondition
    This change adds an implementation for the function
    `OMR::ARM::RegisterDependencyConditions::unionNoRegPostCondition`.
    This implementation was copied from the implementation for POWER.
    
    I am marking this change with [ci skip] because there are no tests
    currently setup for ARM.
    
    Signed-off-by: Leonardo Banderali <leob@linux.vnet.ibm.com>

--- compiler/arm/codegen/OMRRegisterDependency.cpp
+++ compiler/arm/codegen/OMRRegisterDependency.cpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * (c) Copyright IBM Corp. 2000, 2016
+ * (c) Copyright IBM Corp. 2000, 2017
  *
  *  This program and the accompanying materials are made available
  *  under the terms of the Eclipse Public License v1.0 and
@@ -650,4 +650,9 @@ TR::RegisterDependencyConditions *OMR::ARM::RegisterDependencyConditions::cloneA
                               singlePair->getFlags());
       }
    return result;
+   }
+
+void OMR::ARM::RegisterDependencyConditions::unionNoRegPostCondition(TR::Register *reg, TR::CodeGenerator *cg)
+   {
+   addPostCondition(reg, TR::RealRegister::NoReg);
    }

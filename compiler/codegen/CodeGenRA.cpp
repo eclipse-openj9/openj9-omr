bc91d4de01d3d9cfd6d5678519b1d634a0e71d9c
jimmyk <jimmyk@ca.ibm.com>
2017-04-27 16:31:04 -0400

    Enable AutoSIMD support on x86
    Updated getSupportsOpCodeForAutoSIMD in x/codegen/OMRCodeGenerator.cpp to
    reflect the recently implemented vector evaluators. Instead of returning
    false in all cases, getSupportsOpCodeForAutoSIMD will now return true for
    loads, stores and vector splats for int32, int64, float and double data
    and also double type add and multiply. All of these operations are
    supported in the new vector evaluators.
    
    A call to setSupportsAutoSIMD is now made inside initialize in
    OMRCodeGenerator.cpp to indicate that autoSIMD is now supported. This call
    is behind a check for SSE2 support.
    
    The function isFPRUsedAsVRF and any references to it were removed. It was
    added to prepareRegistersForAssignment in error. It is not needed for
    AutoSIMD and sometimes caused registers to not be assigned properly. It
    was only used in one place, so the entire fuction is just being removed.
    
    Issue: #1066
    Signed-off-by: jimmyk <jimmyk@ca.ibm.com>

--- compiler/codegen/CodeGenRA.cpp
+++ compiler/codegen/CodeGenRA.cpp
@@ -409,15 +409,6 @@ OMR::CodeGenerator::prepareRegistersForAssignment()
          registerCursor->setFutureUseCount(registerCursor->getTotalUseCount());
       km = registerCursor->getKindAsMask();
 
-      /*
-       * Sometimes TR_VRF virtual registers need to be assigned even when there are no TR_VRF real registers
-       * This can occur when there is a full overlap between TR_VRF and TR_FPR real registers and they are all marked as TR_FPR
-       * isFPRUsedAsVRF is used to indicate if assignment is necessary
-       * if true, RA is told it may need to assign TR_VRF as well as TR_FPR registers if a TR_FPR real register is seen
-       */
-      if (registerCursor->getKind() == TR_FPR && self()->isFPRUsedAsVRF())
-         km |= TO_KIND_MASK(TR_VRF);
-
       if (!(foundKindsMask & km))
          foundKindsMask |= km;
       }

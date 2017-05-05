705e72b1966ab4cc6ee8421878dd128157fb2cab
Irwin D'Souza <dsouzai@ca.ibm.com>
2017-05-04 22:10:48 -0400

    Remove _evaluatingCompressionSequenceCounter
    With the commit to prevent lowering of trees under concurrent scavenge,
    _evaluatingCompressionSequenceCounter and associated methods to inc,
    dec, and check if the codegen is currently evaluating a compression
    sequence are no longer needed (this was originally needed in order to
    ensure a guarded load was generated instead of the compression
    sequence).
    
    Signed-off-by: Irwin D'Souza <dsouzai@ca.ibm.com>

--- compiler/z/codegen/OMRCodeGenerator.hpp
+++ compiler/z/codegen/OMRCodeGenerator.hpp
@@ -329,13 +329,6 @@ public:
    TR_BranchPreloadCallData _outlineCall;
    TR_BranchPreloadCallData _outlineArrayCall;
 
-   // TODO (GuardedStorage): This needs to be revisited. We need to avoid lowering compressedrefs trees if concurrent
-   // scavenge is enabled, rather than trying to pattern match and avoid evaluating them
-   int32_t _evaluatingCompressionSequenceCounter;
-   void incEvaluatingCompressionSequence();
-   void decEvaluatingCompressionSequence();
-   bool isEvaluatingCompressionSequence();
-
    TR::list<TR_BranchPreloadCallData*> *_callsForPreloadList;
 
    TR::list<TR_BranchPreloadCallData*> * getCallsForPreloadList() { return _callsForPreloadList; }

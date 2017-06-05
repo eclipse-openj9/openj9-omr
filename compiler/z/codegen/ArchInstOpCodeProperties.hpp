6447588679dabaa962d5748fa73331c806063691
Hadi Jooybar <hjooybar@ca.ibm.com>
2017-06-05 14:17:54 -0400

    Fix properties of VLVG instruction (z Systems)
    “VECTOR LOAD VR ELEMENT FROM GR” (VLVG instruction) stores a general register in a vector register.
    For double-word element size (mask = 3) this instruction uses all 64 bits of its source register.
    Therefore, it has to be marked as a 64bit instruction.
    Same logic applies to VLVGP instruction as well.
    
    Signed-off-by: Hadi Jooybar <hjooybar@ca.ibm.com>

--- compiler/z/codegen/ArchInstOpCodeProperties.hpp
+++ compiler/z/codegen/ArchInstOpCodeProperties.hpp
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * (c) Copyright IBM Corp. 2000, 2016
+ * (c) Copyright IBM Corp. 2000, 2017
  *
  *  This program and the accompanying materials are made available
  *  under the terms of the Eclipse Public License v1.0 and
@@ -6049,10 +6049,12 @@
       // VLVG
    S390OpProp_IsLoad |
    S390OpProp_UsesTarget |
-   S390OpProp_SetsOperand1,
+   S390OpProp_SetsOperand1 |
+   S390OpProp_Is64Bit,
 
       // VLVGP
-   S390OpProp_SetsOperand1,
+   S390OpProp_SetsOperand1 |
+   S390OpProp_Is64Bit,
 
       // VLL
    S390OpProp_IsLoad |

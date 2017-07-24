b45f91f887ea7c396f1723df2a608c8bd1c9ae12
Robert Young <rwy0717@gmail.com>
2017-07-24 19:57:06 -0400

    Improve the findlibelf package
    Add a short documentation string.
    Search for both libelf.h and elf.h.
    
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- cmake/modules/FindLibElf.cmake
+++ cmake/modules/FindLibElf.cmake
@@ -1,11 +1,31 @@
-find_path(LIBELF_INCLUDE_DIRS elf.h)
+# Find libelf
+# Will set:
+#  LIBELF_FOUND
+#  LIBELF_INCLUDE_DIRS
+#  LIBELF_LIBRARIES
+#  LIBELF_DEFINES
 
-find_library(LIBELF_LIBRARIES elf)
+find_path(ELF_H_INCLUDE_DIR elf.h)
+
+find_path(LIBELF_H_INCLUDE_DIR libelf.h)
+
+set(LIBELF_INCLUDE_DIRS
+	${ELF_H_INCLUDE_DIR}
+	${LIBELF_H_INCLUDE_DIR}
+)
+
+find_library(LIBELF_LIBRARY elf)
+
+set(LIBELF_LIBRARIES
+	${LIBELF_LIBRARY}
+)
+set (LIBELF_DEFINES "")
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(LibElf
 	DEFAULT_MSG
-	LIBELF_LIBRARIES
-	LIBELF_INCLUDE_DIRS
+	LIBELF_LIBRARY
+	ELF_H_INCLUDE_DIR
+	LIBELF_H_INCLUDE_DIR
 )

87422a51f07ae21f94111e8684627a3ae7359b42
Matthew Gaudet <magaudet@ca.ibm.com>
2017-11-17 12:01:30 -0500

    Add Tril test case for ineg opcode
    Signed-off-by: Matthew Gaudet <magaudet@ca.ibm.com>

---
+++ fvtest/compilertriltest/LogicalTest.cpp
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2017 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *******************************************************************************/
+
+
+#include "OpCodeTest.hpp"
+#include "default_compiler.hpp"
+
+int32_t ineg(int32_t l) {
+    return -l;
+}
+
+class Int32Logical : public TRTest::UnaryOpTest<int32_t> {}; 
+
+TEST_P(Int32Logical, UsingConst) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int32 (block (ireturn (%s (iconst %d) ) )))", param.opcode.c_str(), param.value);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler{trees};
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<int32_t (*)()>();
+    volatile auto exp = param.oracle(param.value);
+    volatile auto act = entry_point();
+    ASSERT_EQ(exp, act);
+}
+
+
+INSTANTIATE_TEST_CASE_P(LogicalTest, Int32Logical, ::testing::Combine(
+    ::testing::ValuesIn(TRTest::const_values<int32_t>()),
+    ::testing::Values(
+        std::make_tuple("ineg", ineg)
+        )));

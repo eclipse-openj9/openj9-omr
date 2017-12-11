rbanerjee/omr

Redesign the IL Validation process
This commit redesigns the ILValidator to improve the general IL
Validation process and lay the necessary groundwork for future
improvements.

Some of the major issues addressed are listed below:

1. Give more control over `when` and `what` we are validating against.

   At different points in the pipeline we can choose to Validate the IL
   against a certain subset of all the available rules.

   The state is passed to the aforementioned `validateIL` method, and depending
   on that `state` of Compilation, we decide which subset of the available Rules
   we should use.

2. Compartmentalize Rules

   The rules are categorized based on the “context” required to check whether
   they are valid or not.

   This gives us the following types:

     - Rules that require one to check the entire method
       (TR::ResolvedMethodSymbol) for a certain property.

     - Rules that only confine themselves to looking at a particular
       Block(TR::Block).

     - Rules that only check whether a particular node (TR::Node) conforms to
       a certain expectation.

3. Decoupling existing Rules

   A large part of which involves decoupling the `Soundness Checks`
   from the rest of the rules.
   Also split up complicated rules into more atomic chunks.

Also add a set of utility functions in order to write said ILValidationRules.

Related Issues: #1901, #566, #1971

Signed-off-by: Rwitaban (Ray) Banerjee <rayb@ca.ibm.com>

compiler/ras/ILValidationUtils.hpp
@@ -1,0 +1,158 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2017 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *******************************************************************************/
+
+#ifndef ILVALIDATION_UTILS_HPP
+#define ILVALIDATION_UTILS_HPP
+
+#include "compile/Compilation.hpp"             // for  Compilation, etc
+#include "infra/Assert.hpp"                    // for  TR_ASSERT
+#include "il/Node.hpp"                         // for  Node
+#include "infra/SideTable.hpp"                 // for  NodeSideTable
+
+namespace TR { class Compilation; }
+
+namespace TR {
+
+struct NodeState
+  {
+  TR::Node    *_node;
+  ncount_t     _futureReferenceCount;
+
+  NodeState(TR::Node *node):_node(node),_futureReferenceCount(node->getReferenceCount()){}
+  };
+
+/**
+ * This is like a NodeChecklist, but more compact.  Rather than track
+ * node global indexes, which can be sparse, this tracks local
+ * indexes, which are relatively dense.  Furthermore, the _basis field
+ * allows us not to waste space on nodes we saw in prior blocks.
+ */
+class LiveNodeWindow
+  {
+
+  NodeSideTable<NodeState> &_sideTable;
+  int32_t                   _basis;
+  TR_BitVector              _liveOffsets; // sideTable index = basis + offset
+
+  public:
+
+  LiveNodeWindow(NodeSideTable<NodeState> &sideTable, TR_Memory *memory);
+
+  bool contains(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     if (index < _basis)
+	return false;
+     else
+	return _liveOffsets.isSet(index - _basis);
+     }
+
+  void add(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     TR_ASSERT(index >= _basis, "Cannot mark node n%dn before basis %d live",
+               node->getGlobalIndex(), _basis);
+     _liveOffsets.set(index - _basis);
+     }
+
+  void remove(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     if (index >= _basis)
+	_liveOffsets.reset(index - _basis);
+     }
+
+  bool isEmpty()
+     {
+     return _liveOffsets.isEmpty();
+     }
+
+  TR::Node *anyLiveNode()
+     {
+     Iterator iter(*this);
+     return iter.currentNode();
+     }
+
+  void startNewWindow()
+     {
+     TR_ASSERT(_liveOffsets.isEmpty(),
+               "Cannot close LiveNodeWindow when there are still live nodes");
+     _basis = _sideTable.size();
+     }
+  public:
+
+  class Iterator
+     {
+     LiveNodeWindow      &_window;
+     TR_BitVectorIterator _bvi;
+     int32_t              _currentOffset; // -1 means iteration is past end
+
+     bool isPastEnd(){ return _currentOffset < 0; }
+
+     public:
+
+     Iterator(LiveNodeWindow &window):
+        _window(window),
+        _bvi(window._liveOffsets),
+        _currentOffset(0xdead /* anything non-negative */)
+	{
+	stepForward();
+	}
+
+     TR::Node *currentNode()
+	{
+	if (isPastEnd())
+	   return NULL;
+	else
+	   return _window._sideTable.getAt(_window._basis + _currentOffset)._node;
+	}
+
+     void stepForward()
+	{
+	TR_ASSERT(!isPastEnd(),
+                  "Can't stepForward a LiveNodeWindow::Iterator that's already past end");
+	if (_bvi.hasMoreElements())
+	   _currentOffset = _bvi.getNextElement();
+	else
+	   _currentOffset = -1;
+	}
+
+     public: // operators
+
+     void operator ++() { stepForward(); }
+     };
+
+  };
+
+
+bool isLoggingEnabled(TR::Compilation *comp);
+
+void updateNodeState(TR::Node *node, NodeSideTable<NodeState>  &nodeStates,
+                     LiveNodeWindow &liveNodes, TR::Compilation *comp);
+
+void checkCondition(TR::Node *node, bool condition, TR::Compilation *comp,
+                    const char *formatStr, ...);
+
+void printDiagnostic(TR::Compilation *comp, const char *formatStr, ...);
+void vprintDiagnostic(TR::Compilation *comp, const char *formatStr, va_list ap);
+
+}
+#endif // ILVALIDATION_UTIL_HPP

eclipse-openj9/openj9-omr

compiler/ras/ILValidationUtils.hpp
@@ -1,0 +1,155 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2017 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *******************************************************************************/
+
+#ifndef ILVALIDATION_UTILS_HPP
+#define ILVALIDATION_UTILS_HPP
+
+#include "compile/Compilation.hpp"
+#include "infra/Assert.hpp"
+#include "il/Node.hpp"
+#include "infra/SideTable.hpp"
+
+namespace TR { class Compilation; }
+
+namespace TR {
+
+struct NodeState
+  {
+  TR::Node    *_node;
+  ncount_t     _futureReferenceCount;
+
+  NodeState(TR::Node *node):_node(node),_futureReferenceCount(node->getReferenceCount()){}
+  };
+
+/**
+ * This is like a NodeChecklist, but more compact.  Rather than track
+ * node global indexes, which can be sparse, this tracks local
+ * indexes, which are relatively dense.  Furthermore, the _basis field
+ * allows us not to waste space on nodes we saw in prior blocks.
+ */
+class LiveNodeWindow
+  {
+
+  NodeSideTable<NodeState> &_sideTable;
+  int32_t                   _basis;
+  TR_BitVector              _liveOffsets; // sideTable index = basis + offset
+
+  public:
+
+  LiveNodeWindow(NodeSideTable<NodeState> &sideTable, TR_Memory *memory);
+
+  bool contains(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     if (index < _basis)
+	return false;
+     else
+	return _liveOffsets.isSet(index - _basis);
+     }
+
+  void add(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     TR_ASSERT(index >= _basis, "Cannot mark node n%dn before basis %d live",
+               node->getGlobalIndex(), _basis);
+     _liveOffsets.set(index - _basis);
+     }
+
+  void remove(TR::Node *node)
+     {
+     int32_t index = _sideTable.indexOf(node);
+     if (index >= _basis)
+	_liveOffsets.reset(index - _basis);
+     }
+
+  bool isEmpty()
+     {
+     return _liveOffsets.isEmpty();
+     }
+
+  TR::Node *anyLiveNode()
+     {
+     Iterator iter(*this);
+     return iter.currentNode();
+     }
+
+  void startNewWindow()
+     {
+     TR_ASSERT(_liveOffsets.isEmpty(),
+               "Cannot close LiveNodeWindow when there are still live nodes");
+     _basis = _sideTable.size();
+     }
+  public:
+
+  class Iterator
+     {
+     LiveNodeWindow      &_window;
+     TR_BitVectorIterator _bvi;
+     int32_t              _currentOffset; // -1 means iteration is past end
+
+     bool isPastEnd(){ return _currentOffset < 0; }
+
+     public:
+
+     Iterator(LiveNodeWindow &window):
+        _window(window),
+        _bvi(window._liveOffsets),
+        _currentOffset(0xdead /* anything non-negative */)
+	{
+	stepForward();
+	}
+
+     TR::Node *currentNode()
+	{
+	if (isPastEnd())
+	   return NULL;
+	else
+	   return _window._sideTable.getAt(_window._basis + _currentOffset)._node;
+	}
+
+     void stepForward()
+	{
+	TR_ASSERT(!isPastEnd(),
+                  "Can't stepForward a LiveNodeWindow::Iterator that's already past end");
+	if (_bvi.hasMoreElements())
+	   _currentOffset = _bvi.getNextElement();
+	else
+	   _currentOffset = -1;
+	}
+
+     public: // operators
+
+     void operator ++() { stepForward(); }
+     };
+
+  };
+
+
+bool isILValidationLoggingEnabled(TR::Compilation *comp);
+
+void checkILCondition(TR::Node *node, bool condition, TR::Compilation *comp,
+                      const char *formatStr, ...);
+
+void printILDiagnostic(TR::Compilation *comp, const char *formatStr, ...);
+void vprintILDiagnostic(TR::Compilation *comp, const char *formatStr, va_list ap);
+
+}
+#endif // ILVALIDATION_UTIL_HPP
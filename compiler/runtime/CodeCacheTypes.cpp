b4e581d520ed25ce8e2e3053ad59a0051744da85
Daryl Maier <maier@ca.ibm.com>
2017-12-22 21:33:17 -0500

    Add missing directory stem prefix to some #include directives
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/runtime/CodeCacheTypes.cpp
+++ compiler/runtime/CodeCacheTypes.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2017 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -19,7 +19,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
  *******************************************************************************/
 
-#include "CodeCacheTypes.hpp"
+#include "runtime/CodeCacheTypes.hpp"
 #include "runtime/CodeCacheManager.hpp"        // for TR::CodeCacheManager
 
 namespace OMR

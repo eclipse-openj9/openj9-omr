ca507201f9b56bc53d131211b49f763b816763f9
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2018-03-21 11:02:56 -0400

    Enable Windows stack protection (/GS flag)
    OpenJ9 PR: https://github.com/eclipse/openj9/pull/1494
    
    Signed-off-by: Theresa Mammarella <Theresa.T.Mammarella@ibm.com>

--- omrmakefiles/rules.win.mk
+++ omrmakefiles/rules.win.mk
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2015, 2015 IBM Corp. and others
+# Copyright (c) 2015, 2018 IBM Corp. and others
 # 
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -70,7 +70,6 @@ GLOBAL_CPPFLAGS+=-D_DLL
 ifeq ($(OMR_OPTIMIZE),1)
   COPTFLAGS=/Ox
 endif
-COPTFLAGS+=/GS- # /GS-: disable buffer security check (that is on by default)
 GLOBAL_CFLAGS+=$(COPTFLAGS)
 GLOBAL_CXXFLAGS+=$(COPTFLAGS)
 

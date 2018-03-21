a06b6bbfe8dd8525b218fba17900a6e4bb185a8d
Keith W. Campbell <keithc@ca.ibm.com>
2018-03-21 11:47:45 -0400

    Add missing/required forward declarations
    * if C1 and S1 are types in the global namespace,
      the forward declarations are required for consistency
    
    class C1;
    struct S1;
    class C2 {
    	friend class C1;
    	struct S1 *f;
    }
    
    * fix line-ends
    * add struct/union tags
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- gc/base/MemorySubSpace.hpp
+++ gc/base/MemorySubSpace.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2015 IBM Corp. and others
+ * Copyright (c) 1991, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,7 +20,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-
 #if !defined(MEMORYSUBSPACE_HPP_)
 #define MEMORYSUBSPACE_HPP_
 
@@ -34,6 +33,7 @@
 #include "MemorySpacesAPI.h"
 #include "ModronAssertions.h"
 
+class GC_MemorySubSpaceRegionIterator;
 class MM_AllocateDescription;
 class MM_AllocationContext;
 class MM_Collector;

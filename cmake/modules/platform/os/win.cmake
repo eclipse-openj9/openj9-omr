f0a918353c369b80f2bf55fa7fcf11a75f62a601
Pavel Samolysov <samolisov@gmail.com>
2018-04-11 13:35:10 +0300

    Add the OMR_OS_WINDOWS compile definition
    As it's discussed with @youngar in the comments to issue #2405,
    it would be good to start using macros prefixed with `OMR`.
    As more people use OMR, it would be nice if we didn't collide
    with commonly used macro names like `WINDOWS`.
    
    The JIT already uses `WINDOWS`, while the rest of the code would
    need to be updated.
    
    Constructions such as `#ifdef WIN32` can be replaced by
    `#if defined(OMR_OS_WINDOWS)`
    
    Constructions such as `#ifdef WIN64` can be replaced by
    `#if defined(OMR_OS_WINDOWS) && defined(OMR_ENV_DATA64)`
    
    Constructions such as `#if defined(WIN32) || defined(WIN64)`
    can be replaced by `#if defined(OMR_OS_WINDOWS)`
    
    The `OMR_OS_WINDOWS` compile definition is added to the CMake
    configuration as well as the autotools one.
    
    Issue: #2405
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- cmake/modules/platform/os/win.cmake
+++ cmake/modules/platform/os/win.cmake
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2017, 2017 IBM Corp. and others
+# Copyright (c) 2017, 2018 IBM Corp. and others
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,6 +20,7 @@
 #############################################################################
 
 list(APPEND OMR_PLATFORM_DEFINITIONS
+	-DOMR_OS_WINDOWS
 	-DWIN32
 	-D_CRT_SECURE_NO_WARNINGS
 	-DCRTAPI1=_cdecl

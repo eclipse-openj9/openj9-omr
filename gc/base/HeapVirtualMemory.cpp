10f785998dcc15f67ebfd3a29efdd76b2b1cfed0
Dmitri Pivkine <Dmitri_Pivkine@ca.ibm.com>
2018-04-26 10:49:51 -0400

    Remove unused heaptailpadding from GC Extensions
    This variable is not used and can be deleted
    
    Signed-off-by: Dmitri Pivkine <Dmitri_Pivkine@ca.ibm.com>

--- gc/base/HeapVirtualMemory.cpp
+++ gc/base/HeapVirtualMemory.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2017 IBM Corp. and others
+ * Copyright (c) 1991, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -74,7 +74,7 @@ MM_HeapVirtualMemory::initialize(MM_EnvironmentBase* env, uintptr_t size)
 	}
 
 	MM_GCExtensionsBase* extensions = env->getExtensions();
-	uintptr_t padding = extensions->heapTailPadding;
+	uintptr_t padding = 0;
 
 	uintptr_t effectiveHeapAlignment = _heapAlignment;
 	/* we need to ensure that we allocate the heap with region alignment since the region table requires that */

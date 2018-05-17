dnakamura/omr

Remove all occurrences of the WIN32_IBMC macro
`IBMC` is a macro usually defined for `XLC` or the `z/OS` system compiler, and
there isn't a Windows version of `XLC`. Also, the `WIN32_IBMC` macro is defined
neither in CMake nor in autotools. So, these are the reasons to remove
all occurrences of the macro at all.

Issue: #2405
Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

include_core/omrutil.h
@@ -143,7 +143,7 @@ uintptr_t findSmallestPrimeGreaterThanOrEqualTo(uintptr_t number);
  */
 uintptr_t getSupportedBiggestNumberByPrimeNumberHelper(void);
 
-#if defined(OMRWINDOWS) && !defined(WIN32_IBMC)
+#if defined(OMRWINDOWS)
 
 /* ---------------- omrgetdbghelp.c ---------------- */
 
@@ -166,7 +166,7 @@ uintptr_t omrgetdbghelp_getDLL(void);
 */
 void omrgetdbghelp_freeDLL(uintptr_t dbgHelpDLL);
 
-#endif  /* defined(OMRWINDOWS) && !defined(WIN32_IBMC) */
+#endif  /* defined(OMRWINDOWS) */
 
 
 /* ---------------- stricmp.c ---------------- */

eclipse-openj9/openj9-omr

include_core/omrutil.h
@@ -143,8 +143,7 @@ uintptr_t findSmallestPrimeGreaterThanOrEqualTo(uintptr_t number);
  */
 uintptr_t getSupportedBiggestNumberByPrimeNumberHelper(void);
 
-#if defined(OMR_OS_WINDOWS) && !defined(WIN32_IBMC)
-
+#if defined(OMR_OS_WINDOWS)
 /* ---------------- omrgetdbghelp.c ---------------- */
 
 /**
@@ -166,8 +165,7 @@ uintptr_t omrgetdbghelp_getDLL(void);
 */
 void omrgetdbghelp_freeDLL(uintptr_t dbgHelpDLL);
 
-#endif  /* defined(OMR_OS_WINDOWS) && !defined(WIN32_IBMC) */
-
+#endif  /* defined(OMR_OS_WINDOWS) */
 
 /* ---------------- stricmp.c ---------------- */
 
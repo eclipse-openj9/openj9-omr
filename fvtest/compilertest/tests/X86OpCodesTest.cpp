3df1a7962b4237143dbbfe2ea95ce3f231562ca3
Pavel Samolysov <samolisov@gmail.com>
2018-05-21 13:10:43 +0300

    Remove the unused math.h header from compilertest
    The header `math.h` is included in the source files
    `compilertest/*OptCodesTest.cpp` even though no defined math functions
    and macroses are used there and this increases compilation time and
    could be a reason of additional compilation errors. The commit removes
    the includes from the compilertest sources.
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- fvtest/compilertest/tests/X86OpCodesTest.cpp
+++ fvtest/compilertest/tests/X86OpCodesTest.cpp
@@ -19,7 +19,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "compile/Method.hpp"

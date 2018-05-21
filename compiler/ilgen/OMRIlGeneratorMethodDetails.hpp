57ac37cf814161cfa819824e5dc6850cc73d5b8b
Pavel Samolysov <samolisov@gmail.com>
2018-05-21 17:17:29 +0300

    Fix C4291 on compiler/ilgen/OMRILGeneratorMethodDetails.hpp
    The following warning was fixed:
    
    warning C4291:  'void *OMR::IlGeneratorMethodDetails::operator
    new(::size_t,TR::IlGeneratorMethodDetails *)': no matching operator
    delete found; memory will not be freed if initialization throws an exception
    
    compiler\ilgen/OMRIlGeneratorMethodDetails.hpp(73): note: see declaration of
    'OMR::IlGeneratorMethodDetails::operator new'
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- compiler/ilgen/OMRIlGeneratorMethodDetails.hpp
+++ compiler/ilgen/OMRIlGeneratorMethodDetails.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -72,6 +72,9 @@ protected:
 
    void *operator new(size_t size, TR::IlGeneratorMethodDetails *p){ return (void*) p; }
    void *operator new(size_t size, TR::IlGeneratorMethodDetails &p){ return (void*)&p; }
+   void operator delete(void *pMem, TR::IlGeneratorMethodDetails *p) {};
+   void operator delete(void *pMem, TR::IlGeneratorMethodDetails &p) {};
+   void operator delete(void *pMem, std::size_t size) { ::operator delete(pMem); };
 
    TR::IlVerifier     * _ilVerifier;
    };

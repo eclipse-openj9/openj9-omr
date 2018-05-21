ac05f92e8da9c85137b50bfbbed178dca180437f
Pavel Samolysov <samolisov@gmail.com>
2018-05-21 13:16:48 +0300

    Remove the unused unistd.h header from compilertest
    The header `unistd.h` provides access to the POSIX operating system API.
    The header is included in the sources of `compilertest/tests` even
    though no defined macros and functions are used there. The commit
    removes the includes from the `compilertest/tests` C++ files.
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- fvtest/compilertest/tests/LogFileTest.cpp
+++ fvtest/compilertest/tests/LogFileTest.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2016 IBM Corp. and others
+ * Copyright (c) 2016, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -26,7 +26,6 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <unistd.h>
 #include <sys/stat.h>
 
 #include "tests/OMRTestEnv.hpp"

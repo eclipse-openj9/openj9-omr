78d02ed46847e21c567189efba148e32ee729161
Andrew Young <youngar17@gmail.com>
2018-05-23 14:32:06 -0400

    Include missing omrcfg.h in VerboseHandler
    On my local cmake build, this causes a configuration error which
    eventually leads to missing symbols during linking.
    
    Signed-off-by: Andrew Young <youngar17@gmail.com>

--- gc/verbose/handler_standard/VerboseHandlerOutputStandard.cpp
+++ gc/verbose/handler_standard/VerboseHandlerOutputStandard.cpp
@@ -20,6 +20,8 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
+#include "omrcfg.h"
+
 #include "omrgcconsts.h"
 #include "gcutils.h"
 

8279656d20fb8a487f64abea5093871a646ef487
Nazim Uddin Bhuiyan <nazimudd@ualberta.ca>
2018-05-24 16:03:56 -0400

    Wrap omrtrace lib within build flags in OMR_Initialize_VM
    The corresponding wrapping was done in OMR_Shutdown_VM too. Not
    everything that wants to initialize/shutdown OMR components may
    want to initialize/shutdown the health center monitoring tools.
    
    Also fixed a potential bug where the port library's omrtty_printf
    member was being used before the port library was initialized.
    
    Signed-off-by: Nazim Uddin Bhuiyan <nazimudd@ualberta.ca>

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2017, 2017 IBM Corp. and others
+# Copyright (c) 2017, 2018 IBM Corp. and others
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -42,6 +42,7 @@ set(OMR_PORT ON CACHE BOOL "Enable portability library")
 set(OMR_TEST_COMPILER OFF CACHE BOOL "Enable building the test compiler")
 set(OMR_THREAD ON CACHE BOOL "Enable thread library")
 set(OMR_TOOLS ON CACHE BOOL "Enable the native build tools")
+set(OMR_RAS_TDF_TRACE ON CACHE BOOL "Enable trace engine")
 
 ###
 ### Tooling
@@ -107,8 +108,6 @@ set(OMR_GC_VLHGC OFF CACHE BOOL "TODO: Document")
 set(OMR_INTERP_HAS_SEMAPHORES ON CACHE BOOL "TODO: Document")
 set(OMR_INTERP_COMPRESSED_OBJECT_HEADER OFF CACHE BOOL "TODO: Document")
 set(OMR_INTERP_SMALL_MONITOR_SLOT OFF CACHE BOOL "TODO: Document")
-
-set(OMR_RAS_TDF_TRACE ON CACHE BOOL "TODO: Document")
 
 set(OMR_THR_ADAPTIVE_SPIN ON CACHE BOOL "TODO: Document")
 set(OMR_THR_JLM ON CACHE BOOL "TODO: Document")

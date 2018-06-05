c4ba32e8988f4af64c0c66161e135b07d69ca6c3
Marius Pirvu <mpirvu@ca.ibm.com>
2018-06-05 11:15:01 -0400

    Add new memory object types for stats purposes
    This commit adds two new memory object types that will be used
    in downstream projects.
    
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- compiler/env/TRMemory.hpp
+++ compiler/env/TRMemory.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -448,6 +448,9 @@ public:
       RegionAllocation,
 
       Debug,
+
+      ClientSessionData,
+      ROMClass,
 
       NumObjectTypes,
       // If adding new object types above, add the corresponding names

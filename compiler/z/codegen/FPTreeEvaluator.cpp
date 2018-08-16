1e2b35bc002b1c0e8ecd44a1da349d6b45fe9bc7
Daniel Hong <daniel.hong@live.com>
2018-08-16 16:06:00 -0400

    Use proper API to recreate a duplicate memory reference
    Use proper API to recreate a duplicate memory reference.
    
    Signed-off-by: Daniel Hong <daniel.hong@live.com>

--- compiler/z/codegen/FPTreeEvaluator.cpp
+++ compiler/z/codegen/FPTreeEvaluator.cpp
@@ -1238,7 +1238,7 @@ OMR::Z::TreeEvaluator::fbits2iEvaluator(TR::Node * node, TR::CodeGenerator * cg)
          TR::MemoryReference * tempMR1 = generateS390MemoryReference(node, f2iSR, cg);
          generateRXInstruction(cg, TR::InstOpCode::STE, node, sourceReg, tempMR1);
 
-         TR::MemoryReference * tempMR2 = generateS390MemoryReference(node, f2iSR, cg);
+         TR::MemoryReference * tempMR2 = generateS390MemoryReference(*tempMR1, 0, cg);
          targetReg = genericLoadHelper<32, 32, MemReg>(node, cg, tempMR2, NULL, false, true);
          }
       }

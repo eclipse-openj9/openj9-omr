eclipse-openj9/openj9-omr

commit 564fb2516755d332a0cdd053dc9fe8defc24689f

Deprecate getBucketPlusIndexRegisters
No bits are ever set in this bitvector, so fold any code related to
querying bits inside this bitvector as false.

Signed-off-by: Filip Jeremic <fjeremic@ca.ibm.com>

compiler/z/codegen/OMRTreeEvaluator.cpp
@@ -7101,11 +7101,10 @@ OMR::Z::TreeEvaluator::aiaddEvaluator(TR::Node * node, TR::CodeGenerator * cg)
 
    aiaddMR->populateAddTree(node, cg);
    aiaddMR->eliminateNegativeDisplacement(node, cg);
-   bool skipFinalLA = false;
+
    // this is partial extract from enforceDisplacementLimit in order to remove the need for the final LA in some cases
    if (aiaddMR->getOffset() >= MAXLONGDISP && aiaddMR->getOffset() <= TR::getMaxSigned<TR::Int32>() && TR::Compiler->target.is32Bit())
       {
-      skipFinalLA = true;
       TR::MemoryReference *tempMR = generateS390MemoryReference(aiaddMR->getBaseRegister(), aiaddMR->getIndexRegister(), 0, cg);
       tempMR->setSymbolReference(aiaddMR->getSymbolReference());
       generateRXInstruction(cg, TR::InstOpCode::LA, node, targetRegister, tempMR);
@@ -7141,23 +7140,6 @@ OMR::Z::TreeEvaluator::aiaddEvaluator(TR::Node * node, TR::CodeGenerator * cg)
          {
          targetRegister->setContainsInternalPointer();
          targetRegister->setPinningArrayPointer(firstChild->getRegister()->getPinningArrayPointer());
-         }
-      }
-
-
-   if (!skipFinalLA)
-      {
-      TR::Instruction *laInst = generateRXInstruction(cg, TR::InstOpCode::LA, node, targetRegister, aiaddMR);
-      TR::Register *baseReg = aiaddMR->getBaseRegister();
-      if (aiaddMR->getOffset() == 0 &&
-          aiaddMR->getIndexRegister() == NULL &&
-          baseReg &&
-          cg->getBucketPlusIndexRegisters().ValueAt(baseReg->getIndex()))
-         {
-         aiaddMR->setBucketBaseRegMemRef();
-         if (cg->traceBCDCodeGen())
-            traceMsg(comp,"\tfound baseReg %s (idx=%d) for node %s (%p) in BucketPlusIndexRegisters setBucketBaseRegMemRef on LA inst %p memRef\n",
-               cg->getDebug()->getName(baseReg),baseReg->getIndex(),node->getOpCode().getName(),node,laInst);
          }
       }
 

compiler/z/codegen/OMRCodeGenerator.cpp
@@ -547,7 +547,6 @@ OMR::Z::CodeGenerator::CodeGenerator()
      _nodeAddressOfCachedStatic(NULL),
      _ccInstruction(NULL),
      _previouslyAssignedTo(self()->comp()->allocator("LocalRA")),
-     _bucketPlusIndexRegisters(self()->comp()->allocator()),
      _currentDEPEND(NULL)
    {
    TR::Compilation *comp = self()->comp();

compiler/z/codegen/OMRCodeGenerator.hpp
@@ -225,9 +225,7 @@ public:
    bool shouldValueBeInACommonedNode(int64_t value);
    int64_t getLargestNegConstThatMustBeMaterialized() {return ((-1ll) << 31) - 1;}   // min 32bit signed integer minus 1
    int64_t getSmallestPosConstThatMustBeMaterialized() {return ((int64_t)0x000000007FFFFFFF) + 1;}   // max 32bit signed integer plus 1
-
-   TR::SparseBitVector & getBucketPlusIndexRegisters()  { return _bucketPlusIndexRegisters; }
-
+   
    // For hanging multiple loads from register symbols onto one common DEPEND
    TR::Instruction *getCurrentDEPEND() {return _currentDEPEND; }
    void setCurrentDEPEND(TR::Instruction *instr) { _currentDEPEND=instr; }
@@ -965,7 +963,6 @@ protected:
 
    protected:
 
-   TR::SparseBitVector _bucketPlusIndexRegisters;
    TR::Instruction *_currentDEPEND;
    };
 

compiler/z/codegen/OMRMemoryReference.hpp
@@ -83,7 +83,6 @@ namespace TR { class UnresolvedDataSnippet; }
 * this happens during doInstructionSelection when we don't have stack frame, so we need to update the memory reference during binary generation */
 #define MemRefCreatedDuringInstructionSelection 0x1000000
 #define TR_S390MemRef_ForceFirstTimeFolding     0x2000000
-#define TR_BucketBaseRegMemRef                  0x8000000
 
 namespace OMR
 {
@@ -326,9 +325,6 @@ void setConstantDataSnippet() {_flags.set(MemRef_ConstantDataSnippet);}
 
 bool isMemRefMustNotSpill()   {return _flags.testAny(MemRefMustNotSpill);}
 void setMemRefMustNotSpill()  {_flags.set(MemRefMustNotSpill);}
-
-bool isBucketBaseRegMemRef()  {return _flags.testAny(TR_BucketBaseRegMemRef);}
-void setBucketBaseRegMemRef() {_flags.set(TR_BucketBaseRegMemRef);}
 
 bool ZeroBasePtr_EvaluateSubtree(TR::Node * subTree, TR::CodeGenerator * cg, MemoryReference * mr);
 

compiler/z/codegen/S390Peephole.cpp
@@ -705,7 +705,6 @@ TR_S390PostRAPeephole::LAReduction()
          }
 
       if (mr &&
-          mr->isBucketBaseRegMemRef() &&
           mr->getOffset() == 0 &&
           laBaseReg && laTargetReg &&
           laBaseReg == laTargetReg &&

commit de0758f53a5b14b89a925168664c223560016df5

Do not skip generation of LA in aiadd evaluator
When evaluating the following tree we currently have a bug where we do
not generate the actual add because the displacement is too large to be
handled by long displacement instructions:

```
aiadd
  aconst 0x784b8500
  iconst 28
```

This is specific to 31-bit. The reason is because we do not generate
the LA instruction to add the 28 to the previous memory reference. This
bug was introduced in commit 564fb25 as part of #2882. In fact the code
that was removed here is actually an optimization on 31-bit that does
not exist for 64-bit but it really should.

This will be addressed in a separate Issue which will be opened as part
of the PR containing this commit.

Signed-off-by: Filip Jeremic <fjeremic@ca.ibm.com>

compiler/z/codegen/OMRTreeEvaluator.cpp
@@ -7101,19 +7101,7 @@ OMR::Z::TreeEvaluator::aiaddEvaluator(TR::Node * node, TR::CodeGenerator * cg)
 
    aiaddMR->populateAddTree(node, cg);
    aiaddMR->eliminateNegativeDisplacement(node, cg);
-
-   // this is partial extract from enforceDisplacementLimit in order to remove the need for the final LA in some cases
-   if (aiaddMR->getOffset() >= MAXLONGDISP && aiaddMR->getOffset() <= TR::getMaxSigned<TR::Int32>() && TR::Compiler->target.is32Bit())
-      {
-      TR::MemoryReference *tempMR = generateS390MemoryReference(aiaddMR->getBaseRegister(), aiaddMR->getIndexRegister(), 0, cg);
-      tempMR->setSymbolReference(aiaddMR->getSymbolReference());
-      generateRXInstruction(cg, TR::InstOpCode::LA, node, targetRegister, tempMR);
-      generateS390ImmOp(cg, TR::InstOpCode::getAddOpCode(), node, targetRegister, targetRegister, (int32_t)aiaddMR->getOffset());
-      }
-   else
-      {
-      aiaddMR->enforceDisplacementLimit(node, cg, NULL);
-      }
+   aiaddMR->enforceDisplacementLimit(node, cg, NULL);
 
    if (node->getOpCodeValue() == TR::aiadd && node->isInternalPointer())
       {
@@ -7143,6 +7131,7 @@ OMR::Z::TreeEvaluator::aiaddEvaluator(TR::Node * node, TR::CodeGenerator * cg)
          }
       }
 
+   generateRXInstruction(cg, TR::InstOpCode::LA, node, targetRegister, aiaddMR);
    node->setRegister(targetRegister);
 
    return targetRegister;
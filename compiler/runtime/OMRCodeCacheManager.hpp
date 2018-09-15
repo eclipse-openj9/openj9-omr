0xdaryl/omr

Move allocation of CodeCacheHashEntrySlab to CodeCache initialization
Currently, the initial CodeCacheHashEntrySlab is allocated before the
CodeCache object is allocated and then passed in as a parameter to the
CodeCache object initialization.  This is done mainly for the reasons
discussed in Issue #2470.

This commit moves the allocation of the CodeCacheHashEntrySlab object
and the first slab to the CodeCache::initialize() function.  Handling
of allocation failures has also been adjusted to accommodate the new
location to ensure memory is reclaimed.

This change is necessary to consolidate trampoline artifact creation
to a single location to facilitate further refactoring in this area.

Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/runtime/OMRCodeCacheManager.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -144,8 +144,7 @@ public:
    void  freeMemory(void *memoryToFree);
 
    TR::CodeCache * allocateCodeCacheObject(TR::CodeCacheMemorySegment *codeCacheSegment,
-                                           size_t codeCacheSizeAllocated,
-                                           CodeCacheHashEntrySlab *hashEntrySlab);
+                                           size_t codeCacheSizeAllocated);
    void * chooseCacheStartAddress(size_t repositorySize);
 
    TR::CodeCache * getFirstCodeCache()            { return _codeCacheList._head; }

eclipse-openj9/openj9-omr

compiler/runtime/OMRCodeCacheManager.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -144,8 +144,7 @@ public:
    void  freeMemory(void *memoryToFree);
 
    TR::CodeCache * allocateCodeCacheObject(TR::CodeCacheMemorySegment *codeCacheSegment,
-                                           size_t codeCacheSizeAllocated,
-                                           CodeCacheHashEntrySlab *hashEntrySlab);
+                                           size_t codeCacheSize);
    void * chooseCacheStartAddress(size_t repositorySize);
 
    TR::CodeCache * getFirstCodeCache()            { return _codeCacheList._head; }
86978a2a65351174b9af58a9b114be21045452fd
Daryl Maier <maier@ca.ibm.com>
2018-08-06 14:51:12 -0400

    Move allocation of CodeCacheHashEntrySlab to CodeCache initialization
    Currently, the initial CodeCacheHashEntrySlab is allocated before the
    CodeCache object is allocated and then passed in as a parameter to the
    CodeCache object initialization.  This is done mainly for the reasons
    discussed in Issue #2470.
    
    This commit moves the allocation of the CodeCacheHashEntrySlab object
    and the first slab to the CodeCache::initialize() function.  Handling
    of allocation failures has also been adjusted to accommodate the new
    location to ensure memory is reclaimed.
    
    This change is necessary to consolidate trampoline artifact creation
    to a single location to facilitate further refactoring in this area.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/runtime/OMRCodeCache.cpp
+++ compiler/runtime/OMRCodeCache.cpp
@@ -1840,8 +1840,8 @@ OMR::CodeCache::reserveNTrampolines(int64_t n)
 //
 TR::CodeCache *
 OMR::CodeCache::allocate(TR::CodeCacheManager *manager,
-                       size_t segmentSize,
-                       int32_t reservingCompThreadID)
+                         size_t segmentSize,
+                         int32_t reservingCompThreadID)
    {
    TR::CodeCacheConfig & config = manager->codeCacheConfig();
    bool verboseCodeCache = config.verboseCodeCache();
@@ -1851,40 +1851,29 @@ OMR::CodeCache::allocate(TR::CodeCacheManager *manager,
 
    if (codeCacheSegment)
       {
-      // allocate hash entry slab now, instead of after codeCache allocation as its less expensive and easier to
-      // fully unroll
-      CodeCacheHashEntrySlab *hashEntrySlab = CodeCacheHashEntrySlab::allocate(manager, config.codeCacheHashEntryAllocatorSlabSize());
-      if (hashEntrySlab)
+      TR::CodeCache *codeCache = manager->allocateCodeCacheObject(codeCacheSegment,
+                                                                  codeCacheSizeAllocated);
+
+      if (codeCache)
          {
-         uint8_t *start = NULL;
-         TR::CodeCache *codeCache = manager->allocateCodeCacheObject(codeCacheSegment,
-                                                                     codeCacheSizeAllocated,
-                                                                     hashEntrySlab);
-
-         if (codeCache)
+         // If we wanted to reserve this code cache, then mark it as reserved now
+         if (reservingCompThreadID >= -1)
             {
-            // If we wanted to reserve this code cache, then mark it as reserved now
-            if (reservingCompThreadID >= -1)
-               {
-               codeCache->reserve(reservingCompThreadID);
-               }
-
-            // Add it to our list of code caches
-            manager->addCodeCache(codeCache);
-
-            if (verboseCodeCache)
-               {
-               TR_VerboseLog::writeLineLocked(TR_Vlog_CODECACHE, "CodeCache allocated %p @ " POINTER_PRINTF_FORMAT "-" POINTER_PRINTF_FORMAT " HelperBase:" POINTER_PRINTF_FORMAT, codeCache, codeCache->getCodeBase(), codeCache->getCodeTop(), codeCache->_helperBase);
-               }
-
-            return codeCache;
+            codeCache->reserve(reservingCompThreadID);
             }
 
-         // failed to allocate code cache so need to free hash entry slab
-         hashEntrySlab->free(manager);
+         // Add it to our list of code caches
+         manager->addCodeCache(codeCache);
+
+         if (verboseCodeCache)
+            {
+            TR_VerboseLog::writeLineLocked(TR_Vlog_CODECACHE, "CodeCache allocated %p @ " POINTER_PRINTF_FORMAT "-" POINTER_PRINTF_FORMAT " HelperBase:" POINTER_PRINTF_FORMAT, codeCache, codeCache->getCodeBase(), codeCache->getCodeTop(), codeCache->_helperBase);
+            }
+
+         return codeCache;
          }
 
-      // failed to allocate hash entry slab so need to free code cache segment
+      // Free code cache segment
       if (manager->usingRepository())
          {
          // return back the portion we carved
--- compiler/runtime/OMRCodeCacheManager.hpp
+++ compiler/runtime/OMRCodeCacheManager.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -144,8 +144,7 @@ public:
    void  freeMemory(void *memoryToFree);
 
    TR::CodeCache * allocateCodeCacheObject(TR::CodeCacheMemorySegment *codeCacheSegment,
-                                           size_t codeCacheSizeAllocated,
-                                           CodeCacheHashEntrySlab *hashEntrySlab);
+                                           size_t codeCacheSize);
    void * chooseCacheStartAddress(size_t repositorySize);
 
    TR::CodeCache * getFirstCodeCache()            { return _codeCacheList._head; }

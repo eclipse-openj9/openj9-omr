d18e9b3480a2460db3de4176f98d5bd802f762bf
Pavel Samolysov <samolisov@gmail.com>
2018-11-26 14:17:21 +0300

    Add OMR::ARM64::Machine::getARM64RealRegister()
    The wrapper `OMR::ARM64::Machine::getARM64RealRegister()` is added to
    bring backward compatibility for the downstream projects.
    
    Issue: #2645
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- compiler/aarch64/codegen/OMRMachine.hpp
+++ compiler/aarch64/codegen/OMRMachine.hpp
@@ -67,6 +67,16 @@ public:
    Machine(TR::CodeGenerator *cg);
 
    /**
+    * @brief This method is the wrapper for \code getRealRegister.
+    * @param[in] regNum : register number
+    * @return RealRegister for specified register number
+    */
+   TR::RealRegister *getARM64RealRegister(TR::RealRegister::RegNum regNum)
+      {
+      return _registerFile[regNum];
+      }
+
+   /**
     * @brief Converts RegNum to RealRegister
     * @param[in] regNum : register number
     * @return RealRegister for specified register number

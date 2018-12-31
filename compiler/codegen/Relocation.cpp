55576a383724e3779b9ff6b54a284a43987673ba
Younes Manton <ymanton@ca.ibm.com>
2018-12-31 09:33:03 -0800

    Introduce a direct method call relocation
    This patch introduces a new relocation type,
    TR_MethodCallAddress, which is intended to
    facilitate the relocation of direct calls.
    
    The relocation is currently only used by the
    x86 codegen.
    
    Signed-off-by: Younes Manton <ymanton@ca.ibm.com>

--- compiler/codegen/Relocation.cpp
+++ compiler/codegen/Relocation.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -460,6 +460,7 @@ const char *TR::ExternalRelocation::_externalRelocationTargetKindNames[TR_NumExt
    "TR_ValidateMethodFromSingleAbstractImplementer (96)",
    "TR_ValidateImproperInterfaceMethodFromCP (97)",
    "TR_SymbolFromManager (98)",
+   "TR_MethodCallAddress (99)",
    };
 
 uintptr_t TR::ExternalRelocation::_globalValueList[TR_NumGlobalValueItems] =
--- compiler/x/codegen/X86BinaryEncoding.cpp
+++ compiler/x/codegen/X86BinaryEncoding.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -1093,6 +1093,11 @@ TR::X86ImmSymInstruction::addMetaDataForCodeAddress(uint8_t *cursor)
                         getNode() ? (uint8_t *)(intptr_t)getNode()->getInlinedSiteIndex() : (uint8_t *)-1,
                         (TR_ExternalRelocationTargetKind) reloTypes [rType], cg()),
                         __FILE__, __LINE__, getNode());
+                  }
+               else if (resolvedMethod)
+                  {
+                  cg()->addProjectSpecializedRelocation(cursor, (uint8_t *)getSymbolReference()->getMethodAddress(), NULL, TR_MethodCallAddress,
+                                         __FILE__, __LINE__, getNode());
                   }
                else
                   {

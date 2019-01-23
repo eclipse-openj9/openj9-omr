10c8d455712a1ee1f81661a28aa9af032c5e48cd
Leonardo Banderali <leonardo2718@protonmail.com>
2019-01-23 15:54:21 -0500

    Don't delete TOC when JIT shuts down on PPC
    When the compiler is shutdown and then re-initialized in the same
    process, it expects to be able to reuse the same TOC as was previously
    used. As such, it cannot be deleted when the JIT is shutdown. This
    commit removes the code that does so from the Test Compiler, matching
    the behaviour of JitBuilder.
    
    Signed-off-by: Leonardo Banderali <leonardo2718@protonmail.com>

--- fvtest/compilertest/control/TestJit.cpp
+++ fvtest/compilertest/control/TestJit.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -203,11 +203,6 @@ shutdownJit()
 
    TR::CodeCacheManager &codeCacheManager = fe->codeCacheManager();
    codeCacheManager.destroy();
-#if defined(TR_TARGET_POWER)
-   delete fe->getPersistentInfo()->getPersistentTOC();
-   fe->getPersistentInfo()->setPersistentTOC(NULL);
-#endif
-
    }
 
 extern "C"

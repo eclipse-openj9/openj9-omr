8fde906beeffcf73b0c393d6978961ad14a04af0
Daryl Maier <maier@ca.ibm.com>
2019-01-24 14:05:47 -0500

    Introduce empty CPU extension classes for ARM and AArch64
    To prevent downstream project breakages, introduce empty extensions
    to the CPU hierarchy for ARM and AArch64.  Once merged, downstream
    projects can make changes to their build systems to accommodate the new
    files before new CPU functionality is added in #3479.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- jitbuilder/build/files/target/aarch64.mk
+++ jitbuilder/build/files/target/aarch64.mk
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2018, 2018 IBM Corp. and others
+# Copyright (c) 2018, 2019 IBM Corp. and others
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -43,7 +43,8 @@ JIT_PRODUCT_BACKEND_SOURCES+= \
     $(JIT_OMR_DIRTY_DIR)/aarch64/codegen/OpBinary.cpp \
     $(JIT_OMR_DIRTY_DIR)/aarch64/codegen/UnaryEvaluator.cpp
 
-#environement files
+#environment files
 
 JIT_PRODUCT_BACKEND_SOURCES+= \
+    $(JIT_OMR_DIRTY_DIR)/aarch64/env/OMRCPU.cpp \
     $(JIT_OMR_DIRTY_DIR)/aarch64/env/OMRDebugEnv.cpp

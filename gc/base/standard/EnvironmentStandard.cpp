ec3fe21a5f9487420811677bfc4497807b137149
Aleksandar Micic <amicic@ca.ibm.com>
2019-04-03 16:25:44 -0400

    Thread final/non-final Release Caches
    threadFinalReleaseCaches method is generalized to be used in a middle of
    a Scavenger cycle, when we do not want to flush Copy Cache Remainders,
    since they can be re-used later.
    
    Additional parameter is introduced to control, if we want to be final
    release (when remainders are abandoned) or not.
    
    At all existing call sites we set the parameter to true and we used to
    abandon them always, what makes this a non-functional change. Future
    users will set the parameter to false when needed.
    
    Signed-off-by: Aleksandar Micic <amicic@ca.ibm.com>

--- gc/base/standard/EnvironmentStandard.cpp
+++ gc/base/standard/EnvironmentStandard.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2018 IBM Corp. and others
+ * Copyright (c) 1991, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -99,7 +99,7 @@ MM_EnvironmentStandard::flushGCCaches()
 	if (getExtensions()->concurrentScavenger) {
 		if (MUTATOR_THREAD == getThreadType()) {
 			if (NULL != getExtensions()->scavenger) {
-				getExtensions()->scavenger->threadFinalReleaseCaches(this);
+				getExtensions()->scavenger->threadReleaseCaches(this, true);
 			}
 		}
 	}

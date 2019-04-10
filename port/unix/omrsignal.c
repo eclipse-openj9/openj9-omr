3db5604dfd9e972c024e23e9d8da8b4ca996f75c
Babneet Singh <sbabneet@ca.ibm.com>
2019-04-10 08:18:47 -0700

    Re-purpose reserved port library signal flags to support new signals
    1) SIGRESERVED8 -> SIGPIPE
    2) SIGRESERVED9 -> SIGALRM
    3) SIGRESERVED16 -> SIGCHLD
    4) SIGRESERVED17 -> SIGALRM
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- port/unix/omrsignal.c
+++ port/unix/omrsignal.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2018 IBM Corp. and others
+ * Copyright (c) 1991, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -201,7 +201,11 @@ static struct {
 	{OMRPORT_SIG_FLAG_SIGINT, SIGINT},
 	{OMRPORT_SIG_FLAG_SIGHUP, SIGHUP},
 	{OMRPORT_SIG_FLAG_SIGCONT, SIGCONT},
-	{OMRPORT_SIG_FLAG_SIGWINCH, SIGWINCH}
+	{OMRPORT_SIG_FLAG_SIGWINCH, SIGWINCH},
+	{OMRPORT_SIG_FLAG_SIGPIPE, SIGPIPE},
+	{OMRPORT_SIG_FLAG_SIGALRM, SIGALRM},
+	{OMRPORT_SIG_FLAG_SIGCHLD, SIGCHLD},
+	{OMRPORT_SIG_FLAG_SIGTSTP, SIGTSTP}
 #if defined(AIXPPC)
 	, {OMRPORT_SIG_FLAG_SIGRECONFIG, SIGRECONFIG}
 #endif

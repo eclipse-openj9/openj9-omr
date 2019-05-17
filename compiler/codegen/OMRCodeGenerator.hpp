aa2e1b5ea2d1f9ccb43f497917c8c80c582090c1
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-05-17 13:37:20 -0400

    Remove CodeGenerator::_nodesSpineCheckedList from OMR
    OMR::CodeGenerator has field _nodesSpineCheckedList that has only
    relevance to OpenJ9. This commit remove the field from OMR.
    
    Issue: #1895
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- compiler/codegen/OMRCodeGenerator.hpp
+++ compiler/codegen/OMRCodeGenerator.hpp
@@ -1886,7 +1886,7 @@ class OMR_EXTENSIBLE CodeGenerator
    TR::list<TR::Register*> *_spilledRegisterList;
    TR::list<OMR::RegisterUsage*> *_referencedRegistersList;
    int32_t _currentPathDepth;
-   TR::list<TR::Node*> _nodesSpineCheckedList;
+   
 
    TR::list<TR_Pair<TR_ResolvedMethod, TR::Instruction> *> _jniCallSites; // list of instrutions representing direct jni call sites
 

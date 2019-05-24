dbfd0f17957757b5b65acdaed6057702f8548280
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2019-05-23 23:57:16 -0400

    Fix struct definition to correctly name type
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- port/aix/omrintrospect.h
+++ port/aix/omrintrospect.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2015 IBM Corp. and others
+ * Copyright (c) 1991, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -50,7 +50,7 @@ typedef ucontext_t thread_context;
 typedef struct AIXFunctionEpilogue {
 	uint32_t nullWord;
 	struct tbtable_short tracebackTable;
-};
+} AIXFunctionEpilogue;
 
 typedef struct AIXStackFrame {
 	struct AIXStackFrame *link;

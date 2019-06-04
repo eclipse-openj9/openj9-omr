31a744d7208868027b274feca5d01b9433b47858
Andrew Craik <ajcraik@ca.ibm.com>
2019-06-04 09:12:58 -0400

    Improve compactNULLCHKs
    This change does two things: (1) when we find a NULLCHK with a first
    child that has reference count 1 - turn that node into a PassThrough to
    provide an opportunity for something more useful to move up under the
    NULLCHK and (2) allow compactNULLCKs to cross block boundaries when the
    two blocks are in treetop order and are in a single predecessor to
    single successor relationship (eg the break is due to exception edges or
    similar.
    
    Signed-off-by: Andrew Craik <ajcraik@ca.ibm.com>

--- compiler/optimizer/OMRSimplifierHandlers.cpp
+++ compiler/optimizer/OMRSimplifierHandlers.cpp
@@ -16423,6 +16423,15 @@ TR::Node *nullchkSimplifier(TR::Node * node, TR::Block * block, TR::Simplifier *
                 s->_blockRemoved |= cfg->removeEdge(*edge);
              }
           }
+       else if (node->getOpCodeValue() == TR::NULLCHK
+                && !node->getFirstChild()->getOpCode().isLikeDef()
+                && node->getFirstChild()->exceptionsRaised() == 0
+                && node->getFirstChild()->getReferenceCount() == 1
+                && node->getFirstChild()->getNumChildren() == 1
+                && performTransformation(s->comp(), "%sNULLCHK passthrough simplification on n%dn\n", s->optDetailString(), node->getGlobalIndex()))
+          {
+          TR::Node::recreate(node->getFirstChild(), TR::PassThrough);
+          }
        }
    return node;
    }

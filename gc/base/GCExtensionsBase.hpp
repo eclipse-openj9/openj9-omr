7af9a62676ec193a0771ee6162f2e1706772cd8c
cedrichansen <cedric.hansen@ibm.com>
2019-06-18 11:32:46 -0400

    Disable dark matter compact for concurrent sweep
    - Disabled dark matter compaction when concurrent sweep is enabled, due to concurrent sweep reporting innacurate dark matter info
    - Doubled default value for `darkMatterCompactThreshold` so that it doesn't trigger unless specified by command line option.
    - Refer to https://github.com/eclipse/omr/pull/3939 for description of the dark matter compact metrics
    
    Signed-off-by: Cedric Hansen <cedric.hansen@ibm.com>

--- gc/base/GCExtensionsBase.hpp
+++ gc/base/GCExtensionsBase.hpp
@@ -1403,7 +1403,7 @@ public:
 		, heapAlignment(HEAP_ALIGNMENT)
 		, absoluteMinimumOldSubSpaceSize(MINIMUM_OLD_SPACE_SIZE)
 		, absoluteMinimumNewSubSpaceSize(MINIMUM_NEW_SPACE_SIZE)
-		, darkMatterCompactThreshold((float)0.20)
+		, darkMatterCompactThreshold((float)0.40)
 		, parSweepChunkSize(0)
 		, heapExpansionMinimumSize(1024 * 1024)
 		, heapExpansionMaximumSize(0)

58b357712a5e32ba98bfbca00eda969359f43aa1
Babneet Singh <sbabneet@ca.ibm.com>
2019-07-09 15:56:33 -0400

    Update monitor_wait_original to use the MCS lock
    monitor_wait_original is an exit point to release the MCS lock. The MCS
    lock needs to be released before waiting.
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- thread/common/omrthread.c
+++ thread/common/omrthread.c
@@ -4460,6 +4460,9 @@ monitor_wait_original(omrthread_t self, omrthread_monitor_t monitor,
 	uintptr_t intrMask = 0;
 	uintptr_t intrFlags = 0;
 	uintptr_t timedOut = 0;
+#if defined(OMR_THR_MCS_LOCKS)
+	omrthread_t nextThread = NULL;
+#endif /* defined(OMR_THR_MCS_LOCKS) */
 
 	ASSERT(monitor);
 	ASSERT(FREE_TAG != monitor->count);
@@ -4523,8 +4526,14 @@ monitor_wait_original(omrthread_t self, omrthread_monitor_t monitor,
 	monitor->owner = NULL;
 	monitor->count = 0;
 
-#ifdef OMR_THR_THREE_TIER_LOCKING
+#if defined(OMR_THR_THREE_TIER_LOCKING)
 	MONITOR_LOCK(monitor, CALLER_MONITOR_WAIT);
+#if defined(OMR_THR_MCS_LOCKS)
+	nextThread = omrthread_mcs_unlock(self, monitor);
+	if (NULL != nextThread) {
+		NOTIFY_WRAPPER(nextThread);
+	}
+#else /* defined(OMR_THR_MCS_LOCKS) */
 #if defined(OMR_THR_SPIN_WAKE_CONTROL)
 	omrthread_spinlock_swapState(monitor, J9THREAD_MONITOR_SPINLOCK_UNOWNED);
 	if (0 == monitor->spinThreads) {
@@ -4535,6 +4544,7 @@ monitor_wait_original(omrthread_t self, omrthread_monitor_t monitor,
 		unblock_spinlock_threads(self, monitor);
 	}
 #endif  /* defined(OMR_THR_SPIN_WAKE_CONTROL) */
+#endif /* defined(OMR_THR_MCS_LOCKS) */
 	self->lockedmonitorcount--;
 #endif /* defined(OMR_THR_THREE_TIER_LOCKING) */
 

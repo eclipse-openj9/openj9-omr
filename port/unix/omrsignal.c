166b6e3da2c136d8f77ef98c739111ad99ba4c97
Babneet Singh <sbabneet@ca.ibm.com>
2019-08-29 15:56:41 -0400

    Update ifdefs to be platform agnostic and for consistency
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- port/unix/omrsignal.c
+++ port/unix/omrsignal.c
@@ -236,12 +236,12 @@ static struct {
 #if defined(SIGPOLL)
 	, {OMRPORT_SIG_FLAG_SIGPOLL, SIGPOLL}
 #endif /* defined(SIGPOLL) */
-#if defined(AIXPPC)
+#if defined(SIGRECONFIG)
 	, {OMRPORT_SIG_FLAG_SIGRECONFIG, SIGRECONFIG}
-#endif /* defined(AIXPPC) */
-#if defined(J9ZOS390)
+#endif /* defined(SIGRECONFIG) */
+#if defined(SIGABND)
 	, {OMRPORT_SIG_FLAG_SIGABEND, SIGABND}
-#endif /* defined(J9ZOS390) */
+#endif /* defined(SIGABND) */
 };
 
 static omrthread_t asynchSignalReporterThread = NULL;

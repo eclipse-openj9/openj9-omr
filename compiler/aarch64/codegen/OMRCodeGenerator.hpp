a7536c9aeade33752f383d80a0e08bb3b3e48d38
Daryl Maier <maier@ca.ibm.com>
2019-10-16 09:18:48 -0400

    Add CodeGenerator getter/setter for returnTypeInfoInstruction
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/aarch64/codegen/OMRCodeGenerator.hpp
+++ compiler/aarch64/codegen/OMRCodeGenerator.hpp
@@ -307,6 +307,17 @@ class OMR_EXTENSIBLE CodeGenerator : public OMR::CodeGenerator
    // We need to provide an implementation to avoid an unimplemented assert. See issue #4446.
    int32_t arrayTranslateAndTestMinimumNumberOfIterations() { return 8; }
 
+   /**
+    * @return Retrieves the cached returnTypeInfo instruction
+    */
+   TR::ARM64ImmInstruction *getReturnTypeInfoInstruction() { return _returnTypeInfoInstruction; }
+
+   /**
+    * @brief Caches the returnTypeInfo instruction
+    * @param[in] rtii : the returnTypeInfo instruction
+    */
+   void setReturnTypeInfoInstruction(TR::ARM64ImmInstruction *rtii) { _returnTypeInfoInstruction = rtii; }
+
    private:
 
    enum // flags

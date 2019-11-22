5ca2707e910258da41f390dfdc53e1f92ced49ce
Kevin Langman <langman@ca.ibm.com>
2019-11-22 17:19:29 -0500

    Add BlockVersioner instanceOf checks for index field objects
    When versioning blocks, it's possible for the generated checks to load
    index variables from objects by duplicating code from within blocks that
    are protected by inlining guards. Therefore the object might not be of
    the expected type possibly resulting in loads that reach past the end
    of the object. In rare cases this load can result in a SEGV when the
    object is allocated at the very end of the heap. In addition, this change
    will abort versioning of blocks when the estimated cost of executing the
    required tests is higher then the savings from removing bound-checks.
    
    Signed-off-by: Kevin Langman <langman@ca.ibm.com>

--- compiler/optimizer/OMRValuePropagation.hpp
+++ compiler/optimizer/OMRValuePropagation.hpp
@@ -788,6 +788,7 @@ class ValuePropagation : public TR::Optimization
       bool _versionBucket;
       bool _notToVersionBucket;
       TR_ScratchList<TR::Node> *_bndChecks;
+      TR_OpaqueClassBlock *_instanceOfClass;
       };
    struct ArrayLengthToVersion : public TR_Link<ArrayLengthToVersion>
       {
@@ -814,7 +815,7 @@ class ValuePropagation : public TR::Optimization
    void removeBndChecksFromFastVersion(BlockVersionInfo *);
    TR::Node * findVarOfSimpleForm(TR::Node *);
    TR::Node * findVarOfSimpleFormOld(TR::Node *);
-   void createNewBucketForArrayIndex(ArrayLengthToVersion *,TR_LinkHead<ArrayLengthToVersion> *, int32_t , TR::Node *, TR::Node *, TR_OpaqueClassBlock *);
+   void createNewBucketForArrayIndex(ArrayLengthToVersion *,TR_LinkHead<ArrayLengthToVersion> *, int32_t , TR::Node *, TR::Node *, TR_OpaqueClassBlock *, TR_OpaqueClassBlock *);
    void collectDefSymRefs(TR::Node *,TR::Node *);
    bool prepareForBlockVersion(TR_LinkHead<ArrayLengthToVersion> *);
    void addToSortedList(TR_LinkHead<ArrayLengthToVersion> *,ArrayLengthToVersion *);

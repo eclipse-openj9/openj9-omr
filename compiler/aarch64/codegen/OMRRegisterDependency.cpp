9056b3bbce225339c52c5c6334e84506cc0b8ad7
Akira Saitoh <saiaki@jp.ibm.com>
2019-12-03 14:11:11 +0900

    AArch64: Implement OMR::ARM64::RegisterDependencyConditions::clone
    - This commits adds implementation of `OMR::ARM64::RegisterDependencyConditions::clone`.
    - It adds optional arguments to `clone` that enables cloning only pre/post part
    of original dependency conditions.
    - It adds utility methods `clonePre` and `clonePost` which call `clone`.
    
    Signed-off-by: Akira Saitoh <saiaki@jp.ibm.com>

--- compiler/aarch64/codegen/OMRRegisterDependency.cpp
+++ compiler/aarch64/codegen/OMRRegisterDependency.cpp
@@ -146,10 +146,46 @@ void OMR::ARM64::RegisterDependencyConditions::incRegisterTotalUseCounts(TR::Cod
 TR::RegisterDependencyConditions *
 OMR::ARM64::RegisterDependencyConditions::clone(
    TR::CodeGenerator *cg,
-   TR::RegisterDependencyConditions *added)
+   TR::RegisterDependencyConditions *added,
+   bool omitPre, bool omitPost)
    {
-   TR_UNIMPLEMENTED();
-   return NULL;
+   int preNum = omitPre ? 0 : getAddCursorForPre();
+   int postNum = omitPost ? 0 : getAddCursorForPost();
+   int addPreNum = 0;
+   int addPostNum = 0;
+
+   if (added != NULL)
+      {
+      addPreNum = omitPre ? 0 : added->getAddCursorForPre();
+      addPostNum = omitPost ? 0 : added->getAddCursorForPost();
+      }
+   
+   TR::RegisterDependencyConditions *result =  new (cg->trHeapMemory()) TR::RegisterDependencyConditions(preNum + addPreNum, postNum + addPostNum, cg->trMemory());
+   for (int i = 0; i < preNum; i++)
+      {
+      auto singlePair = getPreConditions()->getRegisterDependency(i);
+      result->addPreCondition(singlePair->getRegister(), singlePair->getRealRegister(), singlePair->getFlags());
+      }
+
+   for (int i = 0; i < postNum; i++)
+      {
+      auto singlePair = getPostConditions()->getRegisterDependency(i);
+      result->addPostCondition(singlePair->getRegister(), singlePair->getRealRegister(), singlePair->getFlags());
+      }
+
+   for (int i = 0; i < addPreNum; i++)
+      {
+      auto singlePair = added->getPreConditions()->getRegisterDependency(i);
+      result->addPreCondition(singlePair->getRegister(), singlePair->getRealRegister(), singlePair->getFlags());
+      }
+
+   for (int i = 0; i < addPostNum; i++)
+      {
+      auto singlePair = added->getPostConditions()->getRegisterDependency(i);
+      result->addPostCondition(singlePair->getRegister(), singlePair->getRealRegister(), singlePair->getFlags());
+      }
+
+   return result;
    }
 
 void TR_ARM64RegisterDependencyGroup::assignRegisters(

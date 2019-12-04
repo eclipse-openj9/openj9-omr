b3c40ab5fcb42648edc3b505ef725b51d30432d3
Rahil Shah <rahil@ca.ibm.com>
2019-12-04 15:40:34 -0500

    Consider callee type in exceptionRaised/OSRPoint query
    This commit contains mainly following two changes.
    1. Create a list of methods calls to which does not yield to OSR and use this
       information in the query to know if a call node can catch OSR.
    2. Method calls which does not have any side-effects are marked as pure function.
       Calls to those method does not throw an exception. Use this information in query
       to know if a call can raise exceptions.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- compiler/il/OMRMethodSymbol.hpp
+++ compiler/il/OMRMethodSymbol.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corp. and others
+ * Copyright (c) 2000, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -194,6 +194,7 @@ public:
    bool safeToSkipZeroInitializationOnNewarrays() { return false; }
    bool safeToSkipChecksOnArrayCopies() { return false; }
 
+   bool functionCallDoesNotYieldOSR() { return false; }
    bool isPureFunction() { return false; }
 
 protected:

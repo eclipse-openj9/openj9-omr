f7d59850587ecd4e131f219a19c0a05de8b55bbd
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2020-01-14 15:28:23 +0000

    Add unit tests for the Power binary encoder
    Previously, there was no direct testing of the Power codegen's binary
    encoder. While portions of it would be exercised by the Tril tests and
    through the test suites of downstream projects such as OpenJ9, there was
    a significant amount of functionality that was not tested since it is
    either currently unused or only used in very exotic situations that are
    unlikely to be exercised by the existing test suite.
    
    In order to better confirm that the Power binary encoder is working as
    expected, a new set of unit tests are being added that should exercise
    as much of its functionality as possible. The iteration of these tests
    being contributed include comprehensive tests for most non-memory
    opcodes currently in use by the Power codegen in either OMR or OpenJ9.
    
    Note that a number of these tests are currently disabled due to known
    issues in the Power binary encoder. These will be fixed as part of an
    ongoing refactor to make the Power binary encoder easier to maintain.
    
    Signed-off-by: Ben Thomas <ben@benthomas.ca>

---
+++ fvtest/compilerunittest/CodeGenTest.hpp
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef CODEGENTEST_HPP
+#define CODEGENTEST_HPP
+
+#include <gtest/gtest.h>
+
+#include "Jit.hpp"
+#include "codegen/CodeGenerator.hpp"
+#include "compile/Compilation.hpp"
+#include "env/ConcreteFE.hpp"
+#include "env/SystemSegmentProvider.hpp"
+#include "ilgen/IlGenRequest.hpp"
+#include "ilgen/IlGeneratorMethodDetails.hpp"
+#include "ilgen/TypeDictionary.hpp"
+
+#include "codegen/CodeGenerator_inlines.hpp"
+#include "compile/Compilation_inlines.hpp"
+#include "ilgen/IlGeneratorMethodDetails_inlines.hpp"
+
+namespace TRTest {
+
+class NullIlGenRequest : public TR::IlGenRequest {
+    TR::IlGeneratorMethodDetails _details;
+
+public:
+    NullIlGenRequest() : TR::IlGenRequest(_details) {}
+
+    virtual TR_IlGenerator *getIlGenerator(
+        TR::ResolvedMethodSymbol *methodSymbol,
+        TR_FrontEnd *fe,
+        TR::Compilation *comp,
+        TR::SymbolReferenceTable *symRefTab
+    ) {
+        throw 0;
+    }
+
+    virtual void print(TR_FrontEnd *fe, TR::FILE *file, const char *suffix) {}
+};
+
+class JitInitializer {
+public:
+    JitInitializer() {
+        initializeJit();
+    }
+
+    ~JitInitializer() {
+        shutdownJit();
+    }
+};
+
+class CodeGenTest : public ::testing::Test {
+    JitInitializer _jitInit;
+
+    TR::RawAllocator _rawAllocator;
+    TR::SystemSegmentProvider _segmentProvider;
+    TR::Region _dispatchRegion;
+    TR_Memory _trMemory;
+
+    TR::TypeDictionary _types;
+
+    TR::Options _options;
+    NullIlGenRequest _ilGenRequest;
+    TR::ResolvedMethod _method;
+    TR::Compilation _comp;
+public:
+    CodeGenTest() :
+        _jitInit(),
+        _rawAllocator(),
+        _segmentProvider(1 << 16, _rawAllocator),
+        _dispatchRegion(_segmentProvider, _rawAllocator),
+        _trMemory(*OMR::FrontEnd::singleton().persistentMemory(), _dispatchRegion),
+        _types(),
+        _options(),
+        _ilGenRequest(),
+        _method("compunittest", "0", "test", 0, NULL, _types.NoType, NULL, NULL),
+        _comp(0, NULL, &OMR::FrontEnd::singleton(), &_method, _ilGenRequest, _options, _dispatchRegion, &_trMemory, TR_OptimizationPlan::alloc(warm)) {
+    }
+
+    TR::CodeGenerator* cg() { return _comp.cg(); }
+};
+
+}
+
+#endif

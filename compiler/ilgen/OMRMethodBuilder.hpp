3946f5fae1b4222cdd8417cefae09b8d25b302c1
Petar Jelenkovic <pjelenko@unb.ca>
2020-01-24 19:14:30 +0000

    Add DefineGlobal to JitBuilder
    Adds DefineGlobal to JitBuilder and creates JitBuilder tests for
    DefineGlobal.
    
    Signed-off-by: Petar Jelenkovic <pjelenko@unb.ca>

--- compiler/ilgen/OMRMethodBuilder.hpp
+++ compiler/ilgen/OMRMethodBuilder.hpp
@@ -121,6 +121,13 @@ class MethodBuilder : public TR::IlBuilder
    void DefineReturnType(TR::IlType *dt);
    void DefineLocal(const char *name, TR::IlType *dt);
    void DefineMemory(const char *name, TR::IlType *dt, void *location);
+
+   /**
+    * @brief Define a global symbol
+    * @param name the name by which the global symbol will be referred to
+    * @param dt the data type of the global symbol
+    * @param location the address of the value the global symbol refers to
+    */
    void DefineGlobal(const char *name, TR::IlType *dt, void *location);
    void DefineFunction(const char* const name,
                        const char* const fileName,
@@ -347,6 +354,11 @@ class MethodBuilder : public TR::IlBuilder
 
    typedef TR::typed_allocator<std::pair<const char * const, void *>, TR::Region &> GlobalMapAllocator;
    typedef std::map<const char *, void *, StrComparator, GlobalMapAllocator> GlobalMap;
+
+   /**
+    * @brief map of global symbol names and locations of values global symbols refer to.
+    *        This map should only be accessed inside a compilation via lookupSymbol.
+    */
    GlobalMap                   _globals;
 
    typedef TR::typed_allocator<std::pair<const char * const, TR::ResolvedMethod *>, TR::Region &> FunctionMapAllocator;

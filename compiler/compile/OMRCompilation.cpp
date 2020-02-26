0xdaryl/omr

Set Compilation flag enabling read only code
Flag is enabled via the `readOnlyCode` Option.

Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/compile/OMRCompilation.cpp
@@ -459,7 +459,10 @@ OMR::Compilation::Compilation(
    else
       _osrCompilationData = NULL;
 
-
+  if (self()->getOption(TR_GenerateReadOnlyCode))
+      {
+      self()->setGenerateReadOnlyCode();
+      }
 
    }
 
@@ -2757,7 +2760,7 @@ bool OMR::Compilation::isRecursiveMethodTarget(TR::Symbol *targetSymbol)
 const TR::TypeLayout* OMR::Compilation::typeLayout(TR_OpaqueClassBlock * clazz)
    {
    TR::Region& region = self()->region();
-   auto it = _typeLayoutMap.find(clazz); 
+   auto it = _typeLayoutMap.find(clazz);
    if (it != _typeLayoutMap.end())
       {
       return it->second;
@@ -2765,7 +2768,7 @@ const TR::TypeLayout* OMR::Compilation::typeLayout(TR_OpaqueClassBlock * clazz)
    else
       {
       const TR::TypeLayout* layout = TR::Compiler->cls.enumerateFields(region, clazz, self());
-      _typeLayoutMap.insert(std::make_pair(clazz, layout)); 
-      return layout; 
+      _typeLayoutMap.insert(std::make_pair(clazz, layout));
+      return layout;
       }
    }

eclipse-openj9/openj9-omr

compiler/compile/OMRCompilation.cpp
@@ -459,7 +459,10 @@ OMR::Compilation::Compilation(
    else
       _osrCompilationData = NULL;
 
-
+  if (self()->getOption(TR_ForceGenerateReadOnlyCode))
+      {
+      self()->setGenerateReadOnlyCode();
+      }
 
    }
 
@@ -2757,7 +2760,7 @@ bool OMR::Compilation::isRecursiveMethodTarget(TR::Symbol *targetSymbol)
 const TR::TypeLayout* OMR::Compilation::typeLayout(TR_OpaqueClassBlock * clazz)
    {
    TR::Region& region = self()->region();
-   auto it = _typeLayoutMap.find(clazz); 
+   auto it = _typeLayoutMap.find(clazz);
    if (it != _typeLayoutMap.end())
       {
       return it->second;
@@ -2765,7 +2768,7 @@ const TR::TypeLayout* OMR::Compilation::typeLayout(TR_OpaqueClassBlock * clazz)
    else
       {
       const TR::TypeLayout* layout = TR::Compiler->cls.enumerateFields(region, clazz, self());
-      _typeLayoutMap.insert(std::make_pair(clazz, layout)); 
-      return layout; 
+      _typeLayoutMap.insert(std::make_pair(clazz, layout));
+      return layout;
       }
    }
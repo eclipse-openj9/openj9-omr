eclipse-openj9/openj9-omr

commit f3c45015f69e712d33ac86c423260a57374b776e

Fix riscv gprClobberEvaluate to ensure correct gc flags are set on new register
The method gprClobberEvaluate may allocate a new register. In this case, the
new register may need to be flagged as a collected reference or an internal
pointer. This commit ensures that these two flags are propagated from the old
register.

Issue: https://github.com/eclipse/openj9/issues/8397

Signed-off-by: Ryan Shukla <ryans@ibm.com>

compiler/riscv/codegen/OMRCodeGenerator.cpp
@@ -351,8 +351,32 @@ TR::Register *OMR::RV::CodeGenerator::gprClobberEvaluate(TR::Node *node)
    {
    if (node->getReferenceCount() > 1)
       {
+      TR::Register *sourceReg = self()->evaluate(node);
       TR::Register *targetReg = self()->allocateRegister();
-      generateITYPE(TR::InstOpCode::_addi, node, targetReg, self()->evaluate(node), 0, self());
+      generateITYPE(TR::InstOpCode::_addi, node, targetReg, sourceReg, 0, self());
+
+      if (sourceReg->containsCollectedReference())
+         {
+         if (self()->comp()->getOption(TR_TraceCG))
+            traceMsg(
+               self()->comp(),
+               "Setting containsCollectedReference on register %s\n",
+               self()->getDebug()->getName(targetRegister));
+         targetReg->setContainsCollectedReference();
+         }
+      if (sourceReg->containsInternalPointer())
+         {
+         TR::AutomaticSymbol *pinningArrayPointer = sourceRegister->getPinningArrayPointer();
+         if (self()->comp()->getOption(TR_TraceCG))
+            traceMsg(
+               self()->comp(),
+               "Setting containsInternalPointer on register %s and setting pinningArrayPointer to " POINTER_PRINTF_FORMAT "\n",
+               self()->getDebug()->getName(targetRegister),
+               pinningArrayPointer);
+         targetReg->setContainsInternalPointer();
+         targetReg->setPinningArrayPointer(pinningArrayPointer);
+         }
+
       return targetReg;
       }
    else

commit 9a5314e4324af75d6b8443f26d34fea7a07c50aa

RISC-V: fix compilation error introduced by f3c45015f6
Signed-off-by: Jan Vrany <jan.vrany@fit.cvut.cz>

compiler/riscv/codegen/OMRCodeGenerator.cpp
@@ -361,17 +361,17 @@ TR::Register *OMR::RV::CodeGenerator::gprClobberEvaluate(TR::Node *node)
             traceMsg(
                self()->comp(),
                "Setting containsCollectedReference on register %s\n",
-               self()->getDebug()->getName(targetRegister));
+               self()->getDebug()->getName(targetReg));
          targetReg->setContainsCollectedReference();
          }
       if (sourceReg->containsInternalPointer())
          {
-         TR::AutomaticSymbol *pinningArrayPointer = sourceRegister->getPinningArrayPointer();
+         TR::AutomaticSymbol *pinningArrayPointer = sourceReg->getPinningArrayPointer();
          if (self()->comp()->getOption(TR_TraceCG))
             traceMsg(
                self()->comp(),
                "Setting containsInternalPointer on register %s and setting pinningArrayPointer to " POINTER_PRINTF_FORMAT "\n",
-               self()->getDebug()->getName(targetRegister),
+               self()->getDebug()->getName(targetReg),
                pinningArrayPointer);
          targetReg->setContainsInternalPointer();
          targetReg->setPinningArrayPointer(pinningArrayPointer);
jonoommen/omr

Add Dynamic Breadth First Scan Ordering to the GC
Add Dynamic Breadth First Scan Ordering to the GC implemented
using JIT hot fields. This enables the copying of a hot field
marked by the JIT immediately after the object containing the
hot field is copied during a Scavenge.

Signed-off-by: Jonathan Oommen <jon.oommen@gmail.com>

example/glue/ObjectModelDelegate.hpp
@@ -202,6 +202,45 @@ public:
 	}
 
 	/**
+	 * Returns the field offset of the hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled.
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the hottest field of the given object referred to by the forwarded header, return U_8_MAX if a hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the second hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the second hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset2(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the third hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the third hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset3(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
 	 * Get the instance size (total) of a forwarded object from the forwarding pointer. The  size must
 	 * include the header and any expansion bytes to be allocated if the object will grow when moved.
 	 *

gc/base/ObjectModelBase.hpp
@@ -827,6 +827,48 @@ public:
 	}
 
 	/**
+	 * Returns the field offset of the hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled.
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the hottest field of the given object referred to by the forwarded header, return U_8_MAX if a hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the second hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the second hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset2(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset2(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+		/**
+	 * Returns the field offset of the third hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the third hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE U_8
+	getHotFieldOffset3(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset3(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+	/**
 	 * Extract the flag bits from an unforwarded object. Flag bits are returned in the low-order byte of the returned value.
 	 *
 	 * This method will assert if the object has been marked as forwarded.

gc/base/standard/Scavenger.cpp
@@ -105,6 +105,9 @@
 #define FLIP_TENURE_LARGE_SCAN 4
 #define FLIP_TENURE_LARGE_SCAN_DEFERRED 5
 
+/* If scavenger dynamicBreadthFirstScanOrdering and alwaysDepthCopyFirstOffset is enabled, always copy the first offset of each object after the object itself is copied */
+#define DEFAULT_HOT_FIELD_OFFSET 1
+
 /* VM Design 1774: Ideally we would pull these cache line values from the port library but this will suffice for
  * a quick implementation
  */
@@ -246,6 +249,7 @@ MM_Scavenger::initialize(MM_EnvironmentBase *env)
 	 * will contain a valid entry. We set the appropriate number of caches per thread here */
 	switch (_extensions->scavengerScanOrdering) {
 	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_BREADTH_FIRST:
+	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_DYNAMIC_BREADTH_FIRST:
 		_cachesPerThread = FLIP_TENURE_LARGE_SCAN;
 		break;
 	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_HIERARCHICAL:
@@ -1647,6 +1651,9 @@ MM_Scavenger::copy(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHea
 			scavStats->_flipBytes += objectCopySizeInBytes;
 			scavStats->getFlipHistory(0)->_flipBytes[oldObjectAge + 1] += objectReserveSizeInBytes;
 		}
+
+		/* depth copy the hot fields of an object if scavenger dynamicBreadthFirstScanOrdering is enabled */	
+		depthCopyHotFields(env, forwardedHeader, destinationObjectPtr);
 	} else {
 		/* We have not used the reserved space now, but we will for subsequent allocations. If this space was reserved for an individual object,
 		 * we might have created a TLH remainder from previous cache just before reserving this space. This space eventaully can create another remainder.
@@ -1668,6 +1675,42 @@ MM_Scavenger::copy(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHea
 	}
 	/* return value for updating the slot */
 	return destinationObjectPtr;
+}
+
+MMINLINE void
+MM_Scavenger::depthCopyHotFields(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHeader, omrobjectptr_t destinationObjectPtr) {
+	/* depth copy the hot fields of an object up to a depth specified by depthCopyMax */
+	if (env->_hotFieldCopyDepthCount < _extensions->depthCopyMax) {
+		U_8 hotFieldOffset = _extensions->objectModel.getHotFieldOffset(forwardedHeader);
+		if (U_8_MAX != hotFieldOffset) {
+			copyHotField(env, destinationObjectPtr, hotFieldOffset);
+			U_8 hotFieldOffset2 = _extensions->objectModel.getHotFieldOffset2(forwardedHeader);
+			if (U_8_MAX != hotFieldOffset2) {
+				copyHotField(env, destinationObjectPtr, hotFieldOffset2);
+				U_8 hotFieldOffset3 = _extensions->objectModel.getHotFieldOffset3(forwardedHeader);
+				if (U_8_MAX != hotFieldOffset3) {
+					copyHotField(env, destinationObjectPtr, hotFieldOffset3);
+				}
+			}
+		} else if (_extensions->alwaysDepthCopyFirstOffset && !_extensions->objectModel.isIndexable(forwardedHeader)) {
+			copyHotField(env, destinationObjectPtr, DEFAULT_HOT_FIELD_OFFSET);
+		}
+	}
+}
+
+MMINLINE void
+MM_Scavenger::copyHotField(MM_EnvironmentStandard *env, omrobjectptr_t destinationObjectPtr, U_8 offset) {
+	GC_SlotObject hotFieldObject(_omrVM, (fomrobject_t*)(destinationObjectPtr + offset));
+	omrobjectptr_t objectPtr = hotFieldObject.readReferenceFromSlot();							
+	if (isObjectInEvacuateMemory(objectPtr)) {
+		/* Hot field needs to be copy and forwarded.  Check if the work has already been done */
+		MM_ForwardedHeader forwardHeaderHotField(objectPtr, _extensions->compressObjectReferences());
+		if (!forwardHeaderHotField.isForwardedPointer()) {
+			env->_hotFieldCopyDepthCount += 1;
+			copyObject(env, &forwardHeaderHotField);
+			env->_hotFieldCopyDepthCount -= 1;
+		}
+	}
 }
 
 /****************************************
@@ -2336,6 +2379,7 @@ MM_Scavenger::completeScan(MM_EnvironmentStandard *env)
 
 		switch (_extensions->scavengerScanOrdering) {
 		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_BREADTH_FIRST:
+		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_DYNAMIC_BREADTH_FIRST:
 			completeScanCache(env, scanCache);
 			break;
 		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_HIERARCHICAL:

eclipse-openj9/openj9-omr

example/glue/ObjectModelDelegate.hpp
@@ -202,6 +202,45 @@ public:
 	}
 
 	/**
+	 * Returns the field offset of the hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled.
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the hottest field of the given object referred to by the forwarded header, return U_8_MAX if a hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the second hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the second hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset2(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the third hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the third hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset3(MM_ForwardedHeader *forwardedHeader)
+	{
+		return U_8_MAX;
+	}
+
+	/**
 	 * Get the instance size (total) of a forwarded object from the forwarding pointer. The  size must
 	 * include the header and any expansion bytes to be allocated if the object will grow when moved.
 	 *

gc/base/ObjectModelBase.hpp
@@ -827,6 +827,48 @@ public:
 	}
 
 	/**
+	 * Returns the field offset of the hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled.
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the hottest field of the given object referred to by the forwarded header, return U_8_MAX if a hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+	/**
+	 * Returns the field offset of the second hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the second hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset2(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset2(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+		/**
+	 * Returns the field offset of the third hottest field of the object referred to by the forwarded header.
+	 * Valid if scavenger dynamicBreadthFirstScanOrdering is enabled
+	 *
+	 * @param forwardedHeader pointer to the MM_ForwardedHeader instance encapsulating the object
+	 * @return the offset of the third hottest field of the given object referred to by the forwarded header, return U_8_MAX if the hot field does not exist
+	 */
+	MMINLINE uint8_t
+	getHotFieldOffset3(MM_ForwardedHeader *forwardedHeader)
+	{
+		//return _delegate.getHotFieldOffset3(forwardedHeader); //TODO: will be populated once Language specific implementation is ready
+		return U_8_MAX;
+	}
+
+	/**
 	 * Extract the flag bits from an unforwarded object. Flag bits are returned in the low-order byte of the returned value.
 	 *
 	 * This method will assert if the object has been marked as forwarded.

gc/base/standard/Scavenger.cpp
@@ -105,6 +105,9 @@
 #define FLIP_TENURE_LARGE_SCAN 4
 #define FLIP_TENURE_LARGE_SCAN_DEFERRED 5
 
+/* If scavenger dynamicBreadthFirstScanOrdering and alwaysDepthCopyFirstOffset is enabled, always copy the first offset of each object after the object itself is copied */
+#define DEFAULT_HOT_FIELD_OFFSET 1
+
 /* VM Design 1774: Ideally we would pull these cache line values from the port library but this will suffice for
  * a quick implementation
  */
@@ -246,6 +249,7 @@ MM_Scavenger::initialize(MM_EnvironmentBase *env)
 	 * will contain a valid entry. We set the appropriate number of caches per thread here */
 	switch (_extensions->scavengerScanOrdering) {
 	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_BREADTH_FIRST:
+	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_DYNAMIC_BREADTH_FIRST:
 		_cachesPerThread = FLIP_TENURE_LARGE_SCAN;
 		break;
 	case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_HIERARCHICAL:
@@ -1647,6 +1651,9 @@ MM_Scavenger::copy(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHea
 			scavStats->_flipBytes += objectCopySizeInBytes;
 			scavStats->getFlipHistory(0)->_flipBytes[oldObjectAge + 1] += objectReserveSizeInBytes;
 		}
+
+		/* depth copy the hot fields of an object if scavenger dynamicBreadthFirstScanOrdering is enabled */	
+		depthCopyHotFields(env, forwardedHeader, destinationObjectPtr);
 	} else {
 		/* We have not used the reserved space now, but we will for subsequent allocations. If this space was reserved for an individual object,
 		 * we might have created a TLH remainder from previous cache just before reserving this space. This space eventaully can create another remainder.
@@ -1668,6 +1675,42 @@ MM_Scavenger::copy(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHea
 	}
 	/* return value for updating the slot */
 	return destinationObjectPtr;
+}
+
+MMINLINE void
+MM_Scavenger::depthCopyHotFields(MM_EnvironmentStandard *env, MM_ForwardedHeader* forwardedHeader, omrobjectptr_t destinationObjectPtr) {
+	/* depth copy the hot fields of an object up to a depth specified by depthCopyMax */
+	if (env->_hotFieldCopyDepthCount < _extensions->depthCopyMax) {
+		uint8_t hotFieldOffset = _extensions->objectModel.getHotFieldOffset(forwardedHeader);
+		if (U_8_MAX != hotFieldOffset) {
+			copyHotField(env, destinationObjectPtr, hotFieldOffset);
+			uint8_t hotFieldOffset2 = _extensions->objectModel.getHotFieldOffset2(forwardedHeader);
+			if (U_8_MAX != hotFieldOffset2) {
+				copyHotField(env, destinationObjectPtr, hotFieldOffset2);
+				uint8_t hotFieldOffset3 = _extensions->objectModel.getHotFieldOffset3(forwardedHeader);
+				if (U_8_MAX != hotFieldOffset3) {
+					copyHotField(env, destinationObjectPtr, hotFieldOffset3);
+				}
+			}
+		} else if (_extensions->alwaysDepthCopyFirstOffset && !_extensions->objectModel.isIndexable(forwardedHeader)) {
+			copyHotField(env, destinationObjectPtr, DEFAULT_HOT_FIELD_OFFSET);
+		}
+	}
+}
+
+MMINLINE void
+MM_Scavenger::copyHotField(MM_EnvironmentStandard *env, omrobjectptr_t destinationObjectPtr, uint8_t offset) {
+	GC_SlotObject hotFieldObject(_omrVM, (fomrobject_t*)(destinationObjectPtr + offset));
+	omrobjectptr_t objectPtr = hotFieldObject.readReferenceFromSlot();
+	if (isObjectInEvacuateMemory(objectPtr)) {
+		/* Hot field needs to be copy and forwarded.  Check if the work has already been done */
+		MM_ForwardedHeader forwardHeaderHotField(objectPtr, _extensions->compressObjectReferences());
+		if (!forwardHeaderHotField.isForwardedPointer()) {
+			env->_hotFieldCopyDepthCount += 1;
+			copyObject(env, &forwardHeaderHotField);
+			env->_hotFieldCopyDepthCount -= 1;
+		}
+	}
 }
 
 /****************************************
@@ -2336,6 +2379,7 @@ MM_Scavenger::completeScan(MM_EnvironmentStandard *env)
 
 		switch (_extensions->scavengerScanOrdering) {
 		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_BREADTH_FIRST:
+		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_DYNAMIC_BREADTH_FIRST:
 			completeScanCache(env, scanCache);
 			break;
 		case MM_GCExtensionsBase::OMR_GC_SCAVENGER_SCANORDERING_HIERARCHICAL:
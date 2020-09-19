ea176557d51ed8247b46c15356d608ee8c122b4e
Daryl Maier <maier@ca.ibm.com>
2020-09-19 14:26:01 -0400

    Remove JitBuilder CodeGenerator implementation
    The implementation was empty and provided no value over the default
    OMR CodeGenerator.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- jitbuilder/build/files/target/z.mk
+++ jitbuilder/build/files/target/z.mk
@@ -58,7 +58,6 @@ JIT_PRODUCT_BACKEND_SOURCES+=\
     $(JIT_OMR_DIRTY_DIR)/z/codegen/OMRCodeGenPhase.cpp \
     $(JIT_OMR_DIRTY_DIR)/z/env/OMRCPU.cpp \
     $(JIT_OMR_DIRTY_DIR)/z/env/OMRDebugEnv.cpp \
-    $(JIT_PRODUCT_DIR)/z/codegen/JBCodeGenerator.cpp \
     $(JIT_PRODUCT_DIR)/z/codegen/Evaluator.cpp \
     $(JIT_PRODUCT_DIR)/z/codegen/snippet/PPA1Snippet.cpp \
     $(JIT_PRODUCT_DIR)/z/codegen/snippet/PPA2Snippet.cpp

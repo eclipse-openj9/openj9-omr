392e0d35f36e30908d0630fe364b840142d1f338
Younes Manton <ymanton@ca.ibm.com>
2020-10-20 16:13:29 -0400

    Add CCData to OMR compiler
    CCData is a class that represents a table of data suitable for
    implementing GOTs, TOCs, PLTs, and so on.
    
    Signed-off-by: Younes Manton <ymanton@ca.ibm.com>

---
+++ compiler/codegen/CCData.cpp
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#include "OMR/Bytes.hpp"
+
+#include "codegen/CCData.hpp"
+
+#include "infra/Monitor.hpp"
+#include "infra/CriticalSection.hpp"
+
+using OMR::CCData;
+
+CCData::key_t CCData::key(const uint8_t * const data, const size_t sizeBytes)
+   {
+   return key_t(reinterpret_cast<const key_t::value_type *>(data), sizeBytes);
+   }
+
+CCData::key_t CCData::key(const char * const str)
+   {
+   return key_t(reinterpret_cast<const key_t::value_type *>(str));
+   }
+
+// Converts a size in units of bytes to a size in units of sizeof(data_t).
+#define DATA_SIZE_FROM_BYTES_SIZE(x) (((x) + (sizeof(data_t)) - 1) / (sizeof(data_t)))
+
+// Converts an alignment in units of bytes to an alignment in units of alignof(data_t).
+#define DATA_ALIGNMENT_FROM_BYTES_ALIGNMENT(x) (((x) + (alignof(data_t)) - 1) / (alignof(data_t)))
+
+CCData::CCData(const size_t sizeBytes)
+: _data(new data_t[DATA_SIZE_FROM_BYTES_SIZE(sizeBytes)]), _capacity(DATA_SIZE_FROM_BYTES_SIZE(sizeBytes)), _putIndex(0), _lock(TR::Monitor::create("CCDataMutex")), _releaseData(false)
+   {
+   }
+
+CCData::CCData(uint8_t * const storage, const size_t sizeBytes)
+: CCData(alignStorage(storage, sizeBytes))
+   {
+   }
+
+CCData::CCData(const storage_and_size_pair_t storageAndSize)
+: _data(storageAndSize.first), _capacity(storageAndSize.second), _putIndex(0), _lock(TR::Monitor::create("CCDataMutex")), _releaseData(true)
+   {
+   }
+
+CCData::~CCData()
+   {
+   // Memory for data can either be allocated by this class or passed in via
+   // the constructor. If allocated, it has to be freed now; if passed in we can't free
+   // it.
+   // The _data member var is a smart pointer that will automatically
+   // free the underlying memory when it goes out of scope. If memory was passed in
+   // via the constructor we have to release it now to prevent the smart pointer from
+   // freeing it.
+   if (_releaseData)
+      _data.release();
+   }
+
+const CCData::storage_and_size_pair_t CCData::alignStorage(uint8_t * const storage, size_t sizeBytes)
+   {
+   // This function takes buffer, aligns it for data_t, and converts the size in bytes to the size in units of data_t.
+   // It returns the aligned pointer and adjusted size in an std::pair so that we can use it when calling a constructor in an initializer list.
+   void *alignedStorage = storage;
+   OMR::align(alignof(data_t), sizeof(data_t), alignedStorage, sizeBytes);
+   return std::make_pair(reinterpret_cast<data_t *>(alignedStorage), DATA_SIZE_FROM_BYTES_SIZE(sizeBytes));
+   }
+
+bool CCData::put(const uint8_t * const value, const size_t sizeBytes, const size_t alignmentBytes, const key_t * const key, index_t &index)
+   {
+   const OMR::CriticalSection cs(_lock);
+
+   /**
+    * Multiple compilation threads may be attempting to update the same value with
+    * the same key.  Once we have the mutex, check if the value already exists and
+    * return successfully.
+    */
+   if (key && find_unsafe(*key, &index))
+      {
+      return true;
+      }
+
+   // The following feels like it could be simplified and calculated more efficiently. If you're reading this, have a go at it.
+   const size_t sizeDataUnits = DATA_SIZE_FROM_BYTES_SIZE(sizeBytes);
+   const size_t alignmentDataUnits = DATA_ALIGNMENT_FROM_BYTES_ALIGNMENT(alignmentBytes);
+   const size_t alignmentMask = alignmentDataUnits - 1;
+   const size_t alignmentPadding = (alignmentDataUnits - ((reinterpret_cast<uintptr_t>(_data.get() + _putIndex) / alignof(data_t)) & alignmentMask)) & alignmentMask;
+   const size_t remainingCapacity = _capacity - _putIndex;
+
+   if (sizeDataUnits + alignmentPadding > remainingCapacity)
+      return false;
+
+   _putIndex += alignmentPadding;
+   index = _putIndex;
+
+   if (key != nullptr)
+      _mappings[*key] = _putIndex;
+
+   if (value != nullptr)
+      {
+      std::copy(value,
+                value + sizeBytes,
+                reinterpret_cast<uint8_t *>(_data.get() + _putIndex));
+      }
+
+   _putIndex += sizeDataUnits;
+
+   return true;
+   }
+
+bool CCData::get(const index_t index, uint8_t * const value, const size_t sizeBytes) const
+   {
+   if (index >= _capacity)
+      return false;
+
+   std::copy(reinterpret_cast<const uint8_t *>(_data.get() + index),
+             reinterpret_cast<const uint8_t *>(_data.get() + index) + sizeBytes,
+             value);
+
+   return true;
+   }
+
+bool CCData::find(const key_t key, index_t * const index) const
+   {
+   const OMR::CriticalSection cs(_lock);
+   return find_unsafe(key, index);
+   }
+
+bool CCData::find_unsafe(const key_t key, index_t * const index) const
+   {
+   auto e = _mappings.find(key);
+   if (e != _mappings.cend())
+      {
+      if (index != nullptr)
+         *index = e->second;
+      return true;
+      }
+
+   return false;
+   }
---
+++ compiler/codegen/CCData.hpp
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef OMR_CCDATA_INCL
+#define OMR_CCDATA_INCL
+
+#include <memory>
+#include <map>
+#include <string>
+#include <cstddef>
+
+namespace TR { class Monitor; }
+
+namespace OMR
+{
+
+/**
+ * @class CCData
+ *
+ * @brief This class represents a table that can be used to implement a GOT or TOC or a PLT or similar.
+ *
+ * <TODO: A more detailed discussion about the design here.>
+ */
+class CCData
+   {
+   private:
+      /** \typedef data_t Implementation detail. This type represents the units of the table. Typically bytes, but can be some other data type, as long as it can be default-constructed. */
+      typedef uint8_t data_t;
+
+      /** \typedef section_t Implementation detail. This type represents the table itself. It must behave like a pointer to an array of type data_t. */
+      typedef std::unique_ptr<data_t[]> section_t;
+
+   public:
+      /** \typedef index_t This type represents the indices defined in the public interface of this class. They must behave like integral types. */
+      typedef size_t index_t;
+
+      /** \typedef key_t This type represents the keys defined in the public interface of this class. The constructor is unspecified, use CCData_t::key() to create keys. */
+      typedef std::string key_t;
+
+#if __cpp_static_assert
+      static_assert(sizeof(key_t::value_type) == 1, "Unimplemented key unit size, remaining bytes need to be zeroed to support key units >1.");
+#endif
+
+   private:
+      /** \typedef map_t Implementation detail. This type represents the associative container that maps keys to indices. It must behave like std::unordered_map. */
+      typedef std::map<key_t, index_t> map_t;
+
+   public:
+      /**
+       * @brief Creates a key_t from the given value. The value's type must have a unique object representation.
+       *
+       * Unique Object Representations
+       *
+       * Structs and classes usually have padding bytes which are going to be included in their memory representations.
+       * Even if the padding is at the end of the instance, sizeof() will include it. Calculating a key based on the memory
+       * representation is going to include the padding, which is incorrect. If the key includes the padding, two otherwise
+       * equal objects will result in different keys if their padding bytes differ.
+       *
+       * This also applies to floats and doubles because of various "don't care" (DC) bits in the binary representation.
+       * Logically equal float values might have different bit patterns and would result in different keys.
+       *
+       * Types that don't have any padding or DC bits have unique object representations in C++ standard parlance.
+       *
+       * @tparam T The type of the value to create the key from. The type must have a unique object representation. A static assertion checks for the std::has_unique_object_representations<T> type trait on capable compilers.
+       * @param[In] value The value to create the key from.
+       * @return The key.
+       */
+      template <typename T>
+      static key_t key(const T value);
+
+      /**
+       * @brief Creates a key_t from the given buffer of data. The entire buffer will be used as input, including any unused bits/bytes that you may not be aware of. See key(const T value) for info on why this is important to note.
+       *
+       * @param[In] data A pointer to the buffer of data.
+       * @param[In] sizeBytes The size (in bytes) of the data.
+       * @return The key.
+       */
+      static key_t key(const uint8_t * const data, const size_t sizeBytes);
+
+      /**
+       * @brief Creates a key_t from the given null-terminated C string.
+
+       * @param[In] data A pointer to the string.
+       * @return The key.
+       * \TODO: Decide if we need this; useful for string literals, but susceptible to bugs/attack because of the unbounded string.
+       */
+      static key_t key(const char * const str);
+
+      /**
+       * @brief Constructs a CCData.
+
+       * @param[In] sizeBytes The amount of data (in bytes) that CCData can hold.
+       */
+      CCData(const size_t sizeBytes);
+
+      /**
+       * @brief Constructs a CCData and accepts a pointer to a memory buffer that will be used to hold the data.
+       *
+       * The lifetime of the memory buffer must exceed the lifetime of the resulting CCData object. The memory buffer will still be valid after the CCData object is destructed (i.e. the memory buffer will not be freed, do it yourself).
+       *
+       * @param[In] storage A pointer to a memory buffer that CCData will use.
+       * @param[In] sizeBytes The amount of data (in bytes) that the buffer contains.
+       */
+      CCData(uint8_t * const storage, const size_t sizeBytes);
+
+      ~CCData();
+
+      /**
+       * @brief Puts the given value in the table, optionally mapped to the given key (if any), aligned to the value's natural type, and returns the index to the value. Synchronized.
+
+       * @tparam T The type of the value to put in the table. The type must be TriviallyCopyable. A static assertion checks for the std::is_trivially_copyable<T> type trait on capable compilers.
+       * @param[In] value The value to put.
+       * @param[In] key Optional. The key to map the resulting index to. Without a key the index is the only reference to the data.
+       * @param[Out] index The index that refers to the value.
+       */
+      template <typename T>
+      bool put(const T value, const key_t * const key, index_t &index);
+
+      /**
+       * @brief Puts the given value in the table, optionally mapped to the given key (if any), aligned to the given boundary (in bytes), and returns the index to the value. Synchronized.
+
+       * @param[In] value Optional. A pointer to the value to put. If null, no data will be copied but the space will be allocated none the less.
+       * @param[In] sizeBytes The size of the value pointed to.
+       * @param[In] alignmentBytes The alignment (in bytes) to align the value to.
+       * @param[In] key Optional. The key to map the resulting index to. Without a key the index is the only reference to the data. If the key is already mapped to an index the operation will return the index and true, but no data will be written.
+       * @param[Out] index The index that refers to the value.
+       * @return True if the value was placed in the table, or the key was already mapped to an index, false otherwise.
+       */
+      bool put(const uint8_t * const value, const size_t sizeBytes, const size_t alignmentBytes, const key_t * const key, index_t &index);
+
+      /**
+       * @brief Gets the value referred to by the index from the table.
+
+       * @param[In] T The type of the value to get from the table. The type must be TriviallyCopyable. A static assertion checks for the std::is_trivially_copyable<T> type trait on capable compilers.
+       * @param[In] index The index that refers to the value.
+       * @param[out] value A reference to the value to write the result to.
+       * @return True if the value was placed in the table, false otherwise.
+       */
+      template <typename T>
+      bool get(const index_t index, T &value) const;
+
+      /**
+       * @brief Gets the value referred to by the index from the table.
+
+       * @param[In] index The index that refers to the value.
+       * @param[in] value A pointer to the value to write the result to. This parameter is ignored unless this function returns true.
+       * @param[In] sizeBytes The size (in bytes) of the value pointed to. This parameter is ignored unless this function returns true.
+       * @return True if the index refers to an existing value, false otherwise.
+       */
+      bool get(const index_t index, uint8_t * const value, const size_t sizeBytes) const;
+
+      /**
+       * @brief Gets a pointer to the value referred to by the index from the table.
+
+       * @param[In] T The type of the value to get from the table. The type need not be TriviallyCopyable, since this function doesn't do any copying, but it probably should be for symmetry with the put() functions.
+       * @param[In] index The index that refers to the value.
+       * @return A pointer to the value if the index refers to an existing value, nullptr otherwise.
+       */
+      template <typename T>
+      T* get(const index_t index) const;
+
+      /**
+       * @brief Checks if the given key maps to an index in this table and returns the index. Synchronized.
+
+       * @param[In] key The key to check.
+       * @param[out] index Optional. A pointer to write the index to. This parameter is ignored unless this function returns true.
+       * @return True if the given key maps to an index, false otherwise.
+       */
+      bool find(const key_t key, index_t * const index = nullptr) const;
+
+   private:
+      // Helper stuff used by the general (storage, sizeBytes) ctor.
+      typedef std::pair<data_t * const, const size_t> storage_and_size_pair_t;
+      CCData(const storage_and_size_pair_t storageAndSize);
+      static const storage_and_size_pair_t alignStorage(uint8_t * const storage, size_t sizeBytes);
+
+      /**
+       * @brief Checks if the given key maps to an index in this table and returns the index.
+       *        This function is NOT synchronized (hence unsafe).
+       *
+       * @param[In] key The key to check.
+       * @param[out] index Optional. A pointer to write the index to. This parameter is ignored unless this function returns true.
+       * @return True if the given key maps to an index, false otherwise.
+       */
+      bool find_unsafe(const key_t key, index_t * const index = nullptr) const;
+
+   private:
+      section_t         _data;         // Could be const were it not for the release() call in the dtor.
+      const size_t      _capacity;
+      size_t            _putIndex;
+      map_t             _mappings;
+      TR::Monitor      *_lock;
+      const bool        _releaseData;
+   };
+
+}
+
+#endif // OMR_CCDATA_INCL

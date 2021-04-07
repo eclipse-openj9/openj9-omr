janvrany/omr

RISC-V: move FOR_EACH_*_REGISTER() macros to OMRLinkage.hpp
This makes them available for use in other linkages too.

Signed-off-by: Jan Vrany <jan.vrany@fit.cvut.cz>

compiler/riscv/codegen/OMRLinkage.hpp
@@ -79,6 +79,39 @@ class RVMemoryArgument
 #define CallerAllocatesBackingStore 0x20
 #define RV_Reserved              0x40
 
+#define FOR_EACH_REGISTER(machine, block)                                        \
+   for (int regNum = TR::RealRegister::x0; regNum <= TR::RealRegister::x31; regNum++) \
+      {                                                                          \
+      TR::RealRegister *reg                                                      \
+                   = machine->getRealRegister((TR::RealRegister::RegNum)regNum); \
+      { block; }                                                                 \
+      }                                                                          \
+   for (int regNum = TR::RealRegister::f0; regNum <= TR::RealRegister::f31; regNum++) \
+      {                                                                          \
+      TR::RealRegister *reg                                                      \
+                   = machine->getRealRegister((TR::RealRegister::RegNum)regNum); \
+      { block; }                                                                 \
+      }
+
+#define FOR_EACH_RESERVED_REGISTER(machine, props, block)                        \
+   FOR_EACH_REGISTER(machine,                                                    \
+   if (props._registerFlags[(TR::RealRegister::RegNum)regNum] & RV_Reserved)     \
+      { block; }                                                                 \
+   )
+
+#define FOR_EACH_CALLEE_SAVED_REGISTER(machine, props, block)                    \
+   FOR_EACH_REGISTER(machine,                                                    \
+   if (props._registerFlags[(TR::RealRegister::RegNum)regNum] == Preserved)      \
+      { block; }                                                                 \
+   )
+
+#define FOR_EACH_ASSIGNED_CALLEE_SAVED_REGISTER(machine, props, block)           \
+   FOR_EACH_CALLEE_SAVED_REGISTER(machine, props,                                \
+   if (reg->getHasBeenAssignedInMethod())                                        \
+      { block; }                                                                 \
+   )
+
+
 struct RVLinkageProperties
    {
    uint32_t _properties;

eclipse-openj9/openj9-omr

compiler/riscv/codegen/OMRLinkage.hpp
@@ -79,6 +79,39 @@ class RVMemoryArgument
 #define CallerAllocatesBackingStore 0x20
 #define RV_Reserved                 0x40
 
+#define FOR_EACH_REGISTER(machine, block)                                        \
+   for (int regNum = TR::RealRegister::FirstGPR; regNum <= TR::RealRegister::LastGPR; regNum++) \
+      {                                                                          \
+      TR::RealRegister *reg                                                      \
+                   = machine->getRealRegister((TR::RealRegister::RegNum)regNum); \
+      { block; }                                                                 \
+      }                                                                          \
+   for (int regNum = TR::RealRegister::FirstFPR; regNum <= TR::RealRegister::FirstFPR; regNum++) \
+      {                                                                          \
+      TR::RealRegister *reg                                                      \
+                   = machine->getRealRegister((TR::RealRegister::RegNum)regNum); \
+      { block; }                                                                 \
+      }
+
+#define FOR_EACH_RESERVED_REGISTER(machine, props, block)                        \
+   FOR_EACH_REGISTER(machine,                                                    \
+   if (props._registerFlags[(TR::RealRegister::RegNum)regNum] & RV_Reserved)     \
+      { block; }                                                                 \
+   )
+
+#define FOR_EACH_CALLEE_SAVED_REGISTER(machine, props, block)                    \
+   FOR_EACH_REGISTER(machine,                                                    \
+   if (props._registerFlags[(TR::RealRegister::RegNum)regNum] == Preserved)      \
+      { block; }                                                                 \
+   )
+
+#define FOR_EACH_ASSIGNED_CALLEE_SAVED_REGISTER(machine, props, block)           \
+   FOR_EACH_CALLEE_SAVED_REGISTER(machine, props,                                \
+   if (reg->getHasBeenAssignedInMethod())                                        \
+      { block; }                                                                 \
+   )
+
+
 struct RVLinkageProperties
    {
    uint32_t _properties;
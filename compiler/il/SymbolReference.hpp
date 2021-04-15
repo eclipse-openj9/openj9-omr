1c1853ce39dc152d1ff7e8b1cefb3eaee08cd8e4
Tao Guan <james_mango@yahoo.com>
2021-04-15 14:45:30 -0400

    Add default constructor for SymbolReference
    Signed-off-by: Tao Guan <james_mango@yahoo.com>

--- compiler/il/SymbolReference.hpp
+++ compiler/il/SymbolReference.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -40,6 +40,7 @@ class OMR_EXTENSIBLE SymbolReference : public OMR::SymbolReferenceConnector
    {
 
 public:
+   SymbolReference() : OMR::SymbolReferenceConnector() {}
 
    SymbolReference(TR::SymbolReferenceTable * symRefTab) :
       OMR::SymbolReferenceConnector(symRefTab) {}

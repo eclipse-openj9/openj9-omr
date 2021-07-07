66d9bde92a02e4d3397af160b81c14c070f17896
Gita Koblents <koblents@ca.ibm.com>
2021-07-06 20:31:56 -0400

    Don't use once read symbol heuristics in UseDef at warm
        - Use onceReadSymbolsIndices at < warm since it helps performance
          without considerably increasing compile time

--- compiler/optimizer/UseDefInfo.cpp
+++ compiler/optimizer/UseDefInfo.cpp
@@ -152,9 +152,10 @@ void TR_UseDefInfo::prepareUseDefInfo(bool requiresGlobals, bool prefersGlobals,
    TR::SymbolReferenceTable *symRefTab = comp()->getSymRefTab();
    int32_t numSymRefs = comp()->getSymRefCount();
 
+
    if (_hasLoadsAsDefs &&
        !cannotOmitTrivialDefs &&
-       (comp()->getMethodHotness() < hot))
+       (comp()->getMethodHotness() < warm))
       {
       for (int32_t j = 0; j < numSymRefs; j++)
          {

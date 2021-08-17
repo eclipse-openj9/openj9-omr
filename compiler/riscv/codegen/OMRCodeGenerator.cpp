b3cb37e02b7579e2b408ae26f1c173724e3be338
Jan Vrany <jan@vrany.io>
2021-08-17 12:49:48 +0100

    RISC-V: implement virtual guard noping

--- compiler/riscv/codegen/OMRCodeGenerator.cpp
+++ compiler/riscv/codegen/OMRCodeGenerator.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 IBM Corp. and others
+ * Copyright (c) 2019, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -71,6 +71,8 @@ OMR::RV::CodeGenerator::initialize()
    cg->setLastGlobalFPR(TR::Machine::getLastGlobalFPRRegisterNumber());
 
    cg->getLinkage()->initRVRealRegisterLinkage();
+
+   cg->setSupportsVirtualGuardNOPing();
 
    _numberBytesReadInaccessible = 0;
    _numberBytesWriteInaccessible = 0;

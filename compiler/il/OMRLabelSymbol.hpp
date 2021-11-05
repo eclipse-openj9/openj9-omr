a54f109ee8242f6843466a00a64cbfa14123925d
Daryl Maier <maier@ca.ibm.com>
2021-11-05 10:51:42 -0400

    Remove unused LabelSymbol nullCompilationId
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/il/OMRLabelSymbol.hpp
+++ compiler/il/OMRLabelSymbol.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corp. and others
+ * Copyright (c) 2000, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -50,8 +50,6 @@ namespace TR { class StaticSymbol; }
 namespace TR { class SymbolReference; }
 template <class T> class List;
 
-typedef uintptr_t TR_UniqueCompilationId;
-const TR_UniqueCompilationId nullCompilationId(~(TR_UniqueCompilationId) 0);
 
 namespace OMR
 {

9cf62a88a1d4b69a019b79fbc8d81752444b9b68
Jan Vrany <jan@vrany.io>
2021-12-10 15:01:39 +0000

    RISC-V: add CPU class

---
+++ compiler/riscv/env/OMRCPU.cpp
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2022 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#include "env/CPU.hpp"
+#include "env/CompilerEnv.hpp"
+#include "env/jittypes.h"
+#include "omrport.h"
+
+bool
+OMR::RV::CPU::isTargetWithinUnconditionalBranchImmediateRange(intptr_t targetAddress, intptr_t sourceAddress)
+   {
+   intptr_t range = targetAddress - sourceAddress;
+   return range <= self()->maxUnconditionalBranchImmediateForwardOffset() &&
+          range >= self()->maxUnconditionalBranchImmediateBackwardOffset();
+   }
+
+bool
+OMR::RV::CPU::supportsFeature(uint32_t feature)
+   {
+   if (TR::Compiler->omrPortLib == NULL)
+      {
+      return false;
+      }
+
+   OMRPORT_ACCESS_FROM_OMRPORT(TR::Compiler->omrPortLib);
+   return (TRUE == omrsysinfo_processor_has_feature(&_processorDescription, feature));
+   }
+
+const char*
+OMR::RV::CPU::getProcessorName()
+   {
+   const char* returnString = "";
+   switch(_processorDescription.processor)
+      {
+      case OMR_PROCESOR_RISCV64_UNKNOWN:
+         returnString = "Unknown RV64G processor";
+         break;
+      default:
+         returnString = "Unknown RISC-V processor";
+         break;
+      }
+   return returnString;
+   }

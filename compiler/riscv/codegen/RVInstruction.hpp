a34dd3e0552e75673bfea325318a5193380a7965
Jan Vrany <jan@vrany.io>
2021-12-21 15:59:38 +0000

    RISC-V: move instruction encoding utilities to RVInstructionUtils.hpp
    This is to allow other code (such as snippets, trampoline code and so on)
    to use them to encode instructions. In a future, more utilities may be
    added.

--- compiler/riscv/codegen/RVInstruction.hpp
+++ compiler/riscv/codegen/RVInstruction.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 IBM Corp. and others
+ * Copyright (c) 2019, 2023 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -22,106 +22,17 @@
 #ifndef RVINSTRUCTION_INCL
 #define RVINSTRUCTION_INCL
 
-#include <stddef.h>
-#include <stdint.h>
-#include <riscv.h>
+
 
 #include "codegen/CodeGenerator.hpp"
-#include "codegen/Register.hpp"
-#include "codegen/InstOpCode.hpp"
 #include "codegen/Instruction.hpp"
+#include "codegen/RVInstructionUtils.hpp"
 #include "codegen/MemoryReference.hpp"
 #include "il/LabelSymbol.hpp"
 #include "infra/Assert.hpp"
 
 class TR_VirtualGuardSite;
 namespace TR { class SymbolReference; }
-
-#define RISCV_INSTRUCTION_LENGTH 4
-
-static inline uint32_t
-TR_RISCV_RTYPE(uint32_t insn, uint32_t rd, uint32_t rs1, uint32_t rs2)
-   {
-   return ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2));
-   }
-
-static inline uint32_t
-TR_RISCV_RTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, TR::Register *rs1, TR::Register *rs2)
-   {
-   return TR_RISCV_RTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode());
-   }
-
-
-static inline uint32_t
-TR_RISCV_ITYPE(uint32_t insn, uint32_t rd, uint32_t rs1, uint32_t imm)
-   {
-   return ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ENCODE_ITYPE_IMM(imm));
-   }
-
-static inline uint32_t
-TR_RISCV_ITYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, TR::Register *rs1, uint32_t imm)
-   {
-   return TR_RISCV_ITYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), toRealRegister(rs1)->binaryRegCode(), imm);
-   }
-
-
-static inline uint32_t
-TR_RISCV_STYPE(uint32_t insn, uint32_t rs1, uint32_t rs2, uint32_t imm)
-   {
-   return ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_STYPE_IMM(imm));
-   }
-
-static inline uint32_t
-TR_RISCV_STYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rs1, TR::Register *rs2, uint32_t imm)
-   {
-   return TR_RISCV_STYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode(), imm);
-   }
-
-
-static inline uint32_t
-TR_RISCV_SBTYPE(uint32_t insn, uint32_t rs1, uint32_t rs2, uint32_t imm)
-   {
-   return ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_SBTYPE_IMM(imm));
-   }
-
-static inline uint32_t
-TR_RISCV_SBTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rs1, TR::Register *rs2, uint32_t imm)
-   {
-   return TR_RISCV_SBTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode(), imm);
-   }
-
-
-static inline uint32_t
-TR_RISCV_UTYPE(uint32_t insn, uint32_t rd, uint32_t bigimm)
-   {
-   return ((insn) | ((rd) << OP_SH_RD) | ENCODE_UTYPE_IMM(bigimm));
-   }
-
-static inline uint32_t
-TR_RISCV_UTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, uint32_t bigimm)
-   {
-   return TR_RISCV_UTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), bigimm);
-   }
-
-
-static inline uint32_t
-TR_RISCV_UJTYPE(uint32_t insn, uint32_t rd, uint32_t target)
-   {
-   return ((insn) | ((rd) << OP_SH_RD) | ENCODE_UJTYPE_IMM(target));
-   }
-
-static inline uint32_t
-TR_RISCV_UJTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, uint32_t target)
-   {
-   return TR_RISCV_UJTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), target);
-   }
-
-static inline uint32_t
-TR_RISCV_UJTYPE(TR::InstOpCode &insn, TR::Register *rd, uint32_t target)
-   {
-   return TR_RISCV_UJTYPE(insn.getMnemonic(), rd, target);
-   }
-
 
 namespace TR
 {
---
+++ compiler/riscv/codegen/RVInstructionUtils.hpp
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2023 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef RVINSTRUCTIONUTILS_INCL
+#define RVINSTRUCTIONUTILS_INCL
+
+#include <stddef.h>
+#include <stdint.h>
+#include <riscv.h>
+
+#include "codegen/Register.hpp"
+#include "codegen/RealRegister.hpp"
+#include "codegen/InstOpCode.hpp"
+
+#define RISCV_INSTRUCTION_LENGTH 4
+
+static inline uint32_t
+TR_RISCV_RTYPE(uint32_t insn, uint32_t rd, uint32_t rs1, uint32_t rs2)
+   {
+   return ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2));
+   }
+
+static inline uint32_t
+TR_RISCV_RTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, TR::Register *rs1, TR::Register *rs2)
+   {
+   return TR_RISCV_RTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode());
+   }
+
+
+static inline uint32_t
+TR_RISCV_ITYPE(uint32_t insn, uint32_t rd, uint32_t rs1, uint32_t imm)
+   {
+   return ((insn) | ((rd) << OP_SH_RD) | ((rs1) << OP_SH_RS1) | ENCODE_ITYPE_IMM(imm));
+   }
+
+static inline uint32_t
+TR_RISCV_ITYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, TR::Register *rs1, uint32_t imm)
+   {
+   return TR_RISCV_ITYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), toRealRegister(rs1)->binaryRegCode(), imm);
+   }
+
+
+static inline uint32_t
+TR_RISCV_STYPE(uint32_t insn, uint32_t rs1, uint32_t rs2, uint32_t imm)
+   {
+   return ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_STYPE_IMM(imm));
+   }
+
+static inline uint32_t
+TR_RISCV_STYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rs1, TR::Register *rs2, uint32_t imm)
+   {
+   return TR_RISCV_STYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode(), imm);
+   }
+
+
+static inline uint32_t
+TR_RISCV_SBTYPE(uint32_t insn, uint32_t rs1, uint32_t rs2, uint32_t imm)
+   {
+   return ((insn) | ((rs1) << OP_SH_RS1) | ((rs2) << OP_SH_RS2) | ENCODE_SBTYPE_IMM(imm));
+   }
+
+static inline uint32_t
+TR_RISCV_SBTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rs1, TR::Register *rs2, uint32_t imm)
+   {
+   return TR_RISCV_SBTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rs1)->binaryRegCode(), toRealRegister(rs2)->binaryRegCode(), imm);
+   }
+
+
+static inline uint32_t
+TR_RISCV_UTYPE(uint32_t insn, uint32_t rd, uint32_t bigimm)
+   {
+   return ((insn) | ((rd) << OP_SH_RD) | ENCODE_UTYPE_IMM(bigimm));
+   }
+
+static inline uint32_t
+TR_RISCV_UTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, uint32_t bigimm)
+   {
+   return TR_RISCV_UTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), bigimm);
+   }
+
+
+static inline uint32_t
+TR_RISCV_UJTYPE(uint32_t insn, uint32_t rd, uint32_t target)
+   {
+   return ((insn) | ((rd) << OP_SH_RD) | ENCODE_UJTYPE_IMM(target));
+   }
+
+static inline uint32_t
+TR_RISCV_UJTYPE(TR::InstOpCode::Mnemonic insn, TR::Register *rd, uint32_t target)
+   {
+   return TR_RISCV_UJTYPE(TR::InstOpCode::getOpCodeBinaryEncoding(insn), toRealRegister(rd)->binaryRegCode(), target);
+   }
+
+static inline uint32_t
+TR_RISCV_UJTYPE(TR::InstOpCode &insn, TR::Register *rd, uint32_t target)
+   {
+   return TR_RISCV_UJTYPE(insn.getMnemonic(), rd, target);
+   }
+
+#endif // RVINSTRUCTIONUTILS_INCL

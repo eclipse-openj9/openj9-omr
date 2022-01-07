2da84d958096441983cda8bbea99fc527ce21e10
Tao Guan <james_mango@yahoo.com>
2022-01-07 18:28:03 -0500

    Remove declared only methods
    Signed-off-by: Tao Guan <james_mango@yahoo.com>

--- compiler/infra/OMRCfg.hpp
+++ compiler/infra/OMRCfg.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -169,9 +169,6 @@ class CFG
    int32_t allocateNodeNumber() {return _nextNodeNumber++;}
 
    TR::CFGNode *addNode(TR::CFGNode *n, TR_RegionStructure *parent = NULL, bool isEntryInParent = false);
-
-   uint32_t addStructureSubGraphNodes (TR_StructureSubGraphNode *node);
-   void removeStructureSubGraphNodes(TR_StructureSubGraphNode *node);
 
    void addEdge(TR::CFGEdge *e);
 

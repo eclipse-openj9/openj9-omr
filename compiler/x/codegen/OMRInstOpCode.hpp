BradleyWood/omr

Define and use YMM/ZMM src/target register properties for EVEX VEX_L256 and VEX_L512 instructions
Signed-off-by: BradleyWood <bradley.wood@ibm.com>

compiler/x/codegen/OMRInstOpCode.hpp
@@ -104,6 +104,10 @@ namespace TR { class Register; }
 #define IA32OpProp1_FusableCompare            0x00100000
 #define IA32OpProp1_NeedsXacquirePrefix       0x00400000
 #define IA32OpProp1_NeedsXreleasePrefix       0x00800000
+#define IA32OpProp1_YMMSource                 0x01000000
+#define IA32OpProp1_YMMTarget                 0x02000000
+#define IA32OpProp1_ZMMSource                 0x04000000
+#define IA32OpProp1_ZMMTarget                 0x08000000
 ////////////////////
 //
 // AMD64 flags
@@ -348,6 +352,10 @@ class InstOpCode: public OMR::InstOpCode
    inline uint32_t hasDoubleWordTarget()           const {return _properties1[_mnemonic] & IA32OpProp1_DoubleWordTarget;}
    inline uint32_t hasXMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_XMMSource;}
    inline uint32_t hasXMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_XMMTarget;}
+   inline uint32_t hasYMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_YMMSource;}
+   inline uint32_t hasYMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_YMMTarget;}
+   inline uint32_t hasZMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_ZMMSource;}
+   inline uint32_t hasZMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_ZMMTarget;}
    inline uint32_t isPseudoOp()                    const {return _properties1[_mnemonic] & IA32OpProp1_PseudoOp;}
    inline uint32_t needsRepPrefix()                const {return _properties1[_mnemonic] & IA32OpProp1_NeedsRepPrefix;}
    inline uint32_t needsLockPrefix()               const {return _properties1[_mnemonic] & IA32OpProp1_NeedsLockPrefix;}
@@ -384,6 +392,7 @@ class InstOpCode: public OMR::InstOpCode
    inline uint32_t targetRegIsImplicit()           const { return _properties1[_mnemonic] & IA32OpProp1_TargetRegIsImplicit;}
    inline uint32_t sourceRegIsImplicit()           const { return _properties1[_mnemonic] & IA32OpProp1_SourceRegIsImplicit;}
    inline uint32_t isFusableCompare()              const { return _properties1[_mnemonic] & IA32OpProp1_FusableCompare; }
+   inline bool     isEvexInstruction()             const { return _binaries[_mnemonic].vex_l >> 2; }
 
    inline bool isSetRegInstruction() const
       {

eclipse-openj9/openj9-omr

compiler/x/codegen/OMRInstOpCode.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2021 IBM Corp. and others
+ * Copyright (c) 2021, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -102,8 +102,14 @@ namespace TR { class Register; }
 #define IA32OpProp1_SourceRegIsImplicit       0x00040000
 #define IA32OpProp1_TargetRegIsImplicit       0x00080000
 #define IA32OpProp1_FusableCompare            0x00100000
-#define IA32OpProp1_NeedsXacquirePrefix       0x00400000
-#define IA32OpProp1_NeedsXreleasePrefix       0x00800000
+#define IA32OpProp1_VectorIntMask             0x00200000
+#define IA32OpProp1_ZMMTarget                 0x00400000
+// Available                                  0x00800000
+#define IA32OpProp1_YMMSource                 0x01000000
+#define IA32OpProp1_YMMTarget                 0x02000000
+#define IA32OpProp1_ZMMSource                 0x04000000
+#define IA32OpProp1_VectorLongMask            0x10000000
+
 ////////////////////
 //
 // AMD64 flags
@@ -348,11 +354,13 @@ class InstOpCode: public OMR::InstOpCode
    inline uint32_t hasDoubleWordTarget()           const {return _properties1[_mnemonic] & IA32OpProp1_DoubleWordTarget;}
    inline uint32_t hasXMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_XMMSource;}
    inline uint32_t hasXMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_XMMTarget;}
+   inline uint32_t hasYMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_YMMSource;}
+   inline uint32_t hasYMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_YMMTarget;}
+   inline uint32_t hasZMMSource()                  const {return _properties1[_mnemonic] & IA32OpProp1_ZMMSource;}
+   inline uint32_t hasZMMTarget()                  const {return _properties1[_mnemonic] & IA32OpProp1_ZMMTarget;}
    inline uint32_t isPseudoOp()                    const {return _properties1[_mnemonic] & IA32OpProp1_PseudoOp;}
    inline uint32_t needsRepPrefix()                const {return _properties1[_mnemonic] & IA32OpProp1_NeedsRepPrefix;}
    inline uint32_t needsLockPrefix()               const {return _properties1[_mnemonic] & IA32OpProp1_NeedsLockPrefix;}
-   inline uint32_t needsXacquirePrefix()           const {return _properties1[_mnemonic] & IA32OpProp1_NeedsXacquirePrefix;}
-   inline uint32_t needsXreleasePrefix()           const {return _properties1[_mnemonic] & IA32OpProp1_NeedsXreleasePrefix;}
    inline uint32_t clearsUpperBits()               const {return hasIntTarget() && modifiesTarget();}
    inline uint32_t setsUpperBits()                 const {return hasLongTarget() && modifiesTarget();}
    inline uint32_t hasTargetRegisterInOpcode()     const {return _properties[_mnemonic] & IA32OpProp_TargetRegisterInOpcode;}
@@ -384,6 +392,7 @@ class InstOpCode: public OMR::InstOpCode
    inline uint32_t targetRegIsImplicit()           const { return _properties1[_mnemonic] & IA32OpProp1_TargetRegIsImplicit;}
    inline uint32_t sourceRegIsImplicit()           const { return _properties1[_mnemonic] & IA32OpProp1_SourceRegIsImplicit;}
    inline uint32_t isFusableCompare()              const { return _properties1[_mnemonic] & IA32OpProp1_FusableCompare; }
+   inline bool     isEvexInstruction()             const { return _binaries[_mnemonic].vex_l >> 2 == 1; }
 
    inline bool isSetRegInstruction() const
       {
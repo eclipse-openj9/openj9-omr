BradleyWood/omr

Support binary encoding method override on x86
Signed-off-by: BradleyWood <bradley.wood@ibm.com>

compiler/x/codegen/OMRInstOpCode.cpp
@@ -71,8 +71,10 @@ void OMR::X86::InstOpCode::trackUpperBitsOnReg(TR::Register *reg, TR::CodeGenera
       }
    }
 
-template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpCode_t::encode(typename TBuffer::cursor_t cursor, uint8_t rexbits) const
+template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpCode_t::encode(typename TBuffer::cursor_t cursor, uint8_t rexbits, OMR::X86::Encoding encoding) const
    {
+   uint32_t enc = encoding == OMR::X86::Default ? vex_l : encoding;
+
    TBuffer buffer(cursor);
    if (isX87())
       {
@@ -82,21 +84,21 @@ template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpC
       buffer.append((uint8_t)((modrm_opcode << 5) | (modrm_form << 3) | immediate_size));
       return buffer;
       }
+
    // Prefixes
    TR::Instruction::REX rex(rexbits);
    rex.W = rex_w;
-   // Use AVX if possible
 
    TR::Compilation *comp = TR::comp();
    TR_ASSERT_FATAL(comp->compileRelocatableCode() || comp->isOutOfProcessCompilation() || comp->compilePortableCode() || comp->target().cpu.supportsAVX() == TR::CodeGenerator::getX86ProcessorInfo().supportsAVX(), "supportsAVX() failed\n");
 
-   if (supportsAVX() && comp->target().cpu.supportsAVX())
+   if (enc != VEX_L___)
       {
-      if (vex_l >> 2)
+      if (enc >> 2)
          {
          TR::Instruction::EVEX vex(rex, modrm_opcode);
          vex.mm = escape;
-         vex.L = vex_l & 0x3;
+         vex.L = enc & 0x3;
          vex.p = prefixes;
          vex.opcode = opcode;
          buffer.append(vex);
@@ -105,7 +107,7 @@ template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpC
          {
          TR::Instruction::VEX<3> vex(rex, modrm_opcode);
          vex.m = escape;
-         vex.L = vex_l;
+         vex.L = enc;
          vex.p = prefixes;
          vex.opcode = opcode;
 
@@ -217,13 +219,13 @@ void OMR::X86::InstOpCode::CheckAndFinishGroup07(uint8_t* cursor) const
       }
    }
 
-uint8_t OMR::X86::InstOpCode::length(uint8_t rex) const
+uint8_t OMR::X86::InstOpCode::length(OMR::X86::Encoding encoding, uint8_t rex) const
    {
-   return encode<Estimator>(0, rex);
+   return encode<Estimator>(0, rex, encoding);
    }
-uint8_t* OMR::X86::InstOpCode::binary(uint8_t* cursor, uint8_t rex) const
+uint8_t* OMR::X86::InstOpCode::binary(uint8_t* cursor, OMR::X86::Encoding encoding, uint8_t rex) const
    {
-   uint8_t* ret = encode<Writer>(cursor, rex);
+   uint8_t* ret = encode<Writer>(cursor, rex, encoding);
    CheckAndFinishGroup07(ret);
    return ret;
    }

compiler/x/codegen/OMRInstOpCode.hpp
@@ -154,6 +154,17 @@ namespace OMR
 namespace X86
 {
 
+typedef enum
+   {
+   VEX_L128 = 0x0,
+   VEX_L256 = 0x1,
+   Default  = 0x2,
+   Legacy   = 0x3,
+   EVEX_L128 = 0x4,
+   EVEX_L256 = 0x5,
+   EVEX_L512 = 0x6
+   } Encoding;
+
 class InstOpCode: public OMR::InstOpCode
    {
    enum TR_OpCodeVEX_L : uint8_t
@@ -272,7 +283,7 @@ class InstOpCode: public OMR::InstOpCode
          return (escape == ESCAPE_0F__) && (opcode == 0x01);
          }
       // TBuffer should only be one of the two: Estimator when calculating length, and Writer when generating binaries.
-      template <class TBuffer> typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rexbits) const;
+      template <class TBuffer> typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rexbits, OMR::X86::Encoding encoding) const;
       // finalize instruction prefix information, currently only in-use for AVX instructions for VEX.vvvv field
       void finalize(uint8_t* cursor) const;
       };
@@ -311,9 +322,9 @@ class InstOpCode: public OMR::InstOpCode
          }
       };
    // TBuffer should only be one of the two: Estimator when calculating length, and Writer when generating binaries.
-   template <class TBuffer> inline typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rex) const
+   template <class TBuffer> inline typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rex, OMR::X86::Encoding encoding) const
       {
-      return isPseudoOp() ? cursor : info().encode<TBuffer>(cursor, rex);
+      return isPseudoOp() ? cursor : info().encode<TBuffer>(cursor, rex, encoding);
       }
    // Instructions from Group 7 OpCode Extensions need special handling as they requires specific low 3 bits of ModR/M byte
    inline void CheckAndFinishGroup07(uint8_t* cursor) const;
@@ -437,8 +448,8 @@ class InstOpCode: public OMR::InstOpCode
          }
       }
 
-   uint8_t length(uint8_t rex = 0) const;
-   uint8_t* binary(uint8_t* cursor, uint8_t rex = 0) const;
+   uint8_t length(OMR::X86::Encoding encoding, uint8_t rex = 0) const;
+   uint8_t* binary(uint8_t* cursor, OMR::X86::Encoding encoding, uint8_t rex = 0) const;
    void finalize(uint8_t* cursor) const;
    void convertLongBranchToShort()
       { // input must be a long branch in range OMR::InstOpCode::JA4 - OMR::InstOpCode::JMP4

eclipse-openj9/openj9-omr

compiler/x/codegen/OMRInstOpCode.cpp
@@ -71,8 +71,16 @@ void OMR::X86::InstOpCode::trackUpperBitsOnReg(TR::Register *reg, TR::CodeGenera
       }
    }
 
-template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpCode_t::encode(typename TBuffer::cursor_t cursor, uint8_t rexbits) const
+template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpCode_t::encode(typename TBuffer::cursor_t cursor, OMR::X86::Encoding encoding, uint8_t rexbits) const
    {
+   TR::Compilation *comp = TR::comp();
+   uint32_t enc = encoding;
+
+   if (encoding == OMR::X86::Default)
+      {
+      enc = comp->target().cpu.supportsAVX() ? vex_l : OMR::X86::Legacy;
+      }
+
    TBuffer buffer(cursor);
    if (isX87())
       {
@@ -82,21 +90,20 @@ template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpC
       buffer.append((uint8_t)((modrm_opcode << 5) | (modrm_form << 3) | immediate_size));
       return buffer;
       }
+
    // Prefixes
    TR::Instruction::REX rex(rexbits);
    rex.W = rex_w;
-   // Use AVX if possible
 
-   TR::Compilation *comp = TR::comp();
    TR_ASSERT_FATAL(comp->compileRelocatableCode() || comp->isOutOfProcessCompilation() || comp->compilePortableCode() || comp->target().cpu.supportsAVX() == TR::CodeGenerator::getX86ProcessorInfo().supportsAVX(), "supportsAVX() failed\n");
 
-   if (supportsAVX() && comp->target().cpu.supportsAVX())
+   if (enc != VEX_L___)
       {
-      if (vex_l >> 2)
+      if (enc >> 2)
          {
          TR::Instruction::EVEX vex(rex, modrm_opcode);
          vex.mm = escape;
-         vex.L = vex_l & 0x3;
+         vex.L = enc & 0x3;
          vex.p = prefixes;
          vex.opcode = opcode;
          buffer.append(vex);
@@ -105,7 +112,7 @@ template <typename TBuffer> typename TBuffer::cursor_t OMR::X86::InstOpCode::OpC
          {
          TR::Instruction::VEX<3> vex(rex, modrm_opcode);
          vex.m = escape;
-         vex.L = vex_l;
+         vex.L = enc;
          vex.p = prefixes;
          vex.opcode = opcode;
 
@@ -217,13 +224,13 @@ void OMR::X86::InstOpCode::CheckAndFinishGroup07(uint8_t* cursor) const
       }
    }
 
-uint8_t OMR::X86::InstOpCode::length(uint8_t rex) const
+uint8_t OMR::X86::InstOpCode::length(OMR::X86::Encoding encoding, uint8_t rex) const
    {
-   return encode<Estimator>(0, rex);
+   return encode<Estimator>(0, encoding, rex);
    }
-uint8_t* OMR::X86::InstOpCode::binary(uint8_t* cursor, uint8_t rex) const
+uint8_t* OMR::X86::InstOpCode::binary(uint8_t* cursor, OMR::X86::Encoding encoding, uint8_t rex) const
    {
-   uint8_t* ret = encode<Writer>(cursor, rex);
+   uint8_t* ret = encode<Writer>(cursor, encoding, rex);
    CheckAndFinishGroup07(ret);
    return ret;
    }

compiler/x/codegen/OMRInstOpCode.hpp
@@ -154,6 +154,17 @@ namespace OMR
 namespace X86
 {
 
+typedef enum
+   {
+   VEX_L128 = 0x0,
+   VEX_L256 = 0x1,
+   Default  = 0x2,
+   Legacy   = 0x3,
+   EVEX_L128 = 0x4,
+   EVEX_L256 = 0x5,
+   EVEX_L512 = 0x6
+   } Encoding;
+
 class InstOpCode: public OMR::InstOpCode
    {
    enum TR_OpCodeVEX_L : uint8_t
@@ -272,7 +283,7 @@ class InstOpCode: public OMR::InstOpCode
          return (escape == ESCAPE_0F__) && (opcode == 0x01);
          }
       // TBuffer should only be one of the two: Estimator when calculating length, and Writer when generating binaries.
-      template <class TBuffer> typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rexbits) const;
+      template <class TBuffer> typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, OMR::X86::Encoding encoding, uint8_t rexbits) const;
       // finalize instruction prefix information, currently only in-use for AVX instructions for VEX.vvvv field
       void finalize(uint8_t* cursor) const;
       };
@@ -311,9 +322,9 @@ class InstOpCode: public OMR::InstOpCode
          }
       };
    // TBuffer should only be one of the two: Estimator when calculating length, and Writer when generating binaries.
-   template <class TBuffer> inline typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, uint8_t rex) const
+   template <class TBuffer> inline typename TBuffer::cursor_t encode(typename TBuffer::cursor_t cursor, OMR::X86::Encoding encoding, uint8_t rex) const
       {
-      return isPseudoOp() ? cursor : info().encode<TBuffer>(cursor, rex);
+      return isPseudoOp() ? cursor : info().encode<TBuffer>(cursor, encoding, rex);
       }
    // Instructions from Group 7 OpCode Extensions need special handling as they requires specific low 3 bits of ModR/M byte
    inline void CheckAndFinishGroup07(uint8_t* cursor) const;
@@ -437,8 +448,8 @@ class InstOpCode: public OMR::InstOpCode
          }
       }
 
-   uint8_t length(uint8_t rex = 0) const;
-   uint8_t* binary(uint8_t* cursor, uint8_t rex = 0) const;
+   uint8_t length(OMR::X86::Encoding encoding, uint8_t rex = 0) const;
+   uint8_t* binary(uint8_t* cursor, OMR::X86::Encoding encoding, uint8_t rex = 0) const;
    void finalize(uint8_t* cursor) const;
    void convertLongBranchToShort()
       { // input must be a long branch in range OMR::InstOpCode::JA4 - OMR::InstOpCode::JMP4
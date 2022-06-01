b0e8c875f66abfa51e575205c0f99c33b2b71112
Eric Yang <eric.yang@ibm.com>
2022-06-01 10:46:29 -0700

    Add test cases for the cgroup memory limit functions
    Add a new test for omrsysinfo_cgroup_is_memlimit_set and additional test
    cases for omrsysinfo_cgroup_get_memlimit.
    
    Issue: #1281
    Signed-off-by: Eric Yang <eric.yang@ibm.com>

--- fvtest/porttest/si.cpp
+++ fvtest/porttest/si.cpp
@@ -2383,6 +2383,8 @@ protected:
 	const static std::map<std::string, uint64_t> supportedSubsystems;
 	constexpr static char CGROUP_MOUNT_POINT[] = "/sys/fs/cgroup";
 	static uint64_t available;
+	static uint64_t memLimit;
+	static std::string memLimitString;
 	static bool setUpSucceeded;
 
 	/* Initialize static vars. */
@@ -2435,6 +2437,11 @@ protected:
 					}
 				}
 			}
+
+			if (OMR_ARE_ANY_BITS_SET(available, OMR_CGROUP_SUBSYSTEM_MEMORY)) {
+				std::ifstream memLimitFile(CGROUP_MOUNT_POINT + std::string("/memory") + memCgroup + "/memory.limit_in_bytes");
+				ASSERT_TRUE(memLimitFile >> memLimit);
+			}
 		} else {
 			ASSERT_TRUE((bool)std::getline(cgroupFile, line));
 			std::regex v2Regex(R"(^0::(.+)$)");
@@ -2454,6 +2461,11 @@ protected:
 				if (supportedSubsystems.end() != it) {
 					available |= it->second;
 				}
+			}
+
+			if (OMR_ARE_ANY_BITS_SET(available, OMR_CGROUP_SUBSYSTEM_MEMORY)) {
+				std::ifstream memLimitFile(CGROUP_MOUNT_POINT + memCgroup + "/memory.max");
+				ASSERT_TRUE(memLimitFile >> memLimitString);
 			}
 		}
 		setUpSucceeded = true;
@@ -2512,6 +2524,8 @@ const std::map<std::string, uint64_t> CgroupTest::supportedSubsystems = {
 };
 constexpr char CgroupTest::CGROUP_MOUNT_POINT[];
 uint64_t CgroupTest::available = 0;
+uint64_t CgroupTest::memLimit = 0;
+std::string CgroupTest::memLimitString;
 bool CgroupTest::setUpSucceeded = false;
 #endif /* defined(LINUX) */
 
@@ -2671,26 +2685,18 @@ TEST_F(CgroupTest, sysinfo_cgroup_enable)
 /**
  * Test omrsysinfo_cgroup_get_memlimit.
  */
-TEST(PortSysinfoTest, sysinfo_cgroup_get_memlimit)
+TEST_F(CgroupTest, sysinfo_cgroup_get_memlimit)
 {
 	OMRPORT_ACCESS_FROM_OMRPORT(portTestEnv->getPortLibrary());
 	const char *testName = "omrsysinfo_cgroup_get_memlimit";
 	uint64_t cgroupMemLimit = 0;
 	int32_t rc = 0;
-	uint64_t enabledSubsystems = 0;
-	
+
 	reportTestEntry(OMRPORTLIB, testName);
 
-	rc = omrsysinfo_cgroup_get_memlimit(&cgroupMemLimit);
-
-#if !defined(LINUX)
-	if (OMRPORT_ERROR_SYSINFO_CGROUP_UNSUPPORTED_PLATFORM != rc) {
-		outputErrorMessage(PORTTEST_ERROR_ARGS, "omrsysinfo_cgroup_get_memlimit returned %d, expected %d on platform that does not support cgroups\n", rc, OMRPORT_ERROR_SYSINFO_CGROUP_UNSUPPORTED_PLATFORM);
-	}
-#endif
-
+#if defined(LINUX)
 	/* Compare sysinfo_get_physical_memory and sysinfo_cgroup_get_memlimit after enabling memory subsystem */
-	enabledSubsystems = omrsysinfo_cgroup_enable_subsystems(OMR_CGROUP_SUBSYSTEM_MEMORY);
+	uint64_t enabledSubsystems = omrsysinfo_cgroup_enable_subsystems(OMR_CGROUP_SUBSYSTEM_MEMORY);
 	if (OMR_ARE_ALL_BITS_SET(enabledSubsystems, OMR_CGROUP_SUBSYSTEM_MEMORY)) {
 		rc = omrsysinfo_cgroup_get_memlimit(&cgroupMemLimit);
 		if ((0 != rc) && (OMRPORT_ERROR_SYSINFO_CGROUP_MEMLIMIT_NOT_SET != rc)) {
@@ -2703,7 +2709,61 @@ TEST(PortSysinfoTest, sysinfo_cgroup_get_memlimit)
 				outputErrorMessage(PORTTEST_ERROR_ARGS, "Expected omrsysinfo_cgroup_get_memlimit and omrsysinfo_get_physical_memory to return same value, but omrsysinfo_cgroup_get_memlimit returned %ld and omrsysinfo_get_physical_memory returned %ld\n", cgroupMemLimit, physicalMemLimit);
 			}
 		}
+
+		if (CgroupTest::isV1Available) {
+			if (OMRPORT_ERROR_SYSINFO_CGROUP_MEMLIMIT_NOT_SET == rc) {
+				EXPECT_GT(CgroupTest::memLimit, omrsysinfo_get_physical_memory()) << "Cgroup memory is set but omrsysinfo_cgroup_get_memlimit returned not set";
+			} else if (0 == rc) {
+				EXPECT_EQ(CgroupTest::memLimit, cgroupMemLimit);
+			}
+		} else {
+			/* CgroupTest::isV2Available */
+			if (OMRPORT_ERROR_SYSINFO_CGROUP_MEMLIMIT_NOT_SET == rc) {
+				EXPECT_EQ(CgroupTest::memLimitString, "max") << "Cgroup memory is set but omrsysinfo_cgroup_get_memlimit returned not set";
+			} else if (0 == rc) {
+				EXPECT_EQ(std::stoull(CgroupTest::memLimitString), cgroupMemLimit);
+			}
+		}
 	}
+#else /* defined(LINUX) */
+	rc = omrsysinfo_cgroup_get_memlimit(&cgroupMemLimit);
+	if (OMRPORT_ERROR_SYSINFO_CGROUP_UNSUPPORTED_PLATFORM != rc) {
+		outputErrorMessage(PORTTEST_ERROR_ARGS, "omrsysinfo_cgroup_get_memlimit returned %d, expected %d on platform that does not support cgroups\n", rc, OMRPORT_ERROR_SYSINFO_CGROUP_UNSUPPORTED_PLATFORM);
+	}
+#endif /* defined(LINUX) */
+
+	reportTestExit(OMRPORTLIB, testName);
+	return;
+}
+
+/**
+ * Test omrsysinfo_cgroup_is_memlimit_set.
+ */
+TEST_F(CgroupTest, sysinfo_cgroup_is_memlimit_set)
+{
+	OMRPORT_ACCESS_FROM_OMRPORT(portTestEnv->getPortLibrary());
+	const char *testName = "omrsysinfo_cgroup_is_memlimit_set";
+	BOOLEAN result = FALSE;
+
+	reportTestEntry(OMRPORTLIB, testName);
+
+	result = omrsysinfo_cgroup_is_memlimit_set();
+
+#if defined(LINUX)
+	uint64_t enabledSubsystems = omrsysinfo_cgroup_enable_subsystems(OMR_CGROUP_SUBSYSTEM_MEMORY);
+	if (OMR_ARE_ALL_BITS_SET(enabledSubsystems, OMR_CGROUP_SUBSYSTEM_MEMORY)) {
+		if (CgroupTest::isV1Available) {
+			EXPECT_EQ(result, CgroupTest::memLimit <= omrsysinfo_get_physical_memory());
+		} else {
+			/* CgroupTest::isV2Available */
+			EXPECT_EQ(result, CgroupTest::memLimitString != "max");
+		}
+	}
+#else /* defined(LINUX) */
+	if (FALSE != result) {
+		outputErrorMessage(PORTTEST_ERROR_ARGS, "omrsysinfo_cgroup_is_memlimit_set returned TRUE on non-Linux");
+	}
+#endif /* defined(LINUX) */
 
 	reportTestExit(OMRPORTLIB, testName);
 	return;

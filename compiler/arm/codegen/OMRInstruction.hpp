3df5f694d02fd00b038eca777f5b7ad51e6c7780
Tao Guan <james_mango@yahoo.com>
2022-07-31 11:00:39 -0400

    Remove duplicate _opcode at arm and x level   the member not found in other platforms: aarch64, p, riscv, z   the member is currently at OMR::Instruction level with protected attribution
    Signed-off-by: Tao Guan <james_mango@yahoo.com>

--- compiler/arm/codegen/OMRInstruction.hpp
+++ compiler/arm/codegen/OMRInstruction.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2021 IBM Corp. and others
+ * Copyright (c) 2000, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -210,7 +210,6 @@ class OMR_EXTENSIBLE Instruction : public OMR::Instruction
 
 
    private:
-      TR::InstOpCode   _opcode;
       TR_ARMConditionCode                  _conditionCode;
       TR::RegisterDependencyConditions *_conditions;
       bool        _asyncBranch;

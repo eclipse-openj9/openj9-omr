BradleyWood/omr

Declare x86 write mask registers
Signed-off-by: BradleyWood <bradley.wood@ibm.com>

compiler/codegen/OMRCodeGenerator.hpp
@@ -1064,7 +1064,9 @@ public:
    TR::RegisterIterator *setGPRegisterIterator(TR::RegisterIterator *iter) {return (_gpRegisterIterator = iter);}
 
    TR::RegisterIterator *getFPRegisterIterator() {return _fpRegisterIterator;}
+   TR::RegisterIterator *getVMRegisterIterator() {return _vmRegisterIterator;}
    TR::RegisterIterator *setFPRegisterIterator(TR::RegisterIterator *iter) {return (_fpRegisterIterator = iter);}
+   TR::RegisterIterator *setVMRRegisterIterator(TR::RegisterIterator *iter) {return (_vmRegisterIterator = iter);}
 
    // X86 only
    uint32_t estimateBinaryLength(TR::MemoryReference *) { return 0; }
@@ -2050,6 +2052,7 @@ public:
 
    TR::RegisterIterator *_gpRegisterIterator;
    TR::RegisterIterator *_fpRegisterIterator;
+   TR::RegisterIterator *_vmRegisterIterator;
    TR_RegisterAssignmentFlags _regAssignFlags;
 
    uint32_t _preJitMethodEntrySize;

eclipse-openj9/openj9-omr

compiler/codegen/OMRCodeGenerator.hpp
@@ -1064,7 +1064,9 @@ public:
    TR::RegisterIterator *setGPRegisterIterator(TR::RegisterIterator *iter) {return (_gpRegisterIterator = iter);}
 
    TR::RegisterIterator *getFPRegisterIterator() {return _fpRegisterIterator;}
+   TR::RegisterIterator *getVMRegisterIterator() {return _vmRegisterIterator;}
    TR::RegisterIterator *setFPRegisterIterator(TR::RegisterIterator *iter) {return (_fpRegisterIterator = iter);}
+   TR::RegisterIterator *setVMRegisterIterator(TR::RegisterIterator *iter) {return (_vmRegisterIterator = iter);}
 
    // X86 only
    uint32_t estimateBinaryLength(TR::MemoryReference *) { return 0; }
@@ -2052,6 +2054,7 @@ public:
 
    TR::RegisterIterator *_gpRegisterIterator;
    TR::RegisterIterator *_fpRegisterIterator;
+   TR::RegisterIterator *_vmRegisterIterator;
    TR_RegisterAssignmentFlags _regAssignFlags;
 
    uint32_t _preJitMethodEntrySize;
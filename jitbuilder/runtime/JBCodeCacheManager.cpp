289b84353e07deab273d7d8accc06ae466813e6a
Akira Saitoh <saiaki@jp.ibm.com>
2022-10-12 12:48:31 +0900

    AArch64 Mac: Update JBCodeCacheManager to enable jitbuilder
    This commit updates JBCodeCacheManager to allocate
    the CodeCacheMemorySegment object by malloc instead of using a part of
    the mmaped memory segment on AArch64 Mac. The mmaped memory segment is
    not both writable and executable at the same time on Apple Silicon.
    If the CodeCacheMemorySegment object is allocated on the mmaped segment,
    an access violation would occur when updating its members.
    
    Signed-off-by: Akira Saitoh <saiaki@jp.ibm.com>

--- jitbuilder/runtime/JBCodeCacheManager.cpp
+++ jitbuilder/runtime/JBCodeCacheManager.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -98,7 +98,11 @@ JitBuilder::CodeCacheManager::allocateCodeCacheSegment(size_t segmentSize,
          mmap(NULL,
               codeCacheSizeToAllocate,
               PROT_READ | PROT_WRITE | PROT_EXEC,
+#if defined(OMR_ARCH_AARCH64) && defined(OSX)
+              MAP_ANONYMOUS | MAP_PRIVATE | MAP_JIT,
+#else
               MAP_ANONYMOUS | MAP_PRIVATE,
+#endif /* OMR_ARCH_AARCH64 && OSX */
               -1,
               0));
    // keep the impact of this fix localized
@@ -107,8 +111,13 @@ JitBuilder::CodeCacheManager::allocateCodeCacheSegment(size_t segmentSize,
       #undef NO_MAP_ANONYMOUS
    #endif
 #endif /* OMR_OS_WINDOWS */
+#if defined(OMR_ARCH_AARCH64) && defined(OSX)
+   TR::CodeCacheMemorySegment *memSegment = (TR::CodeCacheMemorySegment *) malloc(sizeof(TR::CodeCacheMemorySegment));
+   new (memSegment) TR::CodeCacheMemorySegment(memorySlab, reinterpret_cast<uint8_t *>(memorySlab) + codeCacheSizeToAllocate);
+#else  /* OMR_ARCH_AARCH64 && OSX */
    TR::CodeCacheMemorySegment *memSegment = (TR::CodeCacheMemorySegment *) ((size_t)memorySlab + codeCacheSizeToAllocate - sizeof(TR::CodeCacheMemorySegment));
    new (memSegment) TR::CodeCacheMemorySegment(memorySlab, reinterpret_cast<uint8_t *>(memSegment));
+#endif
    return memSegment;
    }
 
@@ -119,6 +128,9 @@ JitBuilder::CodeCacheManager::freeCodeCacheSegment(TR::CodeCacheMemorySegment * 
    VirtualFree(memSegment->_base, 0, MEM_RELEASE); // second arg must be zero when calling with MEM_RELEASE
 #elif defined(J9ZOS390)
    free(memSegment->_base);
+#elif defined(OMR_ARCH_AARCH64) && defined(OSX)
+   munmap(memSegment->_base, memSegment->_top - memSegment->_base);
+   free(memSegment);
 #else
    munmap(memSegment->_base, memSegment->_top - memSegment->_base + sizeof(TR::CodeCacheMemorySegment));
 #endif

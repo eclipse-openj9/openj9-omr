BradleyWood/omr

x86: Extend CPUID support
Signed-off-by: Bradley Wood <bradley.wood@ibm.com>

compiler/env/ProcessorInfo.hpp
@@ -42,6 +42,7 @@ struct TR_X86CPUIDBuffer
       uint32_t l3;
       } _cacheDescription;
    uint32_t _featureFlags8;
+   uint32_t _featureFlags10;
    };
 
 enum TR_X86ProcessorVendors
@@ -194,6 +195,48 @@ inline uint32_t getFeatureFlags8Mask()
          | TR_AVX512DQ
          | TR_AVX512CD;
    }
+
+enum TR_X86ProcessorFeatures10
+   {
+   TR_PREFETCHWT1         = 0x00000001,
+   TR_AVX512_VBMI         = 0x00000002,
+   TR_UMIP                = 0x00000004,
+   TR_PKU                 = 0x00000008,
+   TR_OSPKE               = 0x00000010,
+   TR_WAITPKG             = 0x00000020,
+   TR_AVX512_VBMI2        = 0x00000040,
+   TR_CET_SS              = 0x00000080,
+   TR_GFNI                = 0x00000100,
+   TR_VAES                = 0x00000200,
+   TR_VPCLMULQDQ          = 0x00000400,
+   TR_AVX512_VNNI         = 0x00000800,
+   TR_AVX512_BITALG       = 0x00001000,
+   TR_TME_EN              = 0x00002000,
+   TR_AVX512_VPOPCNTDQ    = 0x00004000,
+   // Reserved by Intel   = 0x00008000,
+   TR_LA57                = 0x00010000,
+   TR_MAWAU_0             = 0x00020000,
+   TR_MAWAU_1             = 0x00040000,
+   TR_MAWAU_2             = 0x00080000,
+   TR_MAWAU_3             = 0x00100000,
+   TR_MAWAU_4             = 0x00200000,
+   TR_RDPID               = 0x00400000,
+   TR_KL                  = 0x00800000,
+   TR_BUS_LOCK_DETECT     = 0x01000000,
+   TR_CLDEMOTE            = 0x02000000,
+   // Reserved by Intel   = 0x04000000,
+   TR_MOVDIRI             = 0x08000000,
+   TR_MOVDIR64B           = 0x10000000,
+   TR_ENQCMD              = 0x20000000,
+   TR_SGX_LC              = 0x40000000,
+   TR_PKS                 = 0x80000000,
+   };
+
+inline uint32_t getFeatureFlags10Mask()
+   {
+   return 0;
+   }
+
 
 enum TR_ProcessorDescription
    {

eclipse-openj9/openj9-omr

compiler/env/ProcessorInfo.hpp
@@ -42,6 +42,7 @@ struct TR_X86CPUIDBuffer
       uint32_t l3;
       } _cacheDescription;
    uint32_t _featureFlags8;
+   uint32_t _featureFlags10;
    };
 
 enum TR_X86ProcessorVendors
@@ -194,6 +195,48 @@ inline uint32_t getFeatureFlags8Mask()
          | TR_AVX512DQ
          | TR_AVX512CD;
    }
+
+enum TR_X86_CPUID_ecx_eax07_ecx00
+   {
+   TR_PREFETCHWT1         = 0x00000001,
+   TR_AVX512_VBMI         = 0x00000002,
+   TR_UMIP                = 0x00000004,
+   TR_PKU                 = 0x00000008,
+   TR_OSPKE               = 0x00000010,
+   TR_WAITPKG             = 0x00000020,
+   TR_AVX512_VBMI2        = 0x00000040,
+   TR_CET_SS              = 0x00000080,
+   TR_GFNI                = 0x00000100,
+   TR_VAES                = 0x00000200,
+   TR_VPCLMULQDQ          = 0x00000400,
+   TR_AVX512_VNNI         = 0x00000800,
+   TR_AVX512_BITALG       = 0x00001000,
+   TR_TME_EN              = 0x00002000,
+   TR_AVX512_VPOPCNTDQ    = 0x00004000,
+   // Reserved by Intel   = 0x00008000,
+   TR_LA57                = 0x00010000,
+   TR_MAWAU_0             = 0x00020000,
+   TR_MAWAU_1             = 0x00040000,
+   TR_MAWAU_2             = 0x00080000,
+   TR_MAWAU_3             = 0x00100000,
+   TR_MAWAU_4             = 0x00200000,
+   TR_RDPID               = 0x00400000,
+   TR_KL                  = 0x00800000,
+   TR_BUS_LOCK_DETECT     = 0x01000000,
+   TR_CLDEMOTE            = 0x02000000,
+   // Reserved by Intel   = 0x04000000,
+   TR_MOVDIRI             = 0x08000000,
+   TR_MOVDIR64B           = 0x10000000,
+   TR_ENQCMD              = 0x20000000,
+   TR_SGX_LC              = 0x40000000,
+   TR_PKS                 = 0x80000000,
+   };
+
+inline uint32_t getFeatureFlags10Mask()
+   {
+   return 0;
+   }
+
 
 enum TR_ProcessorDescription
    {
1773bf785942663082339e5e0a65d8fe1bd3c175
Marius <mpirvu@ca.ibm.com>
2024-05-31 07:59:33 -0700

    Revert "Set live local index bit vector only when register candidate is created"
    This reverts commit bdb57f72869c9c219a4bd244c3380ca253c1326c.

--- compiler/optimizer/GlobalRegisterAllocator.cpp
+++ compiler/optimizer/GlobalRegisterAllocator.cpp
@@ -2733,12 +2733,11 @@ void TR_GlobalRegisterAllocator::offerAllAutosAndRegisterParmAsCandidates(TR::Bl
             if (paramCursor->getLinkageRegisterIndex() >= 0)
                rc->addAllBlocks();
             }
-
-         i = paramCursor->getLiveLocalIndex();
-         autoAndParmLiveLocalIndex.set(i);
-         registerCandidateByIndex[i] = rc;
          }
 
+      i = paramCursor->getLiveLocalIndex();
+      autoAndParmLiveLocalIndex.set(i);
+      registerCandidateByIndex[i] = rc;
       paramCursor = paramIterator.getNext();
       }
 

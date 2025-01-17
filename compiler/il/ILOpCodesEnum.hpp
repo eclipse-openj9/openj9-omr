de880018ab6b43711d085db3ca2be919d60b48f8
Mark Stoodley <mstoodle@ca.ibm.com>
2025-01-17 11:35:19 -0500

    Correct extensible #include reference
    Signed-off-by: Mark Stoodley <mstoodle@ca.ibm.com>

--- compiler/il/ILOpCodesEnum.hpp
+++ compiler/il/ILOpCodesEnum.hpp
@@ -22,7 +22,7 @@
 #ifndef ILOPCODES_ENUM_INCL
 #define ILOPCODES_ENUM_INCL
 
-#include "compiler/il/OMRILOpCodesEnum.hpp"
+#include "il/OMRILOpCodesEnum.hpp"
 
    FirstTROp = FirstOMROp,
    LastTROp = LastScalarOMROp,

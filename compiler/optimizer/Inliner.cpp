1cf8e40dac7a8932c0a1ec0b873a41d041630a81
Devin Papineau <devin@ajdmp.ca>
2025-03-25 13:13:53 -0400

    Remove out-of-date comment about the result of inlineCallTarget2()

--- compiler/optimizer/Inliner.cpp
+++ compiler/optimizer/Inliner.cpp
@@ -4764,7 +4764,6 @@ bool OMR_InlinerPolicy::tryToInlineTrivialMethod (TR_CallStack* callStack, TR_Ca
    }
 
 //returns false when inlining fails
-//TODO: currently this method returns true in some cases when the inlining fails. This needs to be fixed
 bool TR_InlinerBase::inlineCallTarget2(TR_CallStack * callStack, TR_CallTarget *calltarget, TR::TreeTop** cursorTreeTop, bool inlinefromgraph, int32_t)
    {
    TR_InlinerDelimiter delimiter(tracer(),"inlineCallTarget2");

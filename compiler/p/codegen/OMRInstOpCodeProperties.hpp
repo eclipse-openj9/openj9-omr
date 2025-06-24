568a77bfb55df7acdb16421adccab73e7f921010
Luke Li <luke.li@ibm.com>
2025-06-24 11:03:21 -0400

    Enable Power Instruction cnttzw and cnttzd
    Enable cnttzw and cnttzd which is used by the CountPositives
    intrinsic for P9 and above.
    
    Signed-off-by: Luke Li <luke.li@ibm.com>

--- compiler/p/codegen/OMRInstOpCodeProperties.hpp
+++ compiler/p/codegen/OMRInstOpCodeProperties.hpp
@@ -1074,17 +1074,17 @@
    /* .properties  = */ OMR::Power::InstOpCode::metadata[OMR::InstOpCode::cntlzw].properties & ~PPCOpProp_HasRecordForm | PPCOpProp_IsRecordForm,
    },
 
-   /* { */
-   /* .mnemonic    =    OMR::InstOpCode::cnttzw, */
-   /* .name        =    "cnttzw", */
+   {
+   /* .mnemonic    = */ OMR::InstOpCode::cnttzw,
+   /* .name        = */ "cnttzw",
    /* .description =    "Count Trailing Zeros Word", */
-   /* .prefix      =    0x00000000, */
-   /* .opcode      =    0x7C000434, */
-   /* .format      =    FORMAT_UNKNOWN, */
-   /* .minimumALS  =    OMR_PROCESSOR_PPC_P9, */
-   /* .properties  =    PPCOpProp_HasRecordForm | */
-   /*                   PPCOpProp_SyncSideEffectFree, */
-   /* }, */
+   /* .prefix      = */ 0x00000000,
+   /* .opcode      = */ 0x7C000434,
+   /* .format      = */ FORMAT_RA_RS,
+   /* .minimumALS  = */ OMR_PROCESSOR_PPC_P9,
+   /* .properties  = */ PPCOpProp_HasRecordForm |
+                        PPCOpProp_SyncSideEffectFree,
+   },
 
    /* { */
    /* .mnemonic    =    OMR::InstOpCode::cnttzw_r, */
@@ -1097,17 +1097,17 @@
    /* .properties  =    OMR::Power::InstOpCode::metadata[OMR::InstOpCode::cnttzw].properties & ~PPCOpProp_HasRecordForm | PPCOpProp_IsRecordForm, */
    /* }, */
 
-   /* { */
-   /* .mnemonic    =    OMR::InstOpCode::cnttzd, */
-   /* .name        =    "cnttzd", */
+   {
+   /* .mnemonic    = */ OMR::InstOpCode::cnttzd,
+   /* .name        = */ "cnttzd",
    /* .description =    "Count Trailing Zeros Dword", */
-   /* .prefix      =    0x00000000, */
-   /* .opcode      =    0x7C000474, */
-   /* .format      =    FORMAT_UNKNOWN, */
-   /* .minimumALS  =    OMR_PROCESSOR_PPC_P9, */
-   /* .properties  =    PPCOpProp_HasRecordForm | */
-   /*                   PPCOpProp_SyncSideEffectFree, */
-   /* }, */
+   /* .prefix      = */ 0x00000000,
+   /* .opcode      = */ 0x7C000474,
+   /* .format      = */ FORMAT_RA_RS,
+   /* .minimumALS  = */ OMR_PROCESSOR_PPC_P9,
+   /* .properties  = */ PPCOpProp_HasRecordForm |
+                        PPCOpProp_SyncSideEffectFree,
+   },
 
    /* { */
    /* .mnemonic    =    OMR::InstOpCode::cnttzd_r, */

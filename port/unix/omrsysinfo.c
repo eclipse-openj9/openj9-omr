eclipse-omr/omr

Use proc_listpids to extract pids on macOS
Use proc_pidpath to extract executable paths.

port/unix/omrsysinfo.c
@@ -7687,7 +7687,53 @@ omrsysinfo_get_processes(struct OMRPortLibrary *portLibrary, OMRProcessInfoCallb
 	portLibrary->mem_free_memory(portLibrary, command);
 	closedir(dir);
 	return callback_result;
-#else /* defined(LINUX) */
+#elif defined(OSX) /* defined(LINUX) */
+	uintptr_t callback_result = 0;
+	pid_t *pid_buffer = NULL;
+	int actual_bytes = 0;
+	int i = 0;
+	int num_procs = 0;
+	char path_buf[PROC_PIDPATHINFO_MAXSIZE];
+	int num_bytes = proc_listpids(PROC_ALL_PIDS, 0, NULL, 0);
+	if (num_bytes <= 0) {
+		int32_t rc = findError(errno);
+		portLibrary->error_set_last_error(portLibrary, errno, rc);
+		Trc_PRT_failed_to_call_proc_listpids(rc);
+		return (uintptr_t)(intptr_t)rc;
+	}
+	pid_buffer = (pid_t *)portLibrary->mem_allocate_memory(
+		portLibrary,
+		num_bytes,
+		OMR_GET_CALLSITE(),
+		OMRMEM_CATEGORY_PORT_LIBRARY);
+	if (NULL == pid_buffer) {
+		return OMRPORT_ERROR_SYSINFO_MEMORY_ALLOC_FAILED;
+	}
+	actual_bytes = proc_listpids(PROC_ALL_PIDS, 0, pid_buffer, num_bytes);
+	if (actual_bytes <= 0) {
+		int32_t rc = findError(errno);
+		portLibrary->error_set_last_error(portLibrary, errno, rc);
+		Trc_PRT_failed_to_call_proc_listpids(rc);
+		portLibrary->mem_free_memory(portLibrary, pid_buffer);
+		return (uintptr_t)(intptr_t)rc;
+	}
+	num_procs = actual_bytes / sizeof(pid_t);
+	for (i = 0; i < num_procs; i++) {
+		pid_t pid = pid_buffer[i];
+		if (pid <= 0) {
+			continue;
+		}
+		memset(path_buf, 0, sizeof(path_buf));
+		if (proc_pidpath(pid, path_buf, sizeof(path_buf)) > 0) {
+			callback_result = callback((uintptr_t)pid, path_buf, userData);
+			if (0 != callback_result) {
+				break;
+			}
+		}
+	}
+	portLibrary->mem_free_memory(portLibrary, pid_buffer);
+	return callback_result;
+#else /* defined(OSX) */
 	/* sysinfo_get_processes is not supported on this platform. */
 	return OMRPORT_ERROR_SYSINFO_NOT_SUPPORTED;
 #endif /* defined(LINUX) */

eclipse-openj9/openj9-omr

port/unix/omrsysinfo.c
@@ -7783,7 +7783,60 @@ alloc_failed:
 	portLibrary->mem_free_memory(portLibrary, command);
 	closedir(dir);
 	return callbackResult;
-#else /* defined(LINUX) */
+#elif defined(OSX) /* defined(LINUX) */
+	uintptr_t callbackResult = 0;
+	pid_t *pidBuffer = NULL;
+	int actualBytes = 0;
+	int i = 0;
+	int numProcs = 0;
+	int numBytes = 0;
+	if (NULL == callback) {
+		portLibrary->error_set_last_error_with_message(
+				portLibrary,
+				OMRPORT_ERROR_OPFAILED,
+				"Callback function is NULL.");
+		return (uintptr_t)(intptr_t)OMRPORT_ERROR_OPFAILED;
+	}
+	numBytes = proc_listpids(PROC_ALL_PIDS, 0, NULL, 0);
+	if (numBytes <= 0) {
+		int32_t rc = findError(errno);
+		portLibrary->error_set_last_error(portLibrary, errno, rc);
+		Trc_PRT_failed_to_call_proc_listpids(rc);
+		return (uintptr_t)(intptr_t)rc;
+	}
+	pidBuffer = (pid_t *)portLibrary->mem_allocate_memory(
+			portLibrary,
+			numBytes,
+			OMR_GET_CALLSITE(),
+			OMRMEM_CATEGORY_PORT_LIBRARY);
+	if (NULL == pidBuffer) {
+		return (uintptr_t)(intptr_t)OMRPORT_ERROR_SYSINFO_MEMORY_ALLOC_FAILED;
+	}
+	actualBytes = proc_listpids(PROC_ALL_PIDS, 0, pidBuffer, numBytes);
+	if (actualBytes <= 0) {
+		int32_t rc = findError(errno);
+		portLibrary->error_set_last_error(portLibrary, errno, rc);
+		Trc_PRT_failed_to_call_proc_listpids(rc);
+		portLibrary->mem_free_memory(portLibrary, pidBuffer);
+		return (uintptr_t)(intptr_t)rc;
+	}
+	numProcs = actualBytes / sizeof(pid_t);
+	for (i = 0; i < numProcs; i++) {
+		pid_t pid = pidBuffer[i];
+		char pathBuf[PROC_PIDPATHINFO_MAXSIZE];
+		if (pid <= 0) {
+			continue;
+		}
+		if (proc_pidpath(pid, pathBuf, sizeof(pathBuf)) > 0) {
+			callbackResult = callback((uintptr_t)pid, pathBuf, userData);
+			if (0 != callbackResult) {
+				break;
+			}
+		}
+	}
+	portLibrary->mem_free_memory(portLibrary, pidBuffer);
+	return callbackResult;
+#else /* defined(OSX) */
 	/* sysinfo_get_processes is not supported on this platform. */
 	return OMRPORT_ERROR_SYSINFO_NOT_SUPPORTED;
 #endif /* defined(AIXPPC) */
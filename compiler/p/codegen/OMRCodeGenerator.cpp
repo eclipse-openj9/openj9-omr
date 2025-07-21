midronij/omr

Implement mAllTrue and mmAllTrue
Signed-off-by: midronij <jackie.midroni@ibm.com>

compiler/p/codegen/OMRCodeGenerator.cpp
@@ -1884,6 +1884,12 @@ bool OMR::Power::CodeGenerator::getSupportsOpCodeForAutoSIMD(TR::CPU *cpu, TR::I
             return true;
          else
             return false;
+      case TR::mAnyTrue:
+      case TR::mmAnyTrue:
+         if (cpu->isAtLeast(OMR_PROCESSOR_PPC_P8))
+            return true;
+         else
+            return false;
       case TR::vload:
       case TR::vloadi:
       case TR::vstore:

eclipse-openj9/openj9-omr

compiler/p/codegen/OMRCodeGenerator.cpp
@@ -1884,6 +1884,12 @@ bool OMR::Power::CodeGenerator::getSupportsOpCodeForAutoSIMD(TR::CPU *cpu, TR::I
             return true;
          else
             return false;
+      case TR::mAllTrue:
+      case TR::mmAllTrue:
+         if (cpu->isAtLeast(OMR_PROCESSOR_PPC_P8))
+            return true;
+         else
+            return false;
       case TR::vload:
       case TR::vloadi:
       case TR::vstore:
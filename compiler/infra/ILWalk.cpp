0xdaryl/omr

Replace traceMsg() with direct log updates
Guard tracing with checks for tracing enabled if missing.

Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/infra/ILWalk.cpp
@@ -28,6 +28,7 @@
 #include "infra/Cfg.hpp"
 #include "optimizer/Optimization.hpp"
 #include "optimizer/Optimization_inlines.hpp"
+#include "ras/Logger.hpp"
 
 #include <stdarg.h>
 
@@ -59,10 +60,10 @@ void TR::TreeTopIteratorImpl::logCurrentLocation()
     if (_name && _comp && _comp->getOption(TR_TraceILWalks)) {
         if (currentTree()) {
             TR::Node *node = currentTree()->getNode();
-            traceMsg(_comp, "TREE  %s @ %s n%dn [%p]\n", _name, node->getOpCode().getName(), node->getGlobalIndex(),
-                node);
+            _comp->getLogger()->printf("TREE  %s @ %s n%dn [%p]\n", _name, node->getOpCode().getName(),
+                node->getGlobalIndex(), node);
         } else {
-            traceMsg(_comp, "TREE  %s finished\n", _name);
+            _comp->getLogger()->printf("TREE  %s finished\n", _name);
         }
     }
 }
@@ -115,25 +116,26 @@ bool TR::NodeIterator::isAt(PreorderNodeIterator &other)
 void TR::NodeIterator::logCurrentLocation()
 {
     if (_name && comp() && comp()->getOption(TR_TraceILWalks)) {
+        TR::Logger *log = comp()->getLogger();
         if (currentTree()) {
             TR::Node *node = currentNode();
-            traceMsg(comp(), "NODE  %s  ", _name);
+            log->printf("NODE  %s  ", _name);
             if (stackDepth() >= 2) {
-                traceMsg(comp(), " ");
+                log->printc(' ');
                 for (int32_t i = 0; i < stackDepth() - 2; i++) {
                     if (_stack[i]._isBetweenChildren)
-                        traceMsg(comp(), " |");
+                        log->prints(" |");
                     else
-                        traceMsg(comp(), "  ");
+                        log->prints("  ");
                 }
-                traceMsg(comp(), " %d: ", _stack[_stack.topIndex() - 1]._child);
+                log->printf(" %d: ", _stack[_stack.topIndex() - 1]._child);
             }
-            traceMsg(comp(), "%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
+            log->printf("%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
         } else {
             // Usualy this one doesn't print, because when the iterator finishes
             // naturally, logCurrentLocation is not even called.
             //
-            traceMsg(comp(), "NODE  %s finished\n", _name);
+            log->printf("NODE  %s finished\n", _name);
         }
     }
 }
@@ -264,25 +266,26 @@ TR::Node *TR::NodeOccurrenceIterator::currentNode()
 void TR::NodeOccurrenceIterator::logCurrentLocation()
 {
     if (_name && comp() && comp()->getOption(TR_TraceILWalks)) {
+        TR::Logger *log = comp()->getLogger();
         if (currentTree()) {
             TR::Node *node = currentNode();
-            traceMsg(comp(), "WALK  %s  ", _name);
+            log->printf("WALK  %s  ", _name);
             if (stackDepth() >= 1) {
-                traceMsg(comp(), " ");
+                log->printc(' ');
                 for (int32_t i = 0; i < stackDepth() - 1; i++) {
                     if (_stack[i]._isBetweenChildren)
-                        traceMsg(comp(), " |");
+                        log->prints(" |");
                     else
-                        traceMsg(comp(), "  ");
+                        log->prints("  ");
                 }
-                traceMsg(comp(), " %d: ", _stack[_stack.topIndex()]._child);
+                log->printf(" %d: ", _stack[_stack.topIndex()]._child);
             }
-            traceMsg(comp(), "%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
+            log->printf("%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
         } else {
             // Usually this one doesn't print, because when the iterator finishes
             // naturally, logCurrentLocation is not even called.
             //
-            traceMsg(comp(), "WALK  %s finished\n", _name);
+            log->printf("WALK  %s finished\n", _name);
         }
     }
 }
@@ -396,10 +399,11 @@ TR::ReversePostorderSnapshotBlockIterator::ReversePostorderSnapshotBlockIterator
 {
     takeSnapshot(cfg->getStart()->asBlock());
     if (isLoggingEnabled()) {
-        traceMsg(comp, "BLOCK  %s Snapshot:", _name);
+        TR::Logger *log = comp->getLogger();
+        log->printf("BLOCK  %s Snapshot:", _name);
         for (int32_t i = _postorder.lastIndex(); i >= 0; --i)
-            traceMsg(comp, " %d", _postorder[i]->getNumber());
-        traceMsg(comp, "\n");
+            log->printf(" %d", _postorder[i]->getNumber());
+        log->println();
     }
     logCurrentLocation();
 }
@@ -462,7 +466,8 @@ bool TR::ReversePostorderSnapshotBlockIterator::isStepOperationFinished()
         return true; // Reached the next block in the walk
 
     if (isLoggingEnabled())
-        traceMsg(comp(), "BLOCK  %s Skip block_%d removed during walk\n", _name, currentBlock()->getNumber());
+        comp()->getLogger()->printf("BLOCK  %s Skip block_%d removed during walk\n", _name,
+            currentBlock()->getNumber());
     return false;
 }
 
@@ -470,9 +475,9 @@ void TR::ReversePostorderSnapshotBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (currentBlock())
-            traceMsg(comp(), "BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
+            comp()->getLogger()->printf("BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
         else
-            traceMsg(comp(), "BLOCK  %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK  %s finished\n", _name);
     }
 }
 
@@ -532,8 +537,9 @@ void TR::AllBlockIterator::stepForward()
                 // Found one!
                 //
                 if (isLoggingEnabled())
-                    traceMsg(comp(), "BLOCK  %s REMOVED_BLOCKS_CAN_BE_REINSERTED: block_%d found via extra scan\n",
-                        _name, next->asBlock()->getNumber());
+                    comp()->getLogger()->printf(
+                        "BLOCK  %s REMOVED_BLOCKS_CAN_BE_REINSERTED: block_%d found via extra scan\n", _name,
+                        next->asBlock()->getNumber());
                 break;
             }
         }
@@ -559,9 +565,9 @@ void TR::AllBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (currentBlock())
-            traceMsg(comp(), "BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
+            comp()->getLogger()->printf("BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
         else
-            traceMsg(comp(), "BLOCK  %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK  %s finished\n", _name);
     }
 }
 
@@ -609,9 +615,9 @@ void TR::TreeTopOrderExtendedBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (getFirst() != NULL) {
-            traceMsg(comp(), "BLOCK %s @ block_%d\n", _name, getFirst()->getNumber());
+            comp()->getLogger()->printf("BLOCK %s @ block_%d\n", _name, getFirst()->getNumber());
         } else {
-            traceMsg(comp(), "BLOCK %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK %s finished\n", _name);
         }
     }
 }

compiler/optimizer/LocalOpts.hpp
@@ -38,6 +38,7 @@
 #include "infra/List.hpp"
 #include "optimizer/Optimization.hpp"
 #include "optimizer/OptimizationManager.hpp"
+#include "ras/Logger.hpp"
 
 class TR_RegionStructure;
 class TR_RematAdjustments;
@@ -597,10 +598,10 @@ private:
             int i;
 
             for (i = _nextIndex; i > 0 && _array[i / 2]->objectGT(t->getObject()); i /= 2) {
-                // traceMsg("\tMoving %d to %d\n", i, i/2);
+                // comp->getLogger()->printf("\tMoving %d to %d\n", i, i/2);
                 _array[i] = _array[i / 2];
             }
-            // traceMsg("Storing new item at location %d, _nextIndex is %d\n", i, _nextIndex);
+            // comp->getLogger()->printf("Storing new item at location %d, _nextIndex is %d\n", i, _nextIndex);
             _array[i] = t;
 
             return _nextIndex++;
@@ -631,11 +632,12 @@ private:
 
         void dumpList(TR::Compilation *comp)
         {
-            traceMsg(comp, "heap dump\n");
+            TR::Logger *log = comp->getLogger();
+            log->prints("heap dump\n");
             for (uint32_t i = 0; i < _nextIndex; i++) {
-                traceMsg(comp, "%d [idx %d], ", _array[i]->getObject()->getNumber(), _array[i]->getIndex());
+                log->printf("%d [idx %d], ", _array[i]->getObject()->getNumber(), _array[i]->getIndex());
             }
-            traceMsg(comp, "end heap dump\n");
+            log->prints("end heap dump\n");
         }
 
     private:

compiler/optimizer/LoopReducer.cpp
@@ -4045,6 +4045,8 @@ TR_LoopReducer::TR_LoopReducer(TR::OptimizationManager *manager)
 
 int32_t TR_LoopReducer::perform()
 {
+    TR::Logger *log = comp()->getLogger();
+
     // enable only if the new loop reduction framework is
     // disabled
     //
@@ -4068,9 +4070,9 @@ int32_t TR_LoopReducer::perform()
 
     _cfg = comp()->getFlowGraph();
     if (trace()) {
-        traceMsg(comp(), "Starting LoopReducer\n");
-        traceMsg(comp(), "\nCFG before loop reduction:\n");
-        getDebug()->print(comp()->getLogger(), _cfg);
+        log->prints("Starting LoopReducer\n");
+        log->prints("\nCFG before loop reduction:\n");
+        getDebug()->print(log, _cfg);
     }
 
     // From this point on, stack memory allocations will die when the function returns
@@ -4113,9 +4115,9 @@ int32_t TR_LoopReducer::perform()
     optimizer()->setValueNumberInfo(NULL);
 
     if (trace()) {
-        traceMsg(comp(), "\nCFG after loop reduction:\n");
-        getDebug()->print(comp()->getLogger(), _cfg);
-        traceMsg(comp(), "Ending LoopReducer\n");
+        log->prints("\nCFG after loop reduction:\n");
+        getDebug()->print(log, _cfg);
+        log->prints("Ending LoopReducer\n");
     }
 
     return 1; // actual cost

compiler/optimizer/abstractinterpreter/InliningMethodSummary.cpp
@@ -20,6 +20,7 @@
  *******************************************************************************/
 
 #include "optimizer/abstractinterpreter/InliningMethodSummary.hpp"
+#include "ras/Logger.hpp"
 
 uint32_t TR::InliningMethodSummary::testArgument(TR::AbsValue *arg, uint32_t argPos)
 {
@@ -46,10 +47,12 @@ uint32_t TR::InliningMethodSummary::testArgument(TR::AbsValue *arg, uint32_t arg
 
 void TR::InliningMethodSummary::trace(TR::Compilation *comp)
 {
-    traceMsg(comp, "Inlining Method Summary:\n");
+    TR::Logger *log = comp->getLogger();
+
+    log->prints("Inlining Method Summary:\n");
 
     if (_optsByArg.size() == 0) {
-        traceMsg(comp, "EMPTY\n\n");
+        log->prints("EMPTY\n\n");
         return;
     }
 
@@ -58,9 +61,9 @@ void TR::InliningMethodSummary::trace(TR::Compilation *comp)
             for (size_t j = 0; j < _optsByArg[i]->size(); j++) {
                 TR::PotentialOptimizationPredicate *predicate = (*_optsByArg[i])[j];
 
-                traceMsg(comp, "%s @%d for Argument %d ", predicate->getName(), predicate->getBytecodeIndex(), i);
+                log->printf("%s @%d for Argument %d ", predicate->getName(), predicate->getBytecodeIndex(), i);
                 predicate->trace(comp);
-                traceMsg(comp, "\n");
+                log->println();
             }
         }
     }
@@ -169,6 +172,6 @@ bool TR::PotentialOptimizationVPPredicate::test(TR::AbsValue *value)
 
 void TR::PotentialOptimizationVPPredicate::trace(TR::Compilation *comp)
 {
-    traceMsg(comp, "Predicate Constraint: ");
+    comp->getLogger()->prints("Predicate Constraint: ");
     _constraint->print(_vp);
 }

eclipse-openj9/openj9-omr

compiler/infra/ILWalk.cpp
@@ -28,6 +28,7 @@
 #include "infra/Cfg.hpp"
 #include "optimizer/Optimization.hpp"
 #include "optimizer/Optimization_inlines.hpp"
+#include "ras/Logger.hpp"
 
 #include <stdarg.h>
 
@@ -59,10 +60,10 @@ void TR::TreeTopIteratorImpl::logCurrentLocation()
     if (_name && _comp && _comp->getOption(TR_TraceILWalks)) {
         if (currentTree()) {
             TR::Node *node = currentTree()->getNode();
-            traceMsg(_comp, "TREE  %s @ %s n%dn [%p]\n", _name, node->getOpCode().getName(), node->getGlobalIndex(),
-                node);
+            _comp->getLogger()->printf("TREE  %s @ %s n%dn [%p]\n", _name, node->getOpCode().getName(),
+                node->getGlobalIndex(), node);
         } else {
-            traceMsg(_comp, "TREE  %s finished\n", _name);
+            _comp->getLogger()->printf("TREE  %s finished\n", _name);
         }
     }
 }
@@ -115,25 +116,26 @@ bool TR::NodeIterator::isAt(PreorderNodeIterator &other)
 void TR::NodeIterator::logCurrentLocation()
 {
     if (_name && comp() && comp()->getOption(TR_TraceILWalks)) {
+        OMR::Logger *log = comp()->getLogger();
         if (currentTree()) {
             TR::Node *node = currentNode();
-            traceMsg(comp(), "NODE  %s  ", _name);
+            log->printf("NODE  %s  ", _name);
             if (stackDepth() >= 2) {
-                traceMsg(comp(), " ");
+                log->printc(' ');
                 for (int32_t i = 0; i < stackDepth() - 2; i++) {
                     if (_stack[i]._isBetweenChildren)
-                        traceMsg(comp(), " |");
+                        log->prints(" |");
                     else
-                        traceMsg(comp(), "  ");
+                        log->prints("  ");
                 }
-                traceMsg(comp(), " %d: ", _stack[_stack.topIndex() - 1]._child);
+                log->printf(" %d: ", _stack[_stack.topIndex() - 1]._child);
             }
-            traceMsg(comp(), "%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
+            log->printf("%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
         } else {
             // Usualy this one doesn't print, because when the iterator finishes
             // naturally, logCurrentLocation is not even called.
             //
-            traceMsg(comp(), "NODE  %s finished\n", _name);
+            log->printf("NODE  %s finished\n", _name);
         }
     }
 }
@@ -264,25 +266,26 @@ TR::Node *TR::NodeOccurrenceIterator::currentNode()
 void TR::NodeOccurrenceIterator::logCurrentLocation()
 {
     if (_name && comp() && comp()->getOption(TR_TraceILWalks)) {
+        OMR::Logger *log = comp()->getLogger();
         if (currentTree()) {
             TR::Node *node = currentNode();
-            traceMsg(comp(), "WALK  %s  ", _name);
+            log->printf("WALK  %s  ", _name);
             if (stackDepth() >= 1) {
-                traceMsg(comp(), " ");
+                log->printc(' ');
                 for (int32_t i = 0; i < stackDepth() - 1; i++) {
                     if (_stack[i]._isBetweenChildren)
-                        traceMsg(comp(), " |");
+                        log->prints(" |");
                     else
-                        traceMsg(comp(), "  ");
+                        log->prints("  ");
                 }
-                traceMsg(comp(), " %d: ", _stack[_stack.topIndex()]._child);
+                log->printf(" %d: ", _stack[_stack.topIndex()]._child);
             }
-            traceMsg(comp(), "%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
+            log->printf("%s n%dn [%p]\n", node->getOpCode().getName(), node->getGlobalIndex(), node);
         } else {
             // Usually this one doesn't print, because when the iterator finishes
             // naturally, logCurrentLocation is not even called.
             //
-            traceMsg(comp(), "WALK  %s finished\n", _name);
+            log->printf("WALK  %s finished\n", _name);
         }
     }
 }
@@ -396,10 +399,11 @@ TR::ReversePostorderSnapshotBlockIterator::ReversePostorderSnapshotBlockIterator
 {
     takeSnapshot(cfg->getStart()->asBlock());
     if (isLoggingEnabled()) {
-        traceMsg(comp, "BLOCK  %s Snapshot:", _name);
+        OMR::Logger *log = comp->getLogger();
+        log->printf("BLOCK  %s Snapshot:", _name);
         for (int32_t i = _postorder.lastIndex(); i >= 0; --i)
-            traceMsg(comp, " %d", _postorder[i]->getNumber());
-        traceMsg(comp, "\n");
+            log->printf(" %d", _postorder[i]->getNumber());
+        log->println();
     }
     logCurrentLocation();
 }
@@ -462,7 +466,8 @@ bool TR::ReversePostorderSnapshotBlockIterator::isStepOperationFinished()
         return true; // Reached the next block in the walk
 
     if (isLoggingEnabled())
-        traceMsg(comp(), "BLOCK  %s Skip block_%d removed during walk\n", _name, currentBlock()->getNumber());
+        comp()->getLogger()->printf("BLOCK  %s Skip block_%d removed during walk\n", _name,
+            currentBlock()->getNumber());
     return false;
 }
 
@@ -470,9 +475,9 @@ void TR::ReversePostorderSnapshotBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (currentBlock())
-            traceMsg(comp(), "BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
+            comp()->getLogger()->printf("BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
         else
-            traceMsg(comp(), "BLOCK  %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK  %s finished\n", _name);
     }
 }
 
@@ -532,8 +537,9 @@ void TR::AllBlockIterator::stepForward()
                 // Found one!
                 //
                 if (isLoggingEnabled())
-                    traceMsg(comp(), "BLOCK  %s REMOVED_BLOCKS_CAN_BE_REINSERTED: block_%d found via extra scan\n",
-                        _name, next->asBlock()->getNumber());
+                    comp()->getLogger()->printf(
+                        "BLOCK  %s REMOVED_BLOCKS_CAN_BE_REINSERTED: block_%d found via extra scan\n", _name,
+                        next->asBlock()->getNumber());
                 break;
             }
         }
@@ -559,9 +565,9 @@ void TR::AllBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (currentBlock())
-            traceMsg(comp(), "BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
+            comp()->getLogger()->printf("BLOCK  %s @ block_%d\n", _name, currentBlock()->getNumber());
         else
-            traceMsg(comp(), "BLOCK  %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK  %s finished\n", _name);
     }
 }
 
@@ -609,9 +615,9 @@ void TR::TreeTopOrderExtendedBlockIterator::logCurrentLocation()
 {
     if (isLoggingEnabled()) {
         if (getFirst() != NULL) {
-            traceMsg(comp(), "BLOCK %s @ block_%d\n", _name, getFirst()->getNumber());
+            comp()->getLogger()->printf("BLOCK %s @ block_%d\n", _name, getFirst()->getNumber());
         } else {
-            traceMsg(comp(), "BLOCK %s finished\n", _name);
+            comp()->getLogger()->printf("BLOCK %s finished\n", _name);
         }
     }
 }

compiler/optimizer/LocalOpts.hpp
@@ -38,6 +38,7 @@
 #include "infra/List.hpp"
 #include "optimizer/Optimization.hpp"
 #include "optimizer/OptimizationManager.hpp"
+#include "ras/Logger.hpp"
 
 class TR_RegionStructure;
 class TR_RematAdjustments;
@@ -597,10 +598,10 @@ private:
             int i;
 
             for (i = _nextIndex; i > 0 && _array[i / 2]->objectGT(t->getObject()); i /= 2) {
-                // traceMsg("\tMoving %d to %d\n", i, i/2);
+                // comp->getLogger()->printf("\tMoving %d to %d\n", i, i/2);
                 _array[i] = _array[i / 2];
             }
-            // traceMsg("Storing new item at location %d, _nextIndex is %d\n", i, _nextIndex);
+            // comp->getLogger()->printf("Storing new item at location %d, _nextIndex is %d\n", i, _nextIndex);
             _array[i] = t;
 
             return _nextIndex++;
@@ -631,11 +632,12 @@ private:
 
         void dumpList(TR::Compilation *comp)
         {
-            traceMsg(comp, "heap dump\n");
+            OMR::Logger *log = comp->getLogger();
+            log->prints("heap dump\n");
             for (uint32_t i = 0; i < _nextIndex; i++) {
-                traceMsg(comp, "%d [idx %d], ", _array[i]->getObject()->getNumber(), _array[i]->getIndex());
+                log->printf("%d [idx %d], ", _array[i]->getObject()->getNumber(), _array[i]->getIndex());
             }
-            traceMsg(comp, "end heap dump\n");
+            log->prints("end heap dump\n");
         }
 
     private:

compiler/optimizer/LoopReducer.cpp
@@ -4045,6 +4045,8 @@ TR_LoopReducer::TR_LoopReducer(TR::OptimizationManager *manager)
 
 int32_t TR_LoopReducer::perform()
 {
+    OMR::Logger *log = comp()->getLogger();
+
     // enable only if the new loop reduction framework is
     // disabled
     //
@@ -4068,9 +4070,9 @@ int32_t TR_LoopReducer::perform()
 
     _cfg = comp()->getFlowGraph();
     if (trace()) {
-        traceMsg(comp(), "Starting LoopReducer\n");
-        traceMsg(comp(), "\nCFG before loop reduction:\n");
-        getDebug()->print(comp()->getLogger(), _cfg);
+        log->prints("Starting LoopReducer\n");
+        log->prints("\nCFG before loop reduction:\n");
+        getDebug()->print(log, _cfg);
     }
 
     // From this point on, stack memory allocations will die when the function returns
@@ -4113,9 +4115,9 @@ int32_t TR_LoopReducer::perform()
     optimizer()->setValueNumberInfo(NULL);
 
     if (trace()) {
-        traceMsg(comp(), "\nCFG after loop reduction:\n");
-        getDebug()->print(comp()->getLogger(), _cfg);
-        traceMsg(comp(), "Ending LoopReducer\n");
+        log->prints("\nCFG after loop reduction:\n");
+        getDebug()->print(log, _cfg);
+        log->prints("Ending LoopReducer\n");
     }
 
     return 1; // actual cost

compiler/optimizer/abstractinterpreter/InliningMethodSummary.cpp
@@ -20,6 +20,7 @@
  *******************************************************************************/
 
 #include "optimizer/abstractinterpreter/InliningMethodSummary.hpp"
+#include "ras/Logger.hpp"
 
 uint32_t TR::InliningMethodSummary::testArgument(TR::AbsValue *arg, uint32_t argPos)
 {
@@ -46,10 +47,12 @@ uint32_t TR::InliningMethodSummary::testArgument(TR::AbsValue *arg, uint32_t arg
 
 void TR::InliningMethodSummary::trace(TR::Compilation *comp)
 {
-    traceMsg(comp, "Inlining Method Summary:\n");
+    OMR::Logger *log = comp->getLogger();
+
+    log->prints("Inlining Method Summary:\n");
 
     if (_optsByArg.size() == 0) {
-        traceMsg(comp, "EMPTY\n\n");
+        log->prints("EMPTY\n\n");
         return;
     }
 
@@ -58,9 +61,9 @@ void TR::InliningMethodSummary::trace(TR::Compilation *comp)
             for (size_t j = 0; j < _optsByArg[i]->size(); j++) {
                 TR::PotentialOptimizationPredicate *predicate = (*_optsByArg[i])[j];
 
-                traceMsg(comp, "%s @%d for Argument %d ", predicate->getName(), predicate->getBytecodeIndex(), i);
+                log->printf("%s @%d for Argument %d ", predicate->getName(), predicate->getBytecodeIndex(), i);
                 predicate->trace(comp);
-                traceMsg(comp, "\n");
+                log->println();
             }
         }
     }
@@ -169,6 +172,6 @@ bool TR::PotentialOptimizationVPPredicate::test(TR::AbsValue *value)
 
 void TR::PotentialOptimizationVPPredicate::trace(TR::Compilation *comp)
 {
-    traceMsg(comp, "Predicate Constraint: ");
+    comp->getLogger()->prints("Predicate Constraint: ");
     _constraint->print(_vp);
 }
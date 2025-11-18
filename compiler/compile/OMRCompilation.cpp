0xdaryl/omr

Eliminate some getLoggingEnabled() checks
Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/compile/OMRCompilation.cpp
@@ -925,7 +925,7 @@ int32_t OMR::Compilation::compile()
                 self()->failCompilation<TR::CompilationException>("Catch blocks have real predecessors");
             }
 
-            if ((debug("dumpInitialTrees") || self()->getOption(TR_TraceTrees)) && self()->getLoggingEnabled()) {
+            if (self()->getOption(TR_TraceTrees)) {
                 TR::Logger *log = self()->log();
                 self()->dumpMethodTrees(log, "Initial Trees");
                 self()->getDebug()->print(log, self()->getSymRefTab());
@@ -1813,10 +1813,6 @@ void OMR::Compilation::verifyAndFixRdbarAnchors()
 #ifdef DEBUG
 void OMR::Compilation::dumpMethodGraph(int index, TR::ResolvedMethodSymbol *methodSymbol)
 {
-    if (!self()->getLoggingEnabled()) {
-        return;
-    }
-
     TR::Logger *log = log();
 
     if (methodSymbol == 0)
@@ -2192,8 +2188,8 @@ void OMR::Compilation::diagnosticImpl(const char *s, ...)
 
 void OMR::Compilation::diagnosticImplVA(const char *s, va_list ap)
 {
-    if (self()->getLoggingEnabled()) {
-        TR::Logger *log = self()->log();
+    TR::Logger *log = self()->log();
+    if (log->isEnabled()) {
         log->vprintf(s, ap);
         log->flush();
     }

compiler/ras/Debug.cpp
@@ -350,7 +350,9 @@ void TR_Debug::addInstructionComment(TR::Instruction *instr, char *comment, ...)
 {
     TR_ASSERT(_comp, "Required compilation object is NULL.\n");
 
-    if (comment == NULL || !_comp->getLoggingEnabled())
+    static char *disableInstructionComments = feGetEnv("TR_DisableInstructionComments");
+
+    if (disableInstructionComments || comment == NULL || !_comp->log()->isEnabled())
         return;
 
     CS2::HashIndex hashIndex;
@@ -366,6 +368,7 @@ void TR_Debug::addInstructionComment(TR::Instruction *instr, char *comment, ...)
 
 bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char *format, ...)
 {
+    TR::Logger *log = _comp->log();
     int32_t optIndex = _comp->getOptIndex();
     int32_t firstOptIndex = _comp->getOptions()->getFirstOptIndex();
     int32_t lastOptIndex = _comp->getOptions()->getLastOptIndex();
@@ -383,10 +386,12 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
     bool alreadyFormatted = false;
     char messageBuffer[300];
 
+    static char *disablePerformTransformationLogging = feGetEnv("TR_DisablePerformTransformationLogging");
+
     // We try to avoid doing the work of formatting the string if we don't need to.
     //
     if ((_comp->getOption(TR_CountOptTransformations) && _comp->getOptions()->getVerboseOptTransformationsRegex())
-        || (_comp->getLoggingEnabled() && canOmitTransformation
+        || (!disablePerformTransformationLogging && log->isEnabled() && canOmitTransformation
             && _comp->getOptions()->getDisabledOptTransformations())) {
         va_list args;
         va_start(args, format);
@@ -428,10 +433,8 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
 
     // No need to do the printing logic below if logging is disabled
     //
-    if (!_comp->getLoggingEnabled())
+    if (disablePerformTransformationLogging || !log->isEnabled())
         return true;
-
-    TR::Logger *log = _comp->log();
 
     if (canOmitTransformation) {
         if (_registerAssignmentTraceFlags & TRACERA_IN_PROGRESS)
@@ -468,7 +471,7 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
 
 void TR_Debug::trace(const char *format, ...)
 {
-    if (_comp->getLoggingEnabled()) {
+    if (getLogger()->isEnabled()) {
         va_list args;
         va_start(args, format);
         vtrace(format, args);
@@ -492,8 +495,8 @@ void TR_Debug::vtrace(const char *format, va_list args)
 
 void TR_Debug::traceLnFromLogTracer(const char *preFormatted)
 {
-    if (_comp->getLoggingEnabled()) {
-        TR::Logger *log = getLogger();
+    TR::Logger *log = getLogger();
+    if (log->isEnabled()) {
         log->prints(preFormatted);
         log->println();
         log->flush();

eclipse-openj9/openj9-omr

compiler/compile/OMRCompilation.cpp
@@ -963,7 +963,7 @@ int32_t OMR::Compilation::compile()
                 self()->failCompilation<TR::CompilationException>("Catch blocks have real predecessors");
             }
 
-            if ((debug("dumpInitialTrees") || self()->getOption(TR_TraceTrees)) && self()->getLoggingEnabled()) {
+            if (self()->getOption(TR_TraceTrees)) {
                 OMR::Logger *log = self()->log();
                 self()->dumpMethodTrees(log, "Initial Trees");
                 self()->getDebug()->print(log, self()->getSymRefTab());
@@ -1904,10 +1904,6 @@ void OMR::Compilation::verifyAndFixRdbarAnchors()
 #ifdef DEBUG
 void OMR::Compilation::dumpMethodGraph(int index, TR::ResolvedMethodSymbol *methodSymbol)
 {
-    if (!self()->getLoggingEnabled()) {
-        return;
-    }
-
     OMR::Logger *log = this->log();
 
     if (methodSymbol == 0)
@@ -2322,8 +2318,8 @@ void OMR::Compilation::diagnosticImpl(const char *s, ...)
 
 void OMR::Compilation::diagnosticImplVA(const char *s, va_list ap)
 {
-    if (self()->getLoggingEnabled()) {
-        OMR::Logger *log = self()->log();
+    OMR::Logger *log = self()->log();
+    if (log->isEnabled_DEPRECATED()) {
         log->vprintf(s, ap);
         log->flush();
     }

compiler/ras/Debug.cpp
@@ -350,7 +350,9 @@ void TR_Debug::addInstructionComment(TR::Instruction *instr, char *comment, ...)
 {
     TR_ASSERT(_comp, "Required compilation object is NULL.\n");
 
-    if (comment == NULL || !_comp->getLoggingEnabled())
+    static char *disableInstructionComments = feGetEnv("TR_DisableInstructionComments");
+
+    if (disableInstructionComments || comment == NULL || !_comp->log()->isEnabled_DEPRECATED())
         return;
 
     CS2::HashIndex hashIndex;
@@ -366,6 +368,7 @@ void TR_Debug::addInstructionComment(TR::Instruction *instr, char *comment, ...)
 
 bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char *format, ...)
 {
+    OMR::Logger *log = _comp->log();
     int32_t optIndex = _comp->getOptIndex();
     int32_t firstOptIndex = _comp->getOptions()->getFirstOptIndex();
     int32_t lastOptIndex = _comp->getOptions()->getLastOptIndex();
@@ -383,10 +386,12 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
     bool alreadyFormatted = false;
     char messageBuffer[300];
 
+    static char *disablePerformTransformationLogging = feGetEnv("TR_DisablePerformTransformationLogging");
+
     // We try to avoid doing the work of formatting the string if we don't need to.
     //
     if ((_comp->getOption(TR_CountOptTransformations) && _comp->getOptions()->getVerboseOptTransformationsRegex())
-        || (_comp->getLoggingEnabled() && canOmitTransformation
+        || (!disablePerformTransformationLogging && log->isEnabled_DEPRECATED() && canOmitTransformation
             && _comp->getOptions()->getDisabledOptTransformations())) {
         va_list args;
         va_start(args, format);
@@ -428,10 +433,8 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
 
     // No need to do the printing logic below if logging is disabled
     //
-    if (!_comp->getLoggingEnabled())
+    if (disablePerformTransformationLogging || !log->isEnabled_DEPRECATED())
         return true;
-
-    OMR::Logger *log = _comp->log();
 
     if (canOmitTransformation) {
         if (_registerAssignmentTraceFlags & TRACERA_IN_PROGRESS)
@@ -468,7 +471,7 @@ bool TR_Debug::performTransformationImpl(bool canOmitTransformation, const char 
 
 void TR_Debug::trace(const char *format, ...)
 {
-    if (_comp->getLoggingEnabled()) {
+    if (getLogger()->isEnabled_DEPRECATED()) {
         va_list args;
         va_start(args, format);
         vtrace(format, args);
@@ -492,8 +495,8 @@ void TR_Debug::vtrace(const char *format, va_list args)
 
 void TR_Debug::traceLnFromLogTracer(const char *preFormatted)
 {
-    if (_comp->getLoggingEnabled()) {
-        OMR::Logger *log = getLogger();
+    OMR::Logger *log = getLogger();
+    if (log->isEnabled_DEPRECATED()) {
         log->prints(preFormatted);
         log->println();
         log->flush();
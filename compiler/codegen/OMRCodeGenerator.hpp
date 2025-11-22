f21091274c574cc0bf06c23bdce4832991de09ac
Daryl Maier <maier@ca.ibm.com>
2025-11-21 22:38:38 -0500

    Remove deprecated BuildStructure codegen functionality
    This functionality is no longer used in OMR or any downstream project.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- compiler/codegen/OMRCodeGenerator.hpp
+++ compiler/codegen/OMRCodeGenerator.hpp
@@ -1783,10 +1783,6 @@ public:
 
     TR::RealRegister **_unlatchedRegisterList; // dynamically allocated
 
-    bool shouldBuildStructure() { return _enabledFlags.testAny(ShouldBuildStructure); }
-
-    void setShouldBuildStructure() { _enabledFlags.set(ShouldBuildStructure); }
-
     bool enableRefinedAliasSets();
 
     void setEnableRefinedAliasSets() { _enabledFlags.set(EnableRefinedAliasSets); }
@@ -2411,7 +2407,7 @@ protected:
         // AVAILABLE                     = 0x0004,
         EnableRefinedAliasSets = 0x0008,
         // AVAILABLE                     = 0x0010,
-        ShouldBuildStructure = 0x0020,
+        // AVAILABLE                     = 0x0020,
         LockFreeSpillList = 0x0040, // TAROK only (until it matures)
         UseNonLinearRegisterAssigner = 0x0080, // TAROK only (until it matures)
         TrackRegisterUsage = 0x0100, // TAROK only (until it matures)

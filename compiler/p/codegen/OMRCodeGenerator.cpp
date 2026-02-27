c71d3fbcc2363e01e255b2ad1ffdd93a76e09a02
midronij <jackie.midroni@ibm.com>
2026-02-27 12:01:51 -0500

    Implement vectorized mLastTrue on PPC for P9+
    Signed-off-by: midronij <jackie.midroni@ibm.com>

--- compiler/p/codegen/OMRCodeGenerator.cpp
+++ compiler/p/codegen/OMRCodeGenerator.cpp
@@ -1679,6 +1679,7 @@ bool OMR::Power::CodeGenerator::getSupportsOpCodeForAutoSIMD(TR::CPU *cpu, TR::I
             else
                 return false;
         case TR::mFirstTrue:
+        case TR::mLastTrue:
             if (cpu->isAtLeast(OMR_PROCESSOR_PPC_P9))
                 return true;
             else

0xdaryl/omr

Add Logger print[sc] variants that return exact chars logged
`prints_len`
`printc_len`

Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/ras/Logger.cpp
@@ -43,6 +43,18 @@ TR::Logger::Logger()
     setSkipFlush(envSkipLoggerFlushes ? true : false);
 }
 
+int32_t TR::Logger::prints_len(const char *string)
+{
+    int32_t rc = prints(string);
+    return (rc > 0) ? strlen(string) : rc;
+}
+
+int32_t TR::Logger::printc_len(char c)
+{
+    int32_t rc = printc(c);
+    return (rc > 0) ? 1 : rc;
+}
+
 /*
  * -----------------------------------------------------------------------------
  * NullLogger

eclipse-openj9/openj9-omr

compiler/ras/Logger.cpp
@@ -44,6 +44,18 @@ OMR::Logger::Logger()
     setSkipFlush(envSkipLoggerFlushes ? true : false);
 }
 
+int32_t OMR::Logger::prints_len(const char *string)
+{
+    int32_t rc = prints(string);
+    return (rc >= 0) ? static_cast<int32_t>(strlen(string)) : rc;
+}
+
+int32_t OMR::Logger::printc_len(char c)
+{
+    int32_t rc = printc(c);
+    return (rc >= 0) ? 1 : rc;
+}
+
 /*
  * -----------------------------------------------------------------------------
  * NullLogger